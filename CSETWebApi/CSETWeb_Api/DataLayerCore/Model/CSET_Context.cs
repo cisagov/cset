948951c2a3da567056d3185f7a98c07b1e940fc5
Randy Woods <randall.woods@inl.gov>
2019-02-14 10:10:11 -0700

    Forgot to add the derived context class

---
+++ CSETWebApi/CSETWeb_Api/DataLayerCore/Model/CSET_Context.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayerCore.Model
+{
+    public class CSET_Context : CsetwebContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // TODO:  Does the connection string format differ from the old CSETWebEntities string?
+                string connectionString = ConfigurationManager.ConnectionStrings["CSET_DB"].ConnectionString;
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Query<VIEW_QUESTIONS_STATUS>().ToView("VIEW_QUESTIONS_STATUS").Property(v => v.Answer_Id).HasColumnName("Answer_Id");
+            modelBuilder.Query<vQUESTION_HEADINGS>().ToView("vQUESTION_HEADINGS").Property(v => v.Heading_Pair_Id).HasColumnName("Heading_Pair_Id");
+            modelBuilder.Query<Answer_Questions>().ToView("Answer_Questions").Property(v => v.Answer_Id).HasColumnName("Answer_Id");
+            modelBuilder.Query<Answer_Questions_No_Components>().ToView("Answer_Questions_No_Components").Property(v => v.Answer_Id).HasColumnName("Answer_Id");
+        }
+    }
+}
