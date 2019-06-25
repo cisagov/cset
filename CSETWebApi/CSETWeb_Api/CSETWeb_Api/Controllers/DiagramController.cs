96eec5ad4f3fb1c7fa473c5293982e1800f8d0b0
Randy Woods <randall.woods@inl.gov>
2019-06-25 09:15:48 -0600

    Start new API call to save diagram markup

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_Api/Controllers/DiagramController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CSETWeb_Api.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DiagramController : ApiController
+    {
+        /// <summary>
+        /// Persists the diagram XML in the database.
+        /// </summary>
+        /// <param name="diagramXml"></param>
+        [Route("api/diagram/save")]
+        public void SaveDiagram([FromBody] string diagramXml)
+        {
+            // DIAGRAM_MARKUP
+
+        }
+    }
+}
