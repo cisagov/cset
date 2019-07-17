1349c66fda5ab7a700b1daf3fd86491c0a3072e5
barry <5023364+inlguy@users.noreply.github.com>
2019-07-17 17:28:26 -0600

    added the guid controller

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_Api/Controllers/GuidController.cs
@@ -0,0 +1,27 @@
+﻿using CSETWeb_Api.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace CSETWeb_Api.Controllers
+{
+    [CSETAuthorize]
+    public class GuidController : ApiController
+    {
+        
+        [Route("api/guid/requestblock")]
+        [HttpGet]
+        public List<Guid> GetABlockOfGuids([FromUri] int number=100)
+        {
+            List<Guid> guids = new List<Guid>();
+            for(int i=0; i<number; i++)
+            {
+                guids.Add(Guid.NewGuid());
+            }
+            return guids;
+        }
+    }
+}
