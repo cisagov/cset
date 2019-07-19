4c2f23462dc9a8ab9abc3a49096acea6971ad2bf
barry <5023364+inlguy@users.noreply.github.com>
2019-07-19 11:50:07 -0600

    fixing the unit tests

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiTests/Controllers/GuidControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSETWeb_Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWeb_Api.Controllers.Tests
+{
+    [TestClass()]
+    public class GuidControllerTests
+    {
+        [TestMethod()]
+        public void GetABlockOfGuidsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
