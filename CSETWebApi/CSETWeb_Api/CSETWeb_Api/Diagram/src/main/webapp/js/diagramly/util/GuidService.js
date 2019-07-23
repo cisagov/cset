720cdc5efebd8ad34cca5ab0c8a9086a6a49e809
barry <5023364+inlguy@users.noreply.github.com>
2019-07-23 13:14:20 -0600

    guid service working neeed save fix

--- CSETWebApi/CSETWeb_Api/CSETWeb_Api/Diagram/src/main/webapp/js/diagramly/util/GuidService.js
+++ CSETWebApi/CSETWeb_Api/CSETWeb_Api/Diagram/src/main/webapp/js/diagramly/util/GuidService.js
@@ -2,31 +2,32 @@ var guidService = (function(){
     var guidInstance;
     function init(){
         var GuidList = [];
-        function getNextBlock(){
-            return new Promise( resolve => {
-                    atomic(getRootUrl() + "api/guid/requestblock").then(
-                    function (response) {                    
-                        var i;
-                        var arr = response.data;
-                        for(i = 0; i < arr.length; i++) {                           
-                            GuidList.push(arr[i]);                
-                        }
-
-                        resolve(GuidList.pop());
-                    }).catch(function (error){
-                        //TODO:  Figure out how to really handle the errors
-                        console.log('error code', error.status); // xhr.status
-                        console.log('error description', error.statusText); // xhr.status
-                    })                
-            });         
+        function getNextBlock(){            
+          var url = getRootUrl() + "api/guid/requestblock"
+          var request = new XMLHttpRequest();
+          request.open('GET', url, false);  // `false` makes the request synchronous
+          request.send(null);
+          if (request.status === 200) {            
+              var arr = JSON.parse(request.responseText);
+              var i;                       
+              for(i = 0; i < arr.length; i++) {                           
+                  GuidList.push(arr[i]);                
+              }
+          }
+          else{
+              //TODO:  Figure out how to really handle the errors
+              console.log('error code', error.status); // xhr.status
+              console.log('error description', error.statusText); // xhr.status
+          }             
         } 
-        async function nextBlock()
+        function nextBlock()
         {
             var rval; 
             if(GuidList.length<5)
             {
-                const v = await getNextBlock();
-                v.then(x=> console.log(x));
+                getNextBlock();
+                rval = GuidList.pop();
+                
             }
             else{
                 rval = GuidList.pop();
@@ -60,22 +61,4 @@ function getRootUrl() {
         : window.location.protocol + '/' + window.location.host + '/';
 }
 
-var singleA = guidService.getInstance();
-console.log("next guid" + singleA.getNextGuid());
-
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('resolved');
-      }, 2000);
-    });
-  }
-  
-  async function asyncCall() {
-    console.log('calling');
-    var result = await resolveAfter2Seconds();
-    console.log(result);
-    // expected output: 'resolved'
-  }
-  
-  asyncCall();
+//var singleA = guidService.getInstance().getNextGuid();
