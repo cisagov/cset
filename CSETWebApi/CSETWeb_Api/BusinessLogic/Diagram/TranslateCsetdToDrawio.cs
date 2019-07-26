921fa1ee97026950170aef7ece13dca2c7680ec4
barry <5023364+inlguy@users.noreply.github.com>
2019-07-26 09:41:49 -0600

    changed import to provide fresh guid always

--- CSETWebApi/CSETWeb_Api/BusinessLogic/Diagram/TranslateCsetdToDrawio.cs
+++ CSETWebApi/CSETWeb_Api/BusinessLogic/Diagram/TranslateCsetdToDrawio.cs
@@ -239,7 +239,7 @@ namespace CSETWeb_Api.BusinessLogic.Diagram
                 xRoot.AppendChild(xObject);
                 xObject.SetAttribute("id", newID);
                 xObject.SetAttribute("label", ChildValue(component, "c:label/c:label"));
-                xObject.SetAttribute("ComponentGuid", ChildValue(component, "c:questionid"));
+                xObject.SetAttribute("ComponentGuid", new Guid().ToString());
 
 
                 var xComponent = xDrawio.CreateElement("mxCell");
