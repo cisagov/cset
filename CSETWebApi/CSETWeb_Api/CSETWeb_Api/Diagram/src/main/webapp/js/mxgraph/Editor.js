03165cfc29799d9298f66e0b3be7d4f095abdb2a
Randy Woods <randall.woods@inl.gov>
2019-09-30 16:41:33 -0600

    Cleaned up console.log() calls.  Still experimenting with placing red dots on edges.

--- CSETWebApi/CSETWeb_Api/CSETWeb_Api/Diagram/src/main/webapp/js/mxgraph/Editor.js
+++ CSETWebApi/CSETWeb_Api/CSETWeb_Api/Diagram/src/main/webapp/js/mxgraph/Editor.js
@@ -6,7 +6,6 @@
  */
 Editor = function (chromeless, themes, model, graph, editable)
 {
-    console.log("this is the editor starting");
     mxEventSource.call(this);
     this.chromeless = (chromeless != null) ? chromeless : this.chromeless;
     this.initStencilRegistry();
