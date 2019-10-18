7c6ddd9eb69027e978c07485d829b90e14bdb7f8
Jonathan E. Mecham <jonathan.mecham@inl.gov>
2019-10-18 13:05:59 -0600

    SetController  o - Mk SetController  o - Mv ImportController into SetController  o - Mv ExportController into SetController
    AssessmentImportController
     o - Rn UploadToBlob to ImportAssessment
    
    AssessmentExportController
     o - Mk AssessmentExportController
     o - Mk AssessmentExportManager in BusinessLogic
     o - Mv FileDownloadController Download (export) method to AssessmentExportManager and AssessmentExportController

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_Api/Controllers/AssessmentExportController.cs
@@ -0,0 +1,50 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2019 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using CSETWeb_Api.BusinessLogic.AssessmentIO;
+using CSETWeb_Api.BusinessLogic.AssessmentIO.Export;
+using CSETWeb_Api.Helpers;
+using DataLayerCore.Model;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CSETWeb_Api.Controllers
+{
+    public class AssessmentExportController : ApiController
+    {
+        [HttpGet]
+        [Route("api/assessment/export/{token}")]
+        public Task<HttpResponseMessage> ExportAssessment([FromUri] string token)
+        {
+            var tm = new TokenManager(token);
+            var assessmentId = Auth.AssessmentForUser(token);
+
+            var appCode = tm.Payload("scope");
+            var ext = IOHelper.GetFileExtension(appCode);
+
+            using (var context = new CSET_Context())
+            {
+                var filename = $"{assessmentId}{ext}";
+                var assessmentName = context.INFORMATION.Where(x => x.Id == assessmentId).FirstOrDefault()?.Assessment_Name;
+                if (!string.IsNullOrEmpty(assessmentName))
+                    filename = $"{assessmentName}{ext}";
+
+                var export = new AssessmentExportManager(context);
+                using (var stream = export.ArchiveStream(assessmentId))
+                {
+                    var result = Request.CreateResponse(HttpStatusCode.OK, new StreamContent(stream));
+                    result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    result.Content.Headers.Add("content-disposition", $@"attachment; filename=""{filename}""");
+                    return Task.FromResult(result);
+                }
+            }
+        }
+    }
+}
