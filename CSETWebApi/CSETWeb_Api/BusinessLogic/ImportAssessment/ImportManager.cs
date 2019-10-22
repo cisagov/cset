8dc9eca595e4ad408a68a868668f23a5b20f8be4
INLGuy <5023364+inlguy@users.noreply.github.com>
2019-10-22 15:25:00 -0600

    changed the old import to include the diagram

--- CSETWebApi/CSETWeb_Api/BusinessLogic/ImportAssessment/ImportManager.cs
+++ CSETWebApi/CSETWeb_Api/BusinessLogic/ImportAssessment/ImportManager.cs
@@ -8,6 +8,7 @@ using BusinessLogic.Models;
 using CSETWeb_Api.BusinessLogic.Helpers;
 using CSETWeb_Api.BusinessLogic.ImportAssessment;
 using CSETWeb_Api.BusinessLogic.ImportAssessment.Models.Version_9_0_1;
+using CSETWeb_Api.BusinessManagers;
 using DataLayerCore.Model;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -39,8 +40,7 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers
                     ZipArchive zip = new ZipArchive(fs);
                     StreamReader r = new StreamReader(zip.GetEntry("model.json").Open());
                     string jsonObject = r.ReadToEnd();
-
-
+                 
                     // Apply any data updates to older versions
                     ImportUpgradeManager upgrader = new ImportUpgradeManager();
                     jsonObject = upgrader.Upgrade(jsonObject);
@@ -146,9 +146,9 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers
                     string email = context.USERS.Where(x => x.UserId == currentUserId).First().PrimaryEmail;
 
                     Importer import = new Importer();
-                    Tuple<int, Dictionary<int, DOCUMENT_FILE>> t = import.RunImportManualPortion(model, currentUserId, email, context);
+                    Tuple<int, Dictionary<int, DOCUMENT_FILE>> manualImportReturnValue = import.RunImportManualPortion(model, currentUserId, email, context);
                     import.RunImportAutomatic(jsonObject, context);
-                    Dictionary<int, DOCUMENT_FILE> oldIdToNewDocument = t.Item2;
+                    Dictionary<int, DOCUMENT_FILE> oldIdToNewDocument = manualImportReturnValue.Item2;
                     foreach (jDOCUMENT_FILE d in model.jDOCUMENT_FILE)
                     {
                         DOCUMENT_FILE docDB = oldIdToNewDocument[d.Document_Id];
@@ -159,6 +159,16 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers
                         if(entry!=null)
                             SaveFileToDB(entry, docDB);
                         context.SaveChanges();
+                    }
+                    //NOTE THAT THIS ENTRY WILL ONLY COME FROM A OLD .cset file 
+                    //IMPORT
+                    ZipArchiveEntry importLegacyDiagram = zip.GetEntry("Diagram.csetd");
+                    if (importLegacyDiagram != null)
+                    {
+                        StreamReader ldr = new StreamReader(importLegacyDiagram.Open());
+                        string oldXml = ldr.ReadToEnd();
+                        DiagramManager dm = new DiagramManager(context);
+                        dm.ImportOldCSETDFile(oldXml, manualImportReturnValue.Item1);                        
                     }
                 }
             }
