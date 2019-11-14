f4608a42da5629744451bb7e5ae5938f76011d6c
Randy Woods <randall.woods@inl.gov>
2019-11-14 14:02:11 -0700

    Improved warning/overlay placement on edges. Removed newlines embedded in warning text (component names) Added SERIAL_RADIO to connectable devices in Rule1 and Rule3 Broke up rule message text in code for readability

--- CSETWebApi/CSETWeb_Api/BusinessLogic/BusinessManagers/Diagram/analysis/rules/Rule5.cs
+++ CSETWebApi/CSETWeb_Api/BusinessLogic/BusinessManagers/Diagram/analysis/rules/Rule5.cs
@@ -11,7 +11,13 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers.Diagram.analysis.rules
 {
     class Rule5 : AbstractRule, IRuleEvaluate
     {
-        private String rule5 = "The path identified by the components, {0} and {1}, appears to connect on one side to an external network.  A firewall to filter the traffic to and from the external network is recommended to protect the facility's network.  Note that a 'Web' component, 'Vendor' component, or 'Partner' component are all assumed to interface with an external network.  In addition, a modem with a single connection is assumed to allow a connection from outside the facility's network.";
+        private String rule5 = "The path identified by the components, {0} and {1}, appears to " +
+            "connect on one side to an external network.  A firewall to filter the traffic to and " +
+            "from the external network is recommended to protect the facility's network.  " +
+            "Note that a 'Web' component, 'Vendor' component, or 'Partner' component are all assumed " +
+            "to interface with an external network.  In addition, a modem with a single connection " +
+            "is assumed to allow a connection from outside the facility's network.";
+
         private SimplifiedNetwork network;
 
         public Rule5(SimplifiedNetwork simplifiedNetwork)
@@ -32,9 +38,16 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers.Diagram.analysis.rules
 
         private void CheckRule5(NetworkComponent component)
         {
-            HashSet<String> VisitedNodes = new HashSet<string>();
-            VisitedNodes.Add(component.ID);
-            List<NetworkComponent> list = GetNodeEdges(component, new HashSet<int>() { Constants.FIREWALL });
+            HashSet<String> VisitedNodes = new HashSet<string>
+            {
+                component.ID
+            };
+
+            List<NetworkComponent> list = GetNodeEdges(component, new HashSet<int>()
+            {
+                Constants.FIREWALL
+            });
+
             foreach (NetworkComponent info in list)
             {
                 String componentName = "unnamed";
@@ -49,11 +62,9 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers.Diagram.analysis.rules
                     endComponentName = info.ComponentName;
                 }
 
-                String text = String.Format(rule5, componentName, endComponentName);
+                String text = String.Format(rule5, componentName, endComponentName).Replace("\n", " ");
                 SetLineMessage(component, info, text);
             }
-
         }
-
     }
 }
