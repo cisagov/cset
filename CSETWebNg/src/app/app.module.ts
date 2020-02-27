351560a00e9647358c1472101b98066f7bf32b78
Randy Woods <randall.woods@inl.gov>
2020-02-27 13:42:47 -0700

    Fixed white background on reports.  Fixed related questions display on Requirements page.

--- CSETWebNg/src/app/app.module.ts
+++ CSETWebNg/src/app/app.module.ts
@@ -109,6 +109,7 @@ import { InViewComponent } from './helpers/in-view/in-view.component';
 import { JwtInterceptor } from './helpers/jwt.interceptor';
 import { ProgressComponent } from './helpers/progress/progress.component';
 import { SafePipe } from './helpers/safe.pipe';
+import { LinebreakPipe } from './helpers/linebreak.pipe';
 import { ImportComponent } from './import/import.component';
 import { InitialComponent } from './initial/initial.component';
 import { LandingPageComponent } from './initial/landing-page/landing-page.component';
@@ -281,6 +282,7 @@ import { LocalStoreManager } from './services/storage.service';
         OkayComponent,
         FindingsComponent,
         SafePipe,
+        LinebreakPipe,
         StatusCreateComponent,
         ProgressComponent,
         InViewComponent,
