b96c48de5a70d2d080f67dc6e7f0a9861ac4bfb1
Randy <randall.woods@inl.gov>
2020-04-22 11:33:23 -0600

    Starting a place for a module importer

---
+++ CSETWebApi/CSETWeb_Api/BusinessLogic/ModuleIO/ModuleImporter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWeb_Api.BusinessLogic.ModuleIO
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ModuleImporter
+    {
+    }
+}
