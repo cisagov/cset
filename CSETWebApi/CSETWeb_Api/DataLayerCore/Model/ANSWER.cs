4407f9e75faa4644cde7e9df2b88e0e36d4d7950
Randy <randall.woods@inl.gov>
2020-09-23 10:46:02 -0600

    Fixed ANSWER class

--- CSETWebApi/CSETWeb_Api/DataLayerCore/Model/ANSWER.cs
+++ CSETWebApi/CSETWeb_Api/DataLayerCore/Model/ANSWER.cs
@@ -35,6 +35,7 @@ namespace DataLayerCore.Model
         public bool Reviewed { get; set; }
         [StringLength(2048)]
         public string Feedback { get; set; }
+        public bool Is_Maturity { get; set; }
 
         [ForeignKey("Answer_Text")]
         [InverseProperty("ANSWER")]
