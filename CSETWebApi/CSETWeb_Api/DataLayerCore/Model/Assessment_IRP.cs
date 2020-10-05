d400871bf2092d96e938cdeebf9211e08936405a
Zachary Wilson <wilsonzachary634@gmail.com>
2020-10-05 12:25:11 -0600

    filename fixes for compilation

--- CSETWebApi/CSETWeb_Api/DataLayerCore/Model/Assessment_IRP.cs
+++ CSETWebApi/CSETWeb_Api/DataLayerCore/Model/Assessment_IRP.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace DataLayerCore.Model
 {
     public partial class ASSESSMENT_IRP
