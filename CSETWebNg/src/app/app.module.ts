40f172c5826ed9493a2fcf0c98e3ddaa150a5108
Jason Kuipers <kuipers.jason@gmail.com>
2020-10-20 08:40:27 -0600

    fixed path

--- CSETWebNg/src/app/app.module.ts
+++ CSETWebNg/src/app/app.module.ts
@@ -223,7 +223,7 @@ import { MatCmmcComponent } from './assessment/results/mat-cmmc/mat-cmmc.compone
 import { DiagramQuestionsComponent } from './assessment/questions/diagram-questions/diagram-questions.component';
 import { ExecutiveCMMCComponent } from './reports/executive-cmmc/executive-cmmc.component';
 import { SitesummaryCMMCComponent } from './reports/sitesummary-cmmc/sitesummary-cmmc.component';
-import { Maturity1Component } from './assessment/results/maturity1/maturity1.component';
+import { Maturity1Component } from './assessment/results/mat-cmmc/maturity1/maturity1.component';
 import { Maturity2Component } from './assessment/results/mat-cmmc/maturity2/maturity2.component';
 import { Maturity3Component } from './assessment/results/mat-cmmc/maturity3/maturity3.component';
 
