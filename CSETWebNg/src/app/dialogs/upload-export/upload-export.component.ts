95c57ce59bf2f57ca96eb0c09213efd4fe148fc5
Dylan Johnson <42049100+dylanj1752@users.noreply.github.com>
2020-10-27 19:21:24 -0700

    console.log removal

--- CSETWebNg/src/app/dialogs/upload-export/upload-export.component.ts
+++ CSETWebNg/src/app/dialogs/upload-export/upload-export.component.ts
@@ -118,32 +118,23 @@ export class UploadExportComponent implements OnInit {
     // Hide the cancel-button
     this.showCancelButton = false;
     // When all progress-observables are completed...
-    console.log("Prior to for loop")
     let count = 0
     allProgressObservables.forEach(element => {
-      console.log(element)
       element.subscribe(
         succ => {
-          console.log("succ")
-          console.log(succ)
+          // console.log(succ)
         },
         fail => {
-          console.log("failure")
-          console.log(fail)
+          // console.log(fail)
         },
         comp => {
-          console.log("complete")
-          console.log(comp)
           count += 1
-          console.log(count)
           if(count >= allProgressObservables.length){
-            console.log("ALL COUNTED UP")
             this.dialog.close()
           }
         }
       )
     });
-    console.log("After loop")
     
     // forkJoin(allProgressObservables).subscribe(end => {
     //   // ... the dialog can be closed again...
