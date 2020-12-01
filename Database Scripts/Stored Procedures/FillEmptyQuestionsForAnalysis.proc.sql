073035c6192e2e675c89d311b6cce4e42ec84ab0
Randy <randall.woods@inl.gov>
2020-12-01 08:42:45 -0700

    Fixed some quoted bit values, formatting

--- Database Scripts/Stored Procedures/FillEmptyQuestionsForAnalysis.proc.sql
+++ Database Scripts/Stored Procedures/FillEmptyQuestionsForAnalysis.proc.sql
@@ -22,14 +22,14 @@ BEGIN
 	where @Assessment_Id = @Assessment_Id 
 
 	DECLARE @result int;  
-	exec GetApplicationModeDefault @assessment_id,@applicationmode output
+	exec GetApplicationModeDefault @assessment_id, @applicationmode output
 	if(@ApplicationMode = 'Questions Based')
 		BEGIN
 			BEGIN TRANSACTION;  
 		
 			EXEC @result = sp_getapplock @DbPrincipal = 'dbo', @Resource = '[Answer]', @LockMode = 'Exclusive';  
-				INSERT INTO [dbo].[ANSWER]  ([Is_Requirement],[Question_Or_Requirement_Id],[Answer_Text],[Is_Component],[Is_Framework],[Is_Maturity],[Assessment_Id])     
-			select Is_Requirement=0,s.Question_id,Answer_Text = 'U', Is_Component='0',Is_Framework=0, Is_Maturity=0, Assessment_Id =@Assessment_Id
+				INSERT INTO [dbo].[ANSWER]  ([Is_Requirement], [Question_Or_Requirement_Id], [Answer_Text], [Is_Component], [Is_Framework], [Is_Maturity], [Assessment_Id])     
+			select Is_Requirement = 0, s.Question_id, Answer_Text = 'U', Is_Component = 0, Is_Framework = 0, Is_Maturity = 0, Assessment_Id = @Assessment_Id
 				from (select distinct s.Question_Id from NEW_QUESTION_SETS s 
 					join AVAILABLE_STANDARDS v on s.Set_Name = v.Set_Name 								
 					join NEW_QUESTION_LEVELS l on s.New_Question_Set_Id = l.new_question_set_id
@@ -52,11 +52,11 @@ BEGIN
 	BEGIN
 		BEGIN TRANSACTION;  		
 		EXEC @result = sp_getapplock @DbPrincipal = 'dbo', @Resource = '[Answer]', @LockMode = 'Exclusive';  
-		INSERT INTO [dbo].[ANSWER]  ([Is_Requirement],[Question_Or_Requirement_Id]
-           ,[Answer_Text],[Is_Component],[Is_Framework],[Is_Maturity],[Assessment_Id])     
-		select distinct Is_Requirement=1,s.Requirement_Id, Answer_Text = 'U', Is_Component='0',Is_Framework=0,Is_Maturity=0,av.Assessment_Id 
+		INSERT INTO [dbo].[ANSWER]  ([Is_Requirement], [Question_Or_Requirement_Id], 
+           [Answer_Text], [Is_Component], [Is_Framework], [Is_Maturity], [Assessment_Id])     
+		select distinct Is_Requirement = 1, s.Requirement_Id, Answer_Text = 'U', Is_Component = 0, Is_Framework = 0, Is_Maturity = 0, av.Assessment_Id 
 			from requirement_sets s 
-			join AVAILABLE_STANDARDS av on s.Set_Name=av.Set_Name
+			join AVAILABLE_STANDARDS av on s.Set_Name = av.Set_Name
 			join REQUIREMENT_LEVELS rl on s.Requirement_Id = rl.Requirement_Id
 			left join (select * from ANSWER where Assessment_Id = @Assessment_Id and Is_Requirement = 1) a on s.Requirement_Id = a.Question_Or_Requirement_Id
 		where av.Selected = 1 and av.Assessment_Id = @Assessment_Id and a.Question_Or_Requirement_Id is null and rl.Standard_Level = @SALevel and rl.Level_Type = 'NST'
@@ -68,9 +68,10 @@ BEGIN
 		BEGIN  
 			EXEC sp_releaseapplock @DbPrincipal = 'dbo', @Resource = '[Answer]'; 	
 			COMMIT TRANSACTION;  
-		END;
+		END;  
 		
 		EXEC usp_BuildCatNumbers @assessment_id
 	END   
 	
 END
+
