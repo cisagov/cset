4a129d5a6cd01e59ba92ad533d962957b485fec5
Peter <peter.mercado255@gmail.com>
2020-12-16 13:23:14 -0500

    adding moc base for stubing data

--- CSETWebNg/src/app/reports/acet-answeredquestions/acet-answeredquestions.component.ts
+++ CSETWebNg/src/app/reports/acet-answeredquestions/acet-answeredquestions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ReportService } from '../../services/report.service';
 
 
 @Component({
@@ -9,14 +10,32 @@ import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class AcetAnsweredquestionsComponent implements OnInit {
   response: any = null;
+  mockDataAcetAnsweredquestions: any = {
+    "information": {
+      "Assessment_Name": "Manhattan Assessment",
+      "Assessment_Date": "2020-09-19",
+      "Assessor_Name": "Michael Jones"
+    }
+  };
 
   constructor(
+    public reportSvc: ReportService,
     private titleService: Title,
 
   ) { }
 
   ngOnInit(): void {
     this.titleService.setTitle("Answered Questions Report - ASET");
+
+    // Here get report data
+    // ToDo: Uncomment this and connect backend for report data
+    //this.reportSvc.getReport('<ACET Endpoint>').subscribe(
+    //  (r: any) => {
+    //    this.response = r;
+    //  },
+    //  error => console.log('Executive report load Error: ' + (<Error>error).message)
+    //);
+    this.response = this.mockDataAcetAnsweredquestions;
 
 
   }
--- CSETWebNg/src/app/reports/acet-commentsmarked/acet-commentsmarked.component.ts
+++ CSETWebNg/src/app/reports/acet-commentsmarked/acet-commentsmarked.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ReportService } from '../../services/report.service';
 
 
 @Component({
@@ -9,15 +10,31 @@ import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class AcetCommentsmarkedComponent implements OnInit {
   response: any = null;
+  mockDataAcetCommentsmarked: any = {
+    "information": {
+      "Assessment_Name": "Manhattan Assessment",
+      "Assessment_Date": "2020-09-19",
+      "Assessor_Name": "Michael Jones"
+    }
+  };
 
   constructor(
+    public reportSvc: ReportService,
     private titleService: Title,
 
   ) { }
 
   ngOnInit(): void {
     this.titleService.setTitle("Comments Marked Report - ASET");
-
+    // Here get report data
+    // ToDo: Uncomment this and connect backend for report data
+    //this.reportSvc.getReport('<ACET Endpoint>').subscribe(
+    //  (r: any) => {
+    //    this.response = r;
+    //  },
+    //  error => console.log('Executive report load Error: ' + (<Error>error).message)
+    //);
+    this.response = this.mockDataAcetCommentsmarked;
 
   }
 
