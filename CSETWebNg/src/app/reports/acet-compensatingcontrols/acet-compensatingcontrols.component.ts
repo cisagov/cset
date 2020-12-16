8a979e8cd378be021390ddafabe38d91f64a09f1
Peter <peter.mercado255@gmail.com>
2020-12-16 13:23:14 -0500

    adding moc base for stubing data

--- CSETWebNg/src/app/reports/acet-compensatingcontrols/acet-compensatingcontrols.component.ts
+++ CSETWebNg/src/app/reports/acet-compensatingcontrols/acet-compensatingcontrols.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ReportService } from '../../services/report.service';
 
 
 @Component({
@@ -9,8 +10,16 @@ import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class AcetCompensatingcontrolsComponent implements OnInit {
   response: any = null;
+  mockDataAcetCompensatingcontrols: any = {
+    "information": {
+      "Assessment_Name": "Manhattan Assessment",
+      "Assessment_Date": "2020-09-19",
+      "Assessor_Name": "Michael Jones"
+    }
+  };
 
   constructor(
+    public reportSvc: ReportService,
     private titleService: Title,
 
   ) { }
@@ -18,7 +27,15 @@ export class AcetCompensatingcontrolsComponent implements OnInit {
   ngOnInit(): void {
     this.titleService.setTitle("Compensating Controls Report - ASET");
 
-
+    // Here get report data
+    // ToDo: Uncomment this and connect backend for report data
+    //this.reportSvc.getReport('<ACET Endpoint>').subscribe(
+    //  (r: any) => {
+    //    this.response = r;
+    //  },
+    //  error => console.log('Executive report load Error: ' + (<Error>error).message)
+    //);
+    this.response = this.mockDataAcetCompensatingcontrols;
   }
 
 }
