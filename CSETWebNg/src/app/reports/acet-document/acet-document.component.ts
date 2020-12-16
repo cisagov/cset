c0483e64991fad71bf59bac15687d2d61e1312aa
Peter <peter.mercado255@gmail.com>
2020-12-16 15:13:46 -0500

    adding document report infu

---
+++ CSETWebNg/src/app/reports/acet-document/acet-document.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
+import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ReportService } from '../../services/report.service';
+
+
+@Component({
+  selector: 'app-acet-document',
+  templateUrl: './acet-document.component.html',
+  styleUrls: ['./acet-document.component.scss']
+})
+export class AcetDocumentComponent implements OnInit {
+  response: any = null;
+  mockDataAcetDocument: any = {
+    "information": {
+      "Assessment_Name": "Manhattan Assessment",
+      "Assessment_Date": "2020-09-19",
+      "Assessor_Name": "Michael Jones"
+    }
+  };
+
+  constructor(
+    public reportSvc: ReportService,
+    private titleService: Title,
+
+  ) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle("Documents Report - ASET");
+    // Here get report data
+    // ToDo: Uncomment this and connect backend for report data
+    //this.reportSvc.getReport('<ACET Endpoint>').subscribe(
+    //  (r: any) => {
+    //    this.response = r;
+    //  },
+    //  error => console.log('Executive report load Error: ' + (<Error>error).message)
+    //);
+    this.response = this.mockDataAcetDocument;
+  }
+
+}
