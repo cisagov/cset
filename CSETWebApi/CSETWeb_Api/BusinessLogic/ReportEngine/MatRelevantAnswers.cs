df917867b344b30262748171a447d34b872830dd
Peter <peter.mercado255@gmail.com>
2021-01-26 13:21:44 -0500

    adding in answers into reponce and table for UI

--- CSETWebApi/CSETWeb_Api/BusinessLogic/ReportEngine/MatRelevantAnswers.cs
+++ CSETWebApi/CSETWeb_Api/BusinessLogic/ReportEngine/MatRelevantAnswers.cs
@@ -55,5 +55,6 @@ namespace CSETWeb_Api.BusinessLogic.ReportEngine
         public string QuestionText { get; set; }
         public string MaturityLevel { get; set; }
         public string Comments { get; set; }
+        public string Answer_Text { get; set; }
     }
 }
