37665363c0cbdda243adc6f24fbbc2d73f3156e5
Randy <randall.woods@inl.gov>
2021-03-23 10:14:45 -0600

    Fixed SASS accessibility for CMMC results pages

--- CSETWebNg/src/app/assessment/results/mat-cmmc/cmmc-level-results/cmmc-level-results.component.ts
+++ CSETWebNg/src/app/assessment/results/mat-cmmc/cmmc-level-results/cmmc-level-results.component.ts
@@ -29,6 +29,7 @@ import { MaturityService } from '../../../../../app/services/maturity.service';
 @Component({
   selector: 'app-cmmc-level-results',
   templateUrl: './cmmc-level-results.component.html',  
+  styleUrls: ['../../../../../sass/cmmc-results.scss'],
   // tslint:disable-next-line:use-host-property-decorator
   host: { class: 'd-flex flex-column flex-11a' },
 })
