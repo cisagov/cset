6591648f2c9d4c14485595e95aed59a83d056108
Randy <randall.woods@inl.gov>
2021-04-12 16:11:23 -0600

    Use floor() function to keep MIL levels from partial credit

--- CSETWebNg/src/app/reports/edm/edm-summary-results/edm-summary-results.component.ts
+++ CSETWebNg/src/app/reports/edm/edm-summary-results/edm-summary-results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { floor } from 'lodash';
 import { MaturityService } from '../../../services/maturity.service';
 
 
@@ -11,7 +12,7 @@ export class EdmSummaryResultsComponent implements OnInit {
 
   barValue = 0;
   blueBarValue = 0;
-  flat_data : any[] = null;
+  flat_data: any[] = null;
   flat_data_blue: any[] = null;
   edmPercentData: any;
   indicatorData: any;
@@ -22,7 +23,7 @@ export class EdmSummaryResultsComponent implements OnInit {
 
   ngOnInit(): void {
     this.maturitySvc.getEdmPercentScores().subscribe(
-      (success:any) => {
+      (success: any) => {
         this.edmPercentData = success.summary;
         this.indicatorData = success.partial;
         this.getMaturityIndicatorLevel();
@@ -38,13 +39,13 @@ export class EdmSummaryResultsComponent implements OnInit {
     var nextChild = this.edmPercentData.TopLevelChild;
     this.flat_data = []
     this.flat_data.push(this.getMILBasicData(this.edmPercentData))
-    while(nextChild != null){
+    while (nextChild != null) {
       this.flat_data.unshift(this.getMILBasicData(nextChild))
-      nextChild = nextChild.TopLevelChild;      
+      nextChild = nextChild.TopLevelChild;
     }
     this.flat_data.every(level => {
-      this.barValue += level.PercentageCountRight / level.PercentageTotalCount 
-      if(level.PercentageCountRight != level.PercentageTotalCount){
+      this.barValue += level.PercentageCountRight / level.PercentageTotalCount
+      if (level.PercentageCountRight != level.PercentageTotalCount) {
         return false
       }
       return true
@@ -54,59 +55,66 @@ export class EdmSummaryResultsComponent implements OnInit {
     var nextChild = this.indicatorData.TopLevelChild;
     this.flat_data_blue = []
     this.flat_data_blue.push(this.getBlueMILBasicData(this.indicatorData))
-    while(nextChild != null){
+    while (nextChild != null) {
       this.flat_data_blue.unshift(this.getBlueMILBasicData(nextChild))
-      nextChild = nextChild.TopLevelChild;      
+      nextChild = nextChild.TopLevelChild;
     }
     this.flat_data_blue.every(level => {
-      this.blueBarValue += level.PercentageCountRight / level.PercentageTotalCount 
-      if(level.PercentageCountRight != level.PercentageTotalCount){
+      this.blueBarValue += level.PercentageCountRight / level.PercentageTotalCount
+      if (level.PercentageCountRight != level.PercentageTotalCount) {
         return false
       }
       return true
     });
   }
 
-  getBarWidth(input){
-      let width = input.PercentageCountRight / input.PercentageTotalCount * 100
-      return {
-        'width': `${width}%`
-      }    
-  }
-
-  getMILBasicData(input){
+  getBarWidth(input) {
+    let width = input.PercentageCountRight / input.PercentageTotalCount * 100
     return {
-      "Title_Id" : input.Title_Id,
-      "Children": input.Children,
-      "PercentageCountRight" : input.PercentageCountRight,
-      "PercentageTotalCount" : input.PercentageTotalCount
+      'width': `${width}%`
     }
   }
 
-  getBlueMILBasicData(input){
+  getMILBasicData(input) {
     return {
-      "Title_Id" : input.Title_Id,
+      "Title_Id": input.Title_Id,
       "Children": input.Children,
-      "PercentageCountRight" : input.Score,
-      "PercentageTotalCount" : input.totalCount
+      "PercentageCountRight": input.PercentageCountRight,
+      "PercentageTotalCount": input.PercentageTotalCount
+    }
+  }
+
+  getBlueMILBasicData(input) {
+    return {
+      "Title_Id": input.Title_Id,
+      "Children": input.Children,
+      "PercentageCountRight": input.Score,
+      "PercentageTotalCount": input.totalCount
     }
   }
 
 
-  getBlueBarWidth(input){
+  /**
+   * Returns a CSS width value to match the corresponding
+   * domain tick legend in the X axis.
+   * @param input 
+   * @returns 
+   */
+  getBlueBarWidth(input) {
     let width = 0;
-    if(input < 0 || input > 5){
+    if (input < 0 || input > 5) {
       // console.log("BLUE BAR WIDTH VALUE OUTSIDE OF 0-5 RANGE")
     }
-    else if(input < 1){
+    else if (input < 1) {
       width = (input * .5) * 100;
     } else {
-      width = 50 + (input - 1) * 12.5
+      width = 50 + (floor(input) - 1) * 12.5;
     }
-    let retVal = {
-      'width':`${width}%`
-    }
-    return retVal
+
+    const widthStyle = {
+      'width': `${width}%`
+    };
+    return widthStyle;
   }
 
 }
