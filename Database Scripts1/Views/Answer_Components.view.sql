e4fa55805e01455cd88a2717a2d2b4343b4595b6
Randy <randall.woods@inl.gov>
2021-04-12 16:18:58 -0600

    Removing temporary folder that was accidentally committed

--- Database Scripts1/Views/Answer_Components.view.sql
+++
@@ -1,8 +0,0 @@
-
-
-CREATE VIEW [dbo].[Answer_Components]
-AS
-SELECT        Answer_Id, Assessment_Id, Mark_For_Review, Comment, Alternate_Justification, Is_Requirement, Question_Or_Requirement_Id, Question_Number, Answer_Text, Component_Guid, Is_Component, Is_Framework,
-                          Reviewed, FeedBack
-FROM            dbo.ANSWER
-WHERE        (Is_Requirement = 0) AND (Is_Component = 1)
