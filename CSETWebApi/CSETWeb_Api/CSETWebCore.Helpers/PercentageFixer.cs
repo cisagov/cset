69d755defe254466443773165b8219be4bd8caea
Jason Kuipers <kuipers.jason@gmail.com>
2021-05-12 11:07:16 -0600

    add AnalysisController to controllers

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Helpers/PercentageFixer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSETWebCore.Helpers
+{
+    public class PercentageFixer
+    {
+        List<int?> l = new List<int?>();
+
+        public PercentageFixer(int? y, int? n, int? na, int? a, int? u)
+        {
+            l.Add(y);
+            l.Add(n);
+            l.Add(na);
+            l.Add(a);
+            l.Add(u);
+
+            while (l.Sum() > 100)
+            {
+                var m = l.FindIndex(x => x == l.Max());
+                l[m]--;
+            }
+            while (l.Sum() < 100)
+            {
+                var m = l.FindIndex(x => x == l.Max());
+                l[m]++;
+            }
+        }
+
+        public int? Y { get { return l[0]; } }
+        public int? N { get { return l[1]; } }
+        public int? NA { get { return l[2]; } }
+        public int? A { get { return l[3]; } }
+        public int? U { get { return l[4]; } }
+    }
+}
---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Question/FeedbackQuestion.cs
@@ -0,0 +1,11 @@
+﻿namespace CSETWebCore.Model.Question
+{
+    public class FeedbackQuestion
+    {
+        public string QuestionText { get; set; }
+        public int QuestionID { get; set; }
+        public int AnswerID { get; set; }
+        public string Feedback { get; set; }
+        public string Mode { get; set; }
+    }
+}
