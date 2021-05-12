0b1f5250a57cec38fa035421319020c4691818be
Jason Kuipers <kuipers.jason@gmail.com>
2021-05-12 09:33:58 -0600

    add acet filter controller

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/ACETFilterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ACETFilterController : ControllerBase
+    {
+    }
+}
