f9913647241d2257ef5ce1145a5eee675ade7662
Jason Kuipers <kuipers.jason@gmail.com>
2021-05-17 11:20:28 -0600

    add Demographic Controller to controllers

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Demographic/Industry.cs
@@ -0,0 +1,10 @@
+﻿namespace CSETWebCore.Model.Demographic
+{
+    public class Industry
+    {
+        public int SectorId { get; set; }
+        public int IndustryId { get; set; }
+        public string IndustryName { get; set; }
+        public string Name { get { return IndustryName; } }
+    }
+}
---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Demographic/Sector.cs
@@ -0,0 +1,13 @@
+﻿namespace CSETWebCore.Model.Demographic
+{
+    public class Sector
+    {
+        public int SectorId { get; set; }
+        public string SectorName { get; set; }
+
+        public string Name
+        {
+            get { return SectorName; }
+        }
+    }
+}
