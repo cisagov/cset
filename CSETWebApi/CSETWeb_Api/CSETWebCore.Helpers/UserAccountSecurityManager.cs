fcd12f2a0b230d5c4b8528bc25d8e63288ca54af
Randy <randall.woods@inl.gov>
2021-05-18 11:51:47 -0600

    Refactoring reset password controller

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Helpers/UserAccountSecurityManager.cs
@@ -0,0 +1,178 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2021 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using CSETWebCore.DataLayer;
+using CSETWebCore.Model.Contact;
+using CSETWebCore.Model.Password;
+using CSETWebCore.Model.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSETWebCore.Interfaces.User;
+using CSETWebCore.Interfaces.Notification;
+
+namespace CSETWebCore.Helpers
+{
+    public class UserAccountSecurityManager
+    {
+        private readonly CSETContext _context;
+        private readonly IUserBusiness _userBusiness;
+        private readonly INotificationBusiness _notificationBusiness;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public UserAccountSecurityManager(CSETContext context, IUserBusiness userBusiness, INotificationBusiness notificationBusiness)
+        {
+            _context = context;
+            _userBusiness = userBusiness;
+            _notificationBusiness = notificationBusiness;
+        }
+
+
+        /// <summary>
+        /// Creates a new user and sends them an email containing a temporary password.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public bool CreateUserSendEmail(CreateUser info)
+        {
+            try
+            {
+                // Create the USER and USER_DETAIL_INFORMATION records for this new user
+                var ud = new UserDetail()
+                {
+                    UserId = info.UserId,
+                    Email = info.PrimaryEmail,
+                    FirstName = info.FirstName,
+                    LastName = info.LastName
+                };
+                UserCreateResponse userCreateResponse = _userBusiness.CreateUser(ud);
+
+                _context.USER_SECURITY_QUESTIONS.Add(new USER_SECURITY_QUESTIONS()
+                {
+                    UserId = userCreateResponse.UserId,
+                    SecurityQuestion1 = info.SecurityQuestion1,
+                    SecurityQuestion2 = info.SecurityQuestion2,
+                    SecurityAnswer1 = info.SecurityAnswer1,
+                    SecurityAnswer2 = info.SecurityAnswer2
+                });
+
+                _context.SaveChanges();
+
+                // Send the new temp password to the user
+                _notificationBusiness.SendPasswordEmail(userCreateResponse.PrimaryEmail, info.FirstName, info.LastName, userCreateResponse.TemporaryPassword);
+
+                return true;
+            }
+            catch (ApplicationException app)
+            {
+                throw new Exception("This account already exists. Please request a new password using the Forgot Password link if you have forgotten your password.", app);
+            }
+            catch (DbUpdateException due)
+            {
+                throw new Exception("This account already exists. Please request a new password using the Forgot Password link if you have forgotten your password.", due);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="changePass"></param>
+        /// <returns></returns>
+        public bool ChangePassword(ChangePassword changePass)
+        {
+            try
+            {
+                var user = _context.USERS.Where(x => x.PrimaryEmail == changePass.PrimaryEmail).FirstOrDefault();
+                var info = _context.USER_DETAIL_INFORMATION.Where(x => x.PrimaryEmail == user.PrimaryEmail).FirstOrDefault();
+
+                new PasswordHash().HashPassword(changePass.NewPassword, out string hash, out string salt);
+                user.Password = hash;
+                user.Salt = salt;
+                user.PasswordResetRequired = false;
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="subject"></param>
+        /// <param name="appCode"></param>
+        /// <returns></returns>
+        public bool ResetPassword(string email, string subject, string appCode)
+        {
+            /**
+             * get the user and make sure they exist
+             * set the reset password flag
+             * generate the new password
+             * send the email 
+             * return 
+             */
+            try
+            {
+                var user = _context.USERS.Where(x => x.PrimaryEmail == email).FirstOrDefault();
+
+                user.PasswordResetRequired = true;
+                // TODO RKW:  String password = System.Security..Security.Membership.GeneratePassword(10, 1);
+                var password = "XXXXX";  // TODO RKW:
+#if DEBUG
+                // set the password back to 'abc' for consistency/predictability when debugging
+                if (user.PrimaryEmail == "a@b.com")
+                    password = "abc";
+#endif
+                string hash;
+                string salt;
+                new PasswordHash().HashPassword(password, out hash, out salt);
+                user.Password = hash;
+                user.Salt = salt;
+
+                _notificationBusiness.SendPasswordResetEmail(user.PrimaryEmail, user.FirstName, user.LastName, password, subject);
+
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                // CsetLogManager.Instance.LogErrorMessage(e.ToString());
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Returns a list of potential security questions.
+        /// </summary>
+        /// <returns></returns>
+        public List<PotentialQuestions> GetSecurityQuestionList()
+        {
+            List<PotentialQuestions> questions =
+                (from a in _context.SECURITY_QUESTION
+                 select new PotentialQuestions()
+                 {
+                     SecurityQuestion = a.SecurityQuestion,
+                     SecurityQuestionId = a.SecurityQuestionId
+                 }).ToList<PotentialQuestions>();
+            return questions;
+        }
+    }
+}
---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Password/Password.cs
@@ -0,0 +1,52 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2021 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+
+
+namespace CSETWebCore.Model.Password
+{
+    public class ChangePassword
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string PrimaryEmail { get; set; }
+    }
+
+    public class SecurityQuestions
+    {
+        public string SecurityQuestion1 { get; set; }
+        public string SecurityQuestion2 { get; set; }
+    }
+
+    public class PotentialQuestions
+    {
+        public int SecurityQuestionId { get; set; }
+        public string SecurityQuestion { get; set; }
+        public string Answer { get; set; }
+    }
+
+    public class SecurityQuestionAnswer
+    {
+        public string PrimaryEmail { get; set; }
+
+        public string QuestionText { get; set; }
+        public string AnswerText { get; set; }
+
+        /// <summary>
+        /// This must be explicitly sent by the front end because
+        /// the user is not logged in, so there's no JWT at this point.
+        /// </summary>
+        public string AppCode { get; set; }
+    }
+
+    public class UserStatus
+    {
+        public string PrimaryEmail { get; set; }
+        public int UserId { get; set; }
+        public bool PasswordResetRequired { get; set; }
+    }
+}
+
