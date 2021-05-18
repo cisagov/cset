4e91114b7d16e308bcf3c5fd07012b6ee59c45f0
Jason Kuipers <kuipers.jason@gmail.com>
2021-05-18 15:39:02 -0600

    clean up references

--- CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/ReferenceDocumentsController.cs
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/ReferenceDocumentsController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Net;
