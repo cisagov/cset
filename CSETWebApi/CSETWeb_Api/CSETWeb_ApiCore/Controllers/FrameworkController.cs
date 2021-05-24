2af2f8cc18ac484a5f6baead97fb05894ea8531a
Jason Kuipers <kuipers.jason@gmail.com>
2021-05-24 15:52:52 -0600

    add framwork controller to controllers

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/FrameworkController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CSETWebCore.Interfaces.Framework;
+using CSETWebCore.Interfaces.Helpers;
+using CSETWebCore.Model.Framework;
+
+namespace CSETWebCore.Api.Controllers
+{
+    [ApiController]
+    public class FrameworkController : ControllerBase
+    {
+        private readonly IFrameworkBusiness _framework;
+        private readonly ITokenManager _token;
+
+        public FrameworkController(IFrameworkBusiness framework, ITokenManager token)
+        {
+            _framework = framework;
+            _token = token;
+        }
+
+        /// <summary>
+        /// Returns a list of all displayable cybersecurity framework tiers.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/frameworks")]
+        public FrameworkResponse GetFrameworks()
+        {
+            int assessmentId = _token.AssessmentForUser();
+            return _framework.GetFrameworks(assessmentId);
+        }
+
+
+        /// <summary>
+        /// Persists the selected tier value to the database.
+        /// </summary>
+        [HttpPost]
+        [Route("api/framework")]
+        public void PersistSelectedTierAnswer(TierSelection tier)
+        {
+            // In case nothing is sent, bail out gracefully
+            if (tier == null)
+            {
+                return;
+            }
+
+            int assessmentId = _token.AssessmentForUser();
+            _framework.PersistSelectedTierAnswer(assessmentId, tier);
+        }
+    }
+}
