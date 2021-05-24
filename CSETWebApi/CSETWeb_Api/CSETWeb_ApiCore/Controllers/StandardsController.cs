836eee1afd4ef1caa6b0a372e1512b0ece4d87c8
Jason Kuipers <kuipers.jason@gmail.com>
2021-05-24 15:19:32 -0600

    clean up standards controller

--- CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/StandardsController.cs
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/StandardsController.cs
@@ -12,43 +12,30 @@ using CSETWebCore.Interfaces.Question;
 using CSETWebCore.Interfaces.Demographic;
 using CSETWebCore.Interfaces.Helpers;
 using CSETWebCore.Interfaces.AdminTab;
-using Microsoft.AspNetCore.Authorization;
 using CSETWebCore.DataLayer;
-using CSETWebCore.Business.Acet;
-using CSETWebCore.Business.Reports;
 using CSETWebCore.Interfaces.Reports;
-using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CSETWebCore.Model.Question;
 
 namespace CSETWebCore.Api.Controllers
 {
     public class StandardsController : ControllerBase
     {
-        private readonly IUserAuthentication _userAuthentication;
         private readonly ITokenManager _tokenManager;
         private readonly CSETContext _context;
         private readonly IAssessmentUtil _assessmentUtil;
-        private readonly IAdminTabBusiness _adminTabBusiness;
-        private readonly IReportsDataBusiness _reports;
         private IQuestionRequirementManager _questionRequirement;
         private IDemographicBusiness _demographicBusiness;
         private readonly StandardsBusiness _standards;
 
 
-        public StandardsController(IUserAuthentication userAuthentication, ITokenManager tokenManager, CSETContext context,
-             IAssessmentUtil assessmentUtil, IAdminTabBusiness adminTabBusiness, IReportsDataBusiness reports, IQuestionRequirementManager questionRequirement,
+        public StandardsController(ITokenManager tokenManager, CSETContext context,
+             IAssessmentUtil assessmentUtil, IQuestionRequirementManager questionRequirement,
             IDemographicBusiness demographicBusiness)
         {
-            _userAuthentication = userAuthentication;
             _tokenManager = tokenManager;
             _context = context;
             _assessmentUtil = assessmentUtil;
-            _adminTabBusiness = adminTabBusiness;
-            _reports = reports;
             _questionRequirement = questionRequirement;
             _demographicBusiness = demographicBusiness;
             _standards = new StandardsBusiness(_context, _assessmentUtil, _questionRequirement, _tokenManager,
