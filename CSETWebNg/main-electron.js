dcc0e358f14b4fe30587c717f0d262379a9af404
Marcus <mbgoec2000@gmail.com>
2021-06-07 08:06:34 -0700

    Change window URL

--- CSETWebNg/main-electron.js
+++ CSETWebNg/main-electron.js
@@ -15,14 +15,17 @@ function createWindow () {
   Menu.setApplicationMenu(null)
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'dist/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  // win.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'dist/index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
+
+  win.loadURL('http://localhost:4200/')
 
   // Open the DevTools.
   win.webContents.openDevTools()
+
 
   // Emitted when the window is closed.
   win.on('closed', () => {
