76fd03445a5c2f9191e0b90be3c45cc8dabffccd
Randy <randall.woods@inl.gov>
2021-06-11 14:40:00 -0600

    Adding stubs for RRA results pages

---
+++ CSETWebNg/src/app/assessment/prepare/maturity/tutorial-rra/tutorial-rra.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-rra',
+  templateUrl: './tutorial-rra.component.html'
+})
+export class TutorialRraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
