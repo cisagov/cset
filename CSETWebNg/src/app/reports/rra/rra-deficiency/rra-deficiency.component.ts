7b682d946b615a2cf2254cd0c39961aefac1aac0
Randy <randall.woods@inl.gov>
2021-06-22 15:32:38 -0600

    Sort deficiency report by level, add ranked goal chart

--- CSETWebNg/src/app/reports/rra/rra-deficiency/rra-deficiency.component.ts
+++ CSETWebNg/src/app/reports/rra/rra-deficiency/rra-deficiency.component.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '../../../services/config.service';
 import { MaturityService } from '../../../services/maturity.service';
 import { ReportAnalysisService } from '../../../services/report-analysis.service';
 import { ReportService } from '../../../services/report.service';
+import { RraDataService } from '../../../services/rra-data.service';
 
 @Component({
   selector: 'app-rra-deficiency',
@@ -14,46 +15,110 @@ export class RraDeficiencyComponent implements OnInit {
 
   response: any;
 
+  colorSchemeRed = { domain: ['#9c0006'] };
+  xAxisTicks = [0, 25, 50, 75, 100];
+  topRankedGoals = [];
+
   constructor(
     public analysisSvc: ReportAnalysisService,
-      public reportSvc: ReportService,
-      public configSvc: ConfigService,
-      private titleService: Title,
-      public maturitySvc: MaturityService
-    ) { }
-  
-    ngOnInit() {
-      this.titleService.setTitle("Deficiency Report - RRA");
-  
-      this.maturitySvc.getMaturityDeficiency("RRA").subscribe(
-        (r: any) => {
-          this.response = r;          
-          //run through the deficiencies add them to three groups
-          //then dump out three tables.  DeficiencesList
+    public reportSvc: ReportService,
+    public configSvc: ConfigService,
+    private titleService: Title,
+    public maturitySvc: MaturityService,
+    public rraDataSvc: RraDataService
+  ) { }
 
-        },
-        error => console.log('Deficiency Report Error: ' + (<Error>error).message)
-      );
+  ngOnInit() {
+    this.titleService.setTitle("Deficiency Report - RRA");
+
+    this.maturitySvc.getMaturityDeficiency("RRA").subscribe(
+      (r: any) => {
+        this.response = r;
+
+        // sort the deficiencies by maturity level
+        let basicList = [];
+        let intermediateList = [];
+        let advancedList = [];
+
+        this.response.DeficiencesList.forEach(element => {
+          let level = this.getStringLevel(element.Mat.Maturity_Level);
+          switch (level) {
+            case 'Basic':
+              basicList.push(element);
+              break;
+            case 'Intermediate':
+              intermediateList.push(element);
+              break;
+            case 'Advanced':
+              advancedList.push(element);
+              break;
+          }
+        });
+
+        this.response.DeficiencesList = basicList.concat(intermediateList).concat(advancedList);
+      },
+      error => console.log('Deficiency Report Error: ' + (<Error>error).message)
+    );
+
+
+    this.rraDataSvc.getRRADetail().subscribe((r: any) => {
+      this.createTopRankedGoals(r);
+    },
+      error => console.log('RRA detail load Error: ' + (<Error>error).message)
+    );
+  }
+
+
+  previous = 0;
+  shouldDisplay(next) {
+    if (next == this.previous) {
+      return false;
     }
-
-    previous = 0; 
-    shouldDisplay(next){
-      if(next==this.previous){
-        return false;
-      }
-      else{
-        this.previous = next;
-        return true;
-      }
+    else {
+      this.previous = next;
+      return true;
     }
+  }
 
-    getStringLevel(levelNumber: number){
-      //this should come from db eventually.
-      var levelsList = {
-        11:"Basic",
-        12:"Intermediate",
-        13:"Advanced"
+  /**
+   * 
+   * @param levelNumber 
+   * @returns 
+   */
+  getStringLevel(levelNumber: number) {
+    //this should come from db eventually.
+    var levelsList = {
+      11: "Basic",
+      12: "Intermediate",
+      13: "Advanced"
+    };
+    return levelsList[levelNumber];
+  }
+
+  /**
+   * Build a chart sorting the least-compliant goals to the top.
+   * @param r 
+   */
+  createTopRankedGoals(r: any) {
+    let goalList = [];
+    r.RRASummaryByGoalOverall.forEach(element => {
+      var goal = {
+        name: element.Title, value: (100 - Math.round(element.Percent))
       };
-      return levelsList[levelNumber];
-    }
+      goalList.push(goal);
+    });
+
+    goalList.sort((a, b) => { return b.value - a.value; });
+
+    this.topRankedGoals = goalList;
+  }
+
+  /**
+   * 
+   * @param x 
+   * @returns 
+   */
+  formatPercent(x: any) {
+    return x + '%';
+  }
 }
