83becb2d8d5471da227428137508228042d61941
Jason Kuipers <kuipers.jason@gmail.com>
2021-06-23 16:51:08 -0600

    hide buttons for summary in rra

--- CSETWebNg/src/app/assessment/results/mat-rra/rra-summary-all/rra-summary-all.component.ts
+++ CSETWebNg/src/app/assessment/results/mat-rra/rra-summary-all/rra-summary-all.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RraSummaryAllComponent implements OnInit {
   @Input() title = "RRA Summary";
+  @Input() showNav = true;
   constructor() { }
 
   ngOnInit(): void {
