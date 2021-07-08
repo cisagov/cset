665eb6460c02e42248820bbf6f2cf763d668d76d
Marcus Goeckner <mbgoec2000@gmail.com>
2021-07-08 16:49:38 -0700

    Program checks for available ports and dynamically assigns at runtime

--- CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Program.cs
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Sockets;
 
 namespace CSETWeb_ApiCore
 {
@@ -23,7 +25,42 @@ namespace CSETWeb_ApiCore
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    int startPort = 5000;
+                    int httpPort = CheckPorts(startPort);
+                    int httpsPort = CheckPorts(httpPort + 1);
+                    webBuilder.UseUrls("http://localhost:" + httpPort.ToString() + ";https://localhost:" + httpsPort.ToString())
+                               .UseKestrel()
+                               .UseStartup<Startup>();
                 });
+
+
+        /// <summary>
+        /// Checks if local port is already in use and increments by one until available port is found
+        /// </summary>
+        /// <param name="port"> The starting port to check </param>
+        /// <returns> The next available port to be used by the api </returns>
+        private static int CheckPorts(int port)
+        {
+            bool foundAvailablePort = false;
+            Console.WriteLine("Begin check for available ports to be used...\r\n");
+            while (foundAvailablePort == false)
+            {
+                using (TcpClient tcpClient = new TcpClient())
+                {
+                    try
+                    {
+                        tcpClient.Connect("127.0.0.1", port);
+                        Console.WriteLine("Port " + port.ToString() + " is already listening for connections. Looking at next port...\r\n");
+                        port++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.Write("Port " + port.ToString() + " is not listening for any connections. Application will use this port.\r\n");
+                        foundAvailablePort = true;
+                    }
+                }
+            }
+            return port;
+        }
     }
 }
