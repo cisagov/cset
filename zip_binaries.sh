3cfcac7100eb3180831946b257c3c87612bd2aad
Marcus Goeckner <mbgoec2000@gmail.com>
2021-07-09 12:05:40 -0700

    finish script

--- zip_binaries.sh
+++ zip_binaries.sh
@@ -2,11 +2,11 @@ _versionNum=10.3.0.0
  # Copies SQL files to dist/Data
  ECHO "Copying Database Files"
  # IF NOT EXIST "cset\dist\Data" MD "cset\dist\Data"
- mkdir -p "/dist/Data"
+ mkdir -p dist/Data
  # COPY /Y "cset\Database Images\" "cset\dist\Data"
- cp -r "cset/Database Images/" "/dist/Data"
- ECHO Copying Database for Enterprise Binary Complete
+ cp -r "cset/Database Images/" dist/Data
+ ECHO "Copying Database for Enterprise Binary Complete"
  # Zip files into a folder in current directory
  ECHO "Zipping file to CSET_$_versionNum-Binary.zip"
- ./"7zip/7z.exe" a -tzip "zipped_binaries/CSET_$_versionNum-Binary.zip" "./dist"
+ ./7zip/7z.exe a -tzip zip_binaries/CSET_$_versionNum-Binary.zip dist
  ECHO "Completed Enterprise Binary"
