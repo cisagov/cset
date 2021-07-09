1ef4eae48266ad663c5243102128db2ff1bd2cde
Marcus Goeckner <mbgoec2000@gmail.com>
2021-07-09 11:27:07 -0700

    copying database images to dist

--- zip_binaries.sh
+++ zip_binaries.sh
@@ -2,9 +2,11 @@ _versionNum=10.3.0.0
  # Copies SQL files to dist/Data
  ECHO "Copying Database Files"
  # IF NOT EXIST "cset\dist\Data" MD "cset\dist\Data"
+ mkdir -p "/dist/Data"
  # COPY /Y "cset\Database Images\" "cset\dist\Data"
- # ECHO Copying Database for Enterprise Binary Complete
+ cp -r "cset/Database Images/" "/dist/Data"
+ ECHO Copying Database for Enterprise Binary Complete
  # Zip files into a folder in current directory
  ECHO "Zipping file to CSET_$_versionNum-Binary.zip"
- # "7zip\x64\7zr.exe" a -tzip "CSET_$_versionNum%-Binary.zip" "cset\dist"
+ ./"7zip/7z.exe" a -tzip "zipped_binaries/CSET_$_versionNum-Binary.zip" "./dist"
  ECHO "Completed Enterprise Binary"
