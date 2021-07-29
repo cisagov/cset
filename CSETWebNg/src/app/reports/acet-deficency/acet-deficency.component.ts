2a2469b9a58fa6109d6469dfde30321bc7a024db
Jason Kuipers <kuipers.jason@gmail.com>
2021-07-29 11:22:47 -0600

    add headers to gaps report

--- CSETWebNg/src/app/reports/acet-deficency/acet-deficency.component.ts
+++ CSETWebNg/src/app/reports/acet-deficency/acet-deficency.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { ReportAnalysisService } from '../../services/report-analysis.service';
 import { ReportService } from '../../services/report.service';
 import { ConfigService } from '../../services/config.service';
-import { Title, DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { AcetDashboard } from '../../models/acet-dashboard.model';
-import { AdminTableData, AdminPageData, HoursOverride } from '../../models/admin-save.model';
-import { MaturityService } from '../../services/maturity.service';
+import { Title } from '@angular/platform-browser';
+import { ACETService } from '../../services/acet.service';
+import { forEach } from 'lodash';
 
 
 @Component({
@@ -21,18 +20,27 @@ export class AcetDeficencyComponent implements OnInit {
     public reportSvc: ReportService,
     public configSvc: ConfigService,
     private titleService: Title,
-    public maturitySvc: MaturityService
+    public acetSvc: ACETService
   ) { }
 
   ngOnInit() {
-    this.titleService.setTitle("Deficiency Report - ACET");
+    this.titleService.setTitle("Gap Report - ACET");
 
-    this.maturitySvc.getMaturityDeficiency("ACET").subscribe(
+    this.acetSvc.getAnsweredQuestions().subscribe(
       (r: any) => {
         this.response = r;        
       },
-      error => console.log('Deficiency Report Error: ' + (<Error>error).message)
+      error => console.log('Gap Report Error: ' + (<Error>error).message)
     );
+  }
+
+  checkForGaps(){
+    for(let d of this.response?.MatAnsweredQuestions){
+      if(d.IsDeficient){
+        return true;
+      }
+    }
+    return false;
   }
 
 }
