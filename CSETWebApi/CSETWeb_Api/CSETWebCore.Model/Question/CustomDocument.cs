386e2499df5c6dd51588407bf60df38eb882d6a0
Randy <randall.woods@inl.gov>
2021-09-02 18:06:45 -0600

    Adding plumbing to dl uploaded ref docs from question extras

--- CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Question/CustomDocument.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Question/CustomDocument.cs
@@ -2,16 +2,19 @@
 {
     public class CustomDocument
     {
+        public int File_Id { get; set; }
         public string Title { get; set; }
         public string File_Name { get; set; }
         public string Section_Ref { get; set; }
         public bool Is_Uploaded { get; set; }
+
         public CustomDocument()
         {
         }
 
-        public CustomDocument(string title, string fileName, string sectionRef, bool isUploaded)
+        public CustomDocument(int id, string title, string fileName, string sectionRef, bool isUploaded)
         {
+            this.File_Id = id;
             this.Title = title;
             this.File_Name = fileName;
             this.Section_Ref = sectionRef;
