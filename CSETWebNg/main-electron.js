836cc4dcdda284e4fb34ae8cc76bfd07f376d43f
Marcus Goeckner <mbgoec2000@gmail.com>
2021-09-08 11:24:33 -0700

    adding icon, title, and developer tools to electron window

--- CSETWebNg/main-electron.js
+++ CSETWebNg/main-electron.js
@@ -1,69 +1,69 @@
-const {app, ipcMain, BrowserWindow, Menu} = require('electron')
-const path = require('path')
-const url = require('url')
-const { execFile } = require('child_process')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const url = require('url');
 
-let win
+let mainWindow;
 
-function createWindow () {
+const mainMenuTemplate = [];
+
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
     webPreferences: { nodeIntegration: true }
-  })
+  });
 
-  Menu.setApplicationMenu(null)
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 
   // and load the index.html of the app.
   // paths to some assets still need fixed
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'dist/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // win.loadURL('http://localhost:4200/')
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/index.html'),
+      protocol: "file:",
+      slashes: true,
+      icon: path.join(__dirname, 'favicon_cset.ico'),
+      title: 'CSET'
+    })
+  );
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', () => {
-  // Launch CSET API
-  apiDirPath = path.join(__dirname, '../CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/bin/Debug/net5.0/')
-  execFile(apiDirPath + 'CSETWebCore.Api.exe', { cwd: apiDirPath },
-   (error, stderr) => {
-    if (error) {
-        console.error('stderr', stderr);
-        throw error;
+// Give option for dev tools if not in production
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        click(focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: 'reload'
       }
+    ]
   });
-  createWindow()
-})
+}
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
-  if (win === null) {
-    createWindow()
+app.on('ready', () => {
+  if (mainWindow === null) {
+    createWindow();
   }
-})
-
-
-
-
-
+});
