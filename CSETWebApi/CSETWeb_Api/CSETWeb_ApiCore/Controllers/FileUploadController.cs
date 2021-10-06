8068dc2285f5d714acde8d84c34d70c2ef9a0945
ijohnnmotleyl <john.motley@inl.gov>
2021-10-06 12:30:32 -0600

    Initial endpoint created, returning 501 for now.

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/FileUploadController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Api.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpPost]
+        [Route("/api/files/blob/create/")]
+        public async Task<IActionResult> Upload()
+        {
+
+            return StatusCode(501);
+        }
+    }
+}
