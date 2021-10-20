646a3cbcdcdf5d26ba5be0dcb60dea40915cee4b
Marcus Goeckner <mbgoec2000@gmail.com>
2021-10-20 15:35:41 -0700

    Implement port checking using localStorage and query params

--- CSETWebNg/main-electron.js
+++ CSETWebNg/main-electron.js
@@ -45,64 +45,59 @@ function createWindow() {
 
   if (app.isPackaged) {
     Menu.setApplicationMenu(null);
-    // check angular config file for API port and increment port automatically if designated port is already taken
+
+    // check angular config file for initial API port and increment port automatically if designated port is already taken
     let apiPort = parseInt(angularConfig.api.port);
     let apiUrl = angularConfig.api.url;
-    assignPort(apiPort, apiUrl).then(assignedPort => {
-      // write new config files if port has changed
-      if (assignedPort != angularConfig.api.port) {
-        angularConfig.api.port = assignedPort.toString();
-        const angularJson = JSON.stringify(angularConfig, null, '\t');
+    assignPort(apiPort, null, apiUrl).then(assignedApiPort => {
+      log.info('API launching on port', assignedApiPort);
+      launchAPI(rootDir + '/Website', 'CSETWebCore.Api.exe', assignedApiPort);
+      return assignedApiPort;
+    }).then(assignedApiPort => {
 
-        try {
-          fs.writeFileSync(path.join(__dirname, "dist/assets/config.json"), angularJson);
-        } catch(error) {
-          log.error(error);
-        }
-      }
-      log.info('API launching on port', assignedPort);
-      launchAPI(rootDir + '/Website', 'CSETWebCore.Api.exe', assignedPort);
-      return assignedPort;
-    }).then(assignedPort => {
-      // keep attempting to connect to API, every 2 seconds, then load application
-      retryApiConnection(20, 2000, assignedPort, error => {
-        if (error) {
-          log.error(error);
-          app.quit();
-        } else {
-          // load the index.html of the app
-          mainWindow.loadURL(
-            url.format({
-              pathname: path.join(__dirname, 'dist/index.html'),
-              protocol: 'file:',
-              slashes: true
-            })
-          );
-        }
+      // port checking for reports api...
+      let reportsApiPort = parseInt(angularConfig.reportsApi.substr(angularConfig.reportsApi.length - 5, 4));
+      assignPort(reportsApiPort, assignedApiPort, apiUrl).then(assignedReportsApiPort => {
+        log.info('Reports API launching on port', assignedReportsApiPort);
+        launchAPI(rootDir + '/Website', 'CSETWebCore.Reports.exe', assignedReportsApiPort);
+        return {apiPort: assignedApiPort, reportsApiPort: assignedReportsApiPort};
+      }).then(ports => {
+
+        // keep attempting to connect to API, every 2 seconds, then load application
+        retryApiConnection(20, 2000, ports.apiPort, error => {
+          if (error) {
+            log.error(error);
+            app.quit();
+          } else {
+
+            // load the index.html of the app
+            mainWindow.loadURL(
+              url.format({
+                pathname: path.join(__dirname, 'dist/index.html'),
+                protocol: 'file:',
+                query: {
+                  apiUrl: angularConfig.api.protocol + '://' + angularConfig.api.url + ':' + ports.apiPort,
+                  reportsApiUrl: angularConfig.reportsApi.substr(0, 17) + ports.reportsApiPort + '/'
+                },
+                slashes: true
+              })
+            );
+          }
+        });
       });
     });
-
-
-    // port checking for reports api... (wait some time before port checking to not conflict with main api port checking)
-    // reports api is not needed immediately
-    setTimeout(() => {
-      let reportApiPort = parseInt(angularConfig.reportsApi.substr(angularConfig.reportsApi.length - 5, 4));
-      assignPort(reportApiPort, apiUrl).then(assignedPort => {
-        // write new config file if port has changed
-        if (assignedPort != reportApiPort) {
-          angularConfig.reportsApi = "http://localhost:" + assignedPort + '/';
-          const angularJson = JSON.stringify(angularConfig, null, '\t');
-
-          try {
-            fs.writeFileSync(path.join(__dirname, "dist/assets/config.json"), angularJson);
-          } catch(error) {
-            log.error(error);
-          }
-        }
-        log.info('Reports API launching on port', assignedPort);
-        launchAPI(rootDir + '/Website', 'CSETWebCore.Reports.exe', assignedPort);
-      });
-    }, 6000)
+  } else {
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        query: {
+          apiUrl: angularConfig.api.protocol + '://' + angularConfig.api.url + ':' + angularConfig.api.port,
+          reportsApiUrl: angularConfig.reportsApi
+        },
+        slashes: true
+      })
+    );
   }
 
   // Emitted when the window is closed
@@ -176,7 +171,7 @@ app.on('window-all-closed', () => {
 function launchAPI(exeDir, fileName, port) {
   let exe = exeDir + '/' + fileName;
   let options = {cwd:exeDir};
-  let args = ['--urls', 'http://localhost:' + port]
+  let args = ['--urls', angularConfig.api.protocol + '://' + angularConfig.api.url + ':' + port]
   child(exe, args, options, (error, data) => {
     log.error(error);
     log.info(data.toString());
@@ -184,12 +179,11 @@ function launchAPI(exeDir, fileName, port) {
 }
 
 // Increment port number until a non listening port is found
-function assignPort(port, host) {
+function assignPort(port, offLimitPort, host) {
   return tcpPortUsed.check(port, host).then(status => {
-    log.info('Port', port, 'on', host, 'in use:', status,);
-    if (status === true) {
-      log.info('Incrementing port...');
-      return assignPort(port + 1, host)
+    if (status === true || port === offLimitPort) {
+      log.info('Port', port, 'on', host, 'is already in use. Incrementing port...');
+      return assignPort(port + 1, offLimitPort, host);
     } else {
       return port;
     }
