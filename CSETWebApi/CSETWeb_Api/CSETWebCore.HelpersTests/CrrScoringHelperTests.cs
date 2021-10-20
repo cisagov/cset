fa2fa232c8c4ca623194c971c9d9bd973948be79
inlguy <5023364+inlguy@users.noreply.github.com>
2021-10-20 11:01:50 -0600

    Started unit Test

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.HelpersTests/CrrScoringHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSETWebCore.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Helpers.Tests
+{
+    [TestClass()]
+    public class CrrScoringHelperTests
+    {
+        [TestMethod()]
+        public void InstantiateScoringHelperTest()
+        {
+            CSETWebCore.DataLayer.CSETContext context = new DataLayer.CSETContext();
+            CrrScoringHelper crrScoring = new CrrScoringHelper(context);
+
+            crrScoring.InstantiateScoringHelper(59);
+
+        }
+    }
+}
