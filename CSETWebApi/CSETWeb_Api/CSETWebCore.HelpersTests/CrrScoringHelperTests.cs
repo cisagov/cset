2862ec1cb562fec1277a3df817710423d836a85a
Randy <randall.woods@inl.gov>
2021-10-20 16:45:50 -0600

    Added back some usings that got lost

--- CSETWebApi/CSETWeb_Api/CSETWebCore.HelpersTests/CrrScoringHelperTests.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.HelpersTests/CrrScoringHelperTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace CSETWebCore.Helpers.Tests
 {
