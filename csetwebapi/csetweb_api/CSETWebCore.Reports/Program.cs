ada370115461dc8709edbdd4452efc4d38209b1d
Randy <randall.woods@inl.gov>
2021-10-21 15:07:48 -0600

    Added missing CSF maps. Fixed CSF bars

--- csetwebapi/csetweb_api/CSETWebCore.Reports/Program.cs
+++ csetwebapi/csetweb_api/CSETWebCore.Reports/Program.cs
@@ -18,9 +18,9 @@ namespace CSETWebCore.Reports
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-               .ConfigureWebHostDefaults(webBuilder =>
-               {
-                   webBuilder.UseStartup<Startup>();
-               });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
