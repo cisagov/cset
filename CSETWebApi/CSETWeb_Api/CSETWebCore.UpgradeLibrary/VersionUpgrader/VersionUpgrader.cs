436408b6489532988f3a427e71c85f7a0d0881ac
Marcus Goeckner <mbgoec2000@gmail.com>
2021-11-03 10:45:09 -0700

    Remove log4net from VersionUpgrader

--- CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/VersionUpgrader.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/VersionUpgrader.cs
@@ -13,8 +13,6 @@ namespace UpgradeLibrary.Upgrade
     /// </summary>
     public class VersionUpgrader
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         /// <summary>
         /// 
         /// </summary>
