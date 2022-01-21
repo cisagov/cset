54e43cf3365937ba36ba4a0e7a4a03cf93fbc8d0
Marcus Goeckner <mbgoec2000@gmail.com>
2022-01-21 14:11:33 -0800

    Use quotes on package

--- build_core.sh
+++ build_core.sh
@@ -70,9 +70,9 @@ build_electron() {
 	echo 'Packaging CSET as Electron App'
 	npm run build:electron
 	
-	$package = CSET
+	$package="CSET"
 	
-	mkdir -p ../dist/electron && cp -r electron-builds/$package-win32-x64/. ../dist/electron
+	mkdir -p ../dist/electron && cp -r electron-builds/${package}-win32-x64/. ../dist/electron
 	
 	echo 'Electron package complete'
 }
