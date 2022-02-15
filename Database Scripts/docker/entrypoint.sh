ba8450a5e5396a7a2a4ef202e0125ea28e0c123a
Nathan Woodruff <nwoodr94@gmail.com>
2022-02-15 12:31:14 -0700

    fix database script

--- Database Scripts/docker/entrypoint.sh
+++ Database Scripts/docker/entrypoint.sh
@@ -1,3 +1,3 @@
 # Executes the database startup script, and then run the service in the container
 
-/data/database.sh & /opt/mssql/bin/sqlservr
+/bin/bash /data/database.sh & /opt/mssql/bin/sqlservr
