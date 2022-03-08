d39eeced512431e58358fc975c9b977ca7256d84
Marcus Goeckner <mbgoec2000@gmail.com>
2022-03-08 08:11:42 -0800

    Fix typos

--- CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManager/DbManager.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManager/DbManager.cs
@@ -112,7 +112,7 @@ namespace CSETWebCore.DatabaseManager
                             }
                             else
                             {
-                                log.Error("Database is not fuctioning after copy attempt");
+                                log.Error("Database is not functioning after copy attempt");
                             }
                         }
                     }
@@ -144,7 +144,7 @@ namespace CSETWebCore.DatabaseManager
                         }
                         else
                         {
-                            log.Error("Database is not fuctioning after copy attempt");
+                            log.Error("Database is not functioning after copy attempt");
                         }
                     }
                 }
