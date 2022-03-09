4667854132361de71dadf480db5f5b40ab23e09d
Marcus Goeckner <mbgoec2000@gmail.com>
2022-03-09 10:16:44 -0800

    Add CistPocDetail model

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Contact/CistPocDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Model.Contact
+{
+    public class CistPocDetail
+    {
+        public int ContactId { get; set; }
+        public int Assessmentid { get; set; }
+        public bool IsPrimaryPoc { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string SiteName { get; set; }
+        public string OrganizationName { get; set; }
+        public string Email { get; set; }
+        public string OfficePhone { get; set; }
+        public string CellPhone { get; set; }
+        public string ReportsTo { get; set; }
+        public string EmergencyCommunicationsProtocol { get; set; }
+        public bool IsSiteParticicpant { get; set; }
+    }
+}
