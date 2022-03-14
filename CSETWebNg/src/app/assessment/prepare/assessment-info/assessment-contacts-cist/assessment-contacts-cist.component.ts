c33a47067480c882dbbf6cec525de4e70442dcb8
Marcus Goeckner <mbgoec2000@gmail.com>
2022-03-14 16:01:27 -0700

    Adding cist contact page

---
+++ CSETWebNg/src/app/assessment/prepare/assessment-info/assessment-contacts-cist/assessment-contacts-cist.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assessment-contacts-cist',
+  templateUrl: './assessment-contacts-cist.component.html'
+})
+export class AssessmentContactsCistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeContact(event) {
+
+  }
+
+}
