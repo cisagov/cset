b1f3c7f2213a33c773e8ba8be3fba757ddc5510e
Marcus Goeckner <mbgoec2000@gmail.com>
2022-03-16 13:34:39 -0700

    Add Critical Service Information component

---
+++ CSETWebNg/src/app/assessment/prepare/csi/csi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-csi',
+  templateUrl: './csi.component.html',
+  styleUrls: ['./csi.component.scss']
+})
+export class CsiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
