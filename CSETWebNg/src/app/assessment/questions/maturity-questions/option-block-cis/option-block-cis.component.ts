630d5a670b810e7266fc6bd7a388380804187c64
Randy <randall.woods@inl.gov>
2022-03-23 16:17:52 -0600

    Reworked the checkboxes and radios
    Using Material controls did not handle nested radio/checkbox groups well because the entire option, along with all of its nested content were wrapped in the <label> element, so clicks in the nested controls were being picked up by the parent.

--- CSETWebNg/src/app/assessment/questions/maturity-questions/option-block-cis/option-block-cis.component.ts
+++ CSETWebNg/src/app/assessment/questions/maturity-questions/option-block-cis/option-block-cis.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class OptionBlockCisComponent implements OnInit {
 
+  @Input() q: any;
   @Input() opts: any[];
 
   optRadio: any[];
@@ -17,11 +18,29 @@ export class OptionBlockCisComponent implements OnInit {
 
   constructor() { }
 
+  /**
+   * 
+   */
   ngOnInit(): void {
     // break up the options so that we can group radio buttons in a mixed bag of options
     this.optRadio = this.opts.filter(x => x.optionType == 'Radio');
     this.optCheckbox = this.opts.filter(x => x.optionType == 'Checkbox');
     this.optOther = this.opts.filter(x => x.optionType != 'Radio' && x.optionType != 'Checkbox');
+  }
+
+  /**
+   * 
+   */
+  changeRadio(o, event): void {
+    o.selected = event.target.checked;
+    console.log(o);
+    console.log(event);
+  }
+
+  changeCheckbox(o, event): void {
+    o.selected = event.target.checked;
+    console.log(o);
+    console.log(event);
   }
 
 }
