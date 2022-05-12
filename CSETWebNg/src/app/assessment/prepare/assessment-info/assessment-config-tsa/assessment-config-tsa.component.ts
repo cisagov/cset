77c8f2c72514c67a9ef69674b83961eae18a01f2
lcantillo84 <lcantillodev@gmail.com>
2022-05-12 18:42:10 -0400

    -Maturity and standards  by sectorId and industryId

--- CSETWebNg/src/app/assessment/prepare/assessment-info/assessment-config-tsa/assessment-config-tsa.component.ts
+++ CSETWebNg/src/app/assessment/prepare/assessment-info/assessment-config-tsa/assessment-config-tsa.component.ts
@@ -18,44 +18,7 @@ export class AssessmentConfigTsaComponent implements OnInit {
   expandedDesc: boolean[] = [];
 
   // the list of features that can be selected
-  features: any = [
-    // {
-    //   code: 'rra',
-    //   label: 'Ransomware Readiness Assessment (RRA)',
-    //   description: 'Take the Ransomware Readiness Assessment (RRA) to understand your cybersecurity posture and assess how well your organization is equipped to defend and recover from a ransomware incident.',
-    //   expanded: false
-    // },
-    // {
-    //   code: 'crr',
-    //   label: 'Cyber Resilience Review (CRR)',
-    //   description: 'The CRR is a no-cost, voluntary, non-technical assessment to evaluate an organization’s operational resilience and cybersecurity practices.',
-    //   expanded: false
-    // },
-    // {
-    //   code: 'vadr',
-    //   label: 'Validated Architecture Design Reviews (VADR)',
-    //   description: 'The VADR maturity model enables participants to perform assessments virtually. Virtual assessments include the same elements that make up a traditional VADR: Architecture Design Review,  System Configuration and Log Review, as well as Network Traffic Analysis.',
-    //   expanded: false
-    // },
-    // {
-    //   code: 'TSA2018',
-    //   label: 'TSA Pipeline Security Guidelines March 2018 with April 2021 revision',
-    //   description: 'Utilizing an industry and government collaborative approach, TSA develops guidelines to help advance security measures for the physical and cyber security space. The security measures in this assessment and related guidance provide the basis for TSA’s Pipeline Security Program Corporate Security Reviews and Critical Facility Security Reviews.',
-    //   expanded: false
-    // },
-    // {
-    //   code: 'CSC_V8',
-    //   label: 'CIS v8 (Critical Security Controls)',
-    //   description: 'The CIS controls v8 (Critical Security Controls (CIS Controls)) are a prioritized set of Safeguards to mitigate the most prevalent cyber-attacks against systems and networks. They are mapped to and referenced by multiple legal, regulatory, and policy frameworks and contains controls to address movement to cloud-based computing, virtualization, mobility, outsourcing and Work-from-Home environments.',
-    //   expanded: false
-    // },
-    // {
-    //   code: 'APTA_Rail_V1',
-    //   label: 'Light Rail Transit Standards',
-    //   description: 'Defining a Security Zone Architecture for Rail Transit and Protecting Critical Zones. Securing Control and Communications Systems in Rail Transit Environments.',
-    //   expanded: false
-    // }
-  ];
+  features: any = [];
 
 
   /**
@@ -80,7 +43,6 @@ export class AssessmentConfigTsaComponent implements OnInit {
     this.navSvc.setCurrentPage('info-tsa');
     this.tsaSvc.TSAGetModelsName().subscribe((data)=>{
      this.features=data;
-     console.log(this.features)
     this.features.forEach(element => {
       if(element.set_Name && !element.model_Name){
         this.assessSvc.assessment.useStandard;
