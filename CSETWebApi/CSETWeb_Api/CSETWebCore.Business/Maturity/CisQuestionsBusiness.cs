7fea890b7a3aec2c281c77f81b9204dc2fb4c8fd
Marcus Goeckner <mbgoec2000@gmail.com>
2022-06-13 16:18:34 -0700

    Remove checks for OptionId

--- CSETWebApi/CSETWeb_Api/CSETWebCore.Business/Maturity/CisQuestionsBusiness.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Business/Maturity/CisQuestionsBusiness.cs
@@ -96,8 +96,7 @@ namespace CSETWebCore.Business.Maturity
 
             ANSWER dbAnswer = _context.ANSWER.Where(x => x.Assessment_Id == _assessmentId
                 && x.Question_Or_Requirement_Id == dbQuestion.Mat_Question_Id
-                && x.Question_Type == answer.QuestionType
-                && x.Mat_Option_Id == answer.OptionId).FirstOrDefault();
+                && x.Question_Type == answer.QuestionType).FirstOrDefault();
 
 
             if (dbAnswer == null)
@@ -151,8 +150,7 @@ namespace CSETWebCore.Business.Maturity
             ANSWER dbAnswer = _context.ANSWER.Where(x => x.Assessment_Id == _assessmentId
                 && x.Question_Or_Requirement_Id == dbQuestion.Mat_Question_Id
                 && x.Mat_Option_Id == answer.OptionId
-                && x.Question_Type == answer.QuestionType
-                && x.Mat_Option_Id == answer.OptionId).FirstOrDefault();
+                && x.Question_Type == answer.QuestionType).FirstOrDefault();
 
 
             if (dbAnswer == null)
