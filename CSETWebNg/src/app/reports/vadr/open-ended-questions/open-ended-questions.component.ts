002c65bf99d0b7bc6f511412c8a5af2558e8f0d3
lcantillo84 <lcantillodev@gmail.com>
2022-06-15 09:48:35 -0400

    Added Sub-header
    main question and open ended Q

--- CSETWebNg/src/app/reports/vadr/open-ended-questions/open-ended-questions.component.ts
+++ CSETWebNg/src/app/reports/vadr/open-ended-questions/open-ended-questions.component.ts
@@ -1,48 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { data } from 'jquery';
 import { ConfigService } from '../../../services/config.service';
 import { MaturityService } from '../../../services/maturity.service';
 import { ReportAnalysisService } from '../../../services/report-analysis.service';
 import { ReportService } from '../../../services/report.service';
+import { QuestionGrouping, MaturityQuestionResponse, Domain } from '../../../models/questions.model';
+import { NavigationService } from '../../../services/navigation.service';
+import { QuestionFilterService } from '../../../services/filtering/question-filter.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { QuestionFiltersComponent } from '../../../dialogs/question-filters/question-filters.component';
+import { MaturityFilteringService } from '../../../services/filtering/maturity-filtering/maturity-filtering.service';
 
 @Component({
   selector: 'app-open-ended-questions',
   templateUrl: './open-ended-questions.component.html',
-  styleUrls: ['../../reports.scss', '../../acet-reports.scss']
+  styleUrls: ['../../reports.scss', '../../acet-reports.scss', './open-ended-questions.component.scss']
 })
 export class OpenEndedQuestionsComponent implements OnInit {
+  groupings: QuestionGrouping[];
+  subgroup: any [];
+  openEndedQuestion = false;
 
+  onlyOpenQuestionData=[];
+  modelName: string = '';
   response: any;
+  // showYNQuestions = false;
+  // response: any;
+  //
+  // @Input() myGrouping: QuestionGrouping;
+  // colorSchemeRed = { domain: ['#DC3545'] };
+  // xAxisTicks = [0, 25, 50, 75, 100];
+  //
+  // answerDistribByGoal = [];
+  // topRankedGoals = [];
 
-  colorSchemeRed = { domain: ['#DC3545'] };
-  xAxisTicks = [0, 25, 50, 75, 100];
+  loaded = false;
 
-  answerDistribByGoal = [];
-  topRankedGoals = [];
-
+  filterDialogRef: MatDialogRef<QuestionFiltersComponent>;
   constructor(
     public analysisSvc: ReportAnalysisService,
     public reportSvc: ReportService,
     public configSvc: ConfigService,
     private titleService: Title,
-    public maturitySvc: MaturityService
+    public maturitySvc: MaturityService,
+    public navSvc: NavigationService,
+    public filterSvc: QuestionFilterService,
+    public maturityFilteringSvc: MaturityFilteringService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
+    this.loadQuestions();
     this.titleService.setTitle("Validated Architecture Design Review Report - VADR");
-this.maturitySvc.getMaturityOpenEndedQuestions("VADR").subscribe(data=>{
-  console.log(data);
-})
-    this.maturitySvc.getMaturityDeficiency("VADR").subscribe(
-      (r: any) => {
-        this.response = r;
-        console.log(r)
-        // remove any child questions - they are not Y/N
-        this.response.deficienciesList = this.response.deficienciesList.filter(x => x.mat.parent_Question_Id == null);
-      },
-      error => console.log('Deficiency Report Error: ' + (<Error>error).message)
-    );
   }
 
 
@@ -56,7 +66,46 @@ this.maturitySvc.getMaturityOpenEndedQuestions("VADR").subscribe(data=>{
       return true;
     }
   }
+  loadQuestions() {
+    const magic = this.navSvc.getMagic();
+    this.groupings = null;
+    this.maturitySvc.getQuestionsList(this.configSvc.installationMode, false).subscribe(
+      (response: MaturityQuestionResponse) => {
+        this.modelName = response.modelName;
+        // this.questionsAlias = response.questionsAlias;
 
+        this.groupings = response.groupings;
+        this.groupings.forEach(element => {
+          this.subgroup=element.subGroupings
+          // console.log(this.subgroup)
+          this.subgroup.forEach(s=>{
+            // this.openEndedQuestion = true;
+            this.onlyOpenQuestionData.push(s);
+           s.questions.forEach(i => {
+              if(i.freeResponseAnswer !=null){
+                this.openEndedQuestion=true;
+                console.log(this.openEndedQuestion )
+              }
+            // this.onlyOpenQuestionData.push(i);
 
+           });
+          })
+        });
+        console.log(this.onlyOpenQuestionData)
+        this.subgroup.forEach(q=>{
+          // console.log(q)
+        })
+      },
+      error => {
+        console.log(
+          'Error getting questions: ' +
+          (<Error>error).name +
+          (<Error>error).message
+        );
+        console.log('Error getting questions: ' + (<Error>error).stack);
+      }
+    );
+  }
 }
+
 
