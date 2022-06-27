1528f5255f2d790fb7b38e550ae877bf83b9058c
Randy <randall.woods@inl.gov>
2022-06-27 16:57:51 -0600

    Add requirements mode to Security Plan report

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Reports/ControlRow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Model.Reports
+{
+    public class ControlRow
+    {
+        public int Requirement_Id;
+        public int Question_Id;
+
+        public string Requirement_Text;
+        public string Requirement_Title;
+        public string Standard_Level;
+        public string Short_Name;
+        public string Standard_Category;
+        public string Standard_Sub_Category;
+        public string Answer_Text;
+        public string Comment;
+        public string Simple_Question;
+    }
+}
