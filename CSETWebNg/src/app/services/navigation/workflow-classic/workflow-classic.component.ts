a1b26e69d10a5cc2d10929ff0f1f61eeb49c2ec7
Randy <randall.woods@inl.gov>
2022-07-20 13:54:10 -0600

    Starting a new navigation service for workflows

---
+++ CSETWebNg/src/app/services/navigation/workflow-classic/workflow-classic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workflow-classic',
+  templateUrl: './workflow-classic.component.html',
+  styleUrls: ['./workflow-classic.component.scss']
+})
+export class WorkflowClassicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
