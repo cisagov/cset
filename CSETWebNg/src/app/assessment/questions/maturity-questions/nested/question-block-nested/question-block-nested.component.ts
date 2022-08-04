bc8a499964c9fb2fa42687d36c7c8e0f9cf45ffc
Randy <randall.woods@inl.gov>
2022-08-04 12:35:53 -0600

    Handset formatting for CIS questions

--- CSETWebNg/src/app/assessment/questions/maturity-questions/nested/question-block-nested/question-block-nested.component.ts
+++ CSETWebNg/src/app/assessment/questions/maturity-questions/nested/question-block-nested/question-block-nested.component.ts
@@ -29,6 +29,7 @@ import { QuestionsService } from '../../../../../services/questions.service';
 import { ConfigService } from '../../../../../services/config.service';
 import { QuestionExtrasDialogComponent } from '../../../question-extras-dialog/question-extras-dialog.component';
 import { AssessmentService } from '../../../../../services/assessment.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-question-block-nested',
@@ -41,6 +42,7 @@ export class QuestionBlockNestedComponent implements OnInit {
 
   questionList: Question[];
 
+  handsetPortrait = false;
 
   // temporary debug aid
   showIdTag = false;
@@ -50,13 +52,18 @@ export class QuestionBlockNestedComponent implements OnInit {
     public questionsSvc: QuestionsService,
     public cisSvc: CisService,
     private configSvc: ConfigService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    public boSvc: BreakpointObserver
   ) { }
 
   /**
    *
    */
   ngOnInit(): void {
+    this.boSvc.observe(Breakpoints.HandsetPortrait).subscribe(hp => {
+      this.handsetPortrait = hp.matches;
+    });
+
     if (!!this.grouping) {
       this.questionList = this.grouping.questions;
     }
@@ -205,8 +212,8 @@ export class QuestionBlockNestedComponent implements OnInit {
           showMfr: true
         }
       },
-      width: '50%',
-      maxWidth: '50%'
+      width: this.handsetPortrait ? '90%' : '50%',
+      maxWidth: this.handsetPortrait ? '90%' : '50%'
     });
   }
 
