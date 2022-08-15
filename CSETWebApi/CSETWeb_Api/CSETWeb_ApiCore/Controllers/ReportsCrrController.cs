b89bf5c0131b43d75050e07bb20bf0cf1f295639
dsantiagojobs <dsantiagojobs@gmail.com>
2022-08-15 17:41:01 -0400

    made controller changes so marcus can call them

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/ReportsCrrController.cs
@@ -0,0 +1,138 @@
+﻿using CSETWebCore.Business.Reports;
+using CSETWebCore.Interfaces.Assessment;
+using CSETWebCore.Interfaces.Crr;
+using CSETWebCore.Interfaces.Demographic;
+using CSETWebCore.Interfaces.Helpers;
+using CSETWebCore.Interfaces.Reports;
+using CSETWebCore.Reports.Models;
+using Microsoft.AspNetCore.Mvc;
+using CSETWebCore.Api.Models;
+using System.Xml.XPath;
+using System.Linq;
+using System.Collections.Generic;
+using CSETWebCore.DataLayer.Model;
+
+namespace CSETWebCore.Api.Controllers
+{
+    public class ReportsCrrController : Controller
+    {
+        private readonly ITokenManager _token;
+        private readonly ICrrScoringHelper _crr;
+        private readonly IAssessmentBusiness _assessment;
+        private readonly IDemographicBusiness _demographic;
+        private readonly IReportsDataBusiness _report;
+        private readonly CSETContext _context;
+
+        public ReportsCrrController(ITokenManager token, ICrrScoringHelper crr, IAssessmentBusiness assessment, IDemographicBusiness demographic, IReportsDataBusiness report, CSETContext context)
+        {
+            _token = token;
+            _crr = crr;
+            _assessment = assessment;
+            _demographic = demographic;
+            _report = report;
+            _context = context;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="includeResultsStylesheet"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/reportscrr/GetCrrModel")]
+        public IActionResult GetCrrModel(string token, bool includeResultsStylesheet = true)
+        {
+            var assessmentId = _token.AssessmentForUser();
+            _crr.InstantiateScoringHelper(assessmentId);
+            var detail = _assessment.GetAssessmentDetail(assessmentId, token);
+            var demographics = _demographic.GetDemographics(assessmentId);
+            _report.SetReportsAssessmentId(assessmentId);
+            var deficiencyData = new MaturityBasicReportData()
+            {
+                Information = _report.GetInformation(),
+                DeficienciesList = _report.GetMaturityDeficiencies(),
+                Comments = _report.GetCommentsList(),
+                MarkedForReviewList = _report.GetMarkedForReviewList(),
+                QuestionsList = _report.GetQuestionsList()
+            };
+            CrrResultsModel crrResultsData = _crr.GetCrrResultsSummary(); //GenerateCrrResults();
+            CrrVM viewModel = new CrrVM(detail, demographics.CriticalService, _crr, deficiencyData);
+            viewModel.IncludeResultsStylesheet = includeResultsStylesheet;
+            viewModel.ReportChart = _crr.GetPercentageOfPractice();
+            viewModel.crrResultsData = crrResultsData;
+            return Ok(viewModel);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="domain"></param>
+        /// <param name="mil"></param>
+        /// <param name="scale"></param>
+        /// <returns></returns>
+
+        [HttpGet]
+        [Route("api/reportscrr/widget/milheatmap")]
+
+        public IActionResult GetWidget([FromQuery] string domain, [FromQuery] string mil, [FromQuery] double? scale = null)
+        {
+            var assessmentId = _token.AssessmentForUser();
+            _crr.InstantiateScoringHelper(assessmentId);
+
+
+            var xMil = _crr.XDoc.XPathSelectElement($"//Domain[@abbreviation='{domain}']/Mil[@label='{mil}']");
+            if (xMil == null)
+            {
+                return NotFound();
+            }
+
+            // populate the widget with the MIL strip and collapse any hidden goal strips
+            var heatmap = new Helpers.ReportWidgets.MilHeatMap(xMil, true, true);
+            if (scale != null)
+            {
+                heatmap.Scale((double)scale);
+            }
+
+            // return the svg
+            return Content(heatmap.ToString(), "image/svg+xml");
+        }
+
+        private CrrResultsModel GenerateCrrResults()
+        {
+            MaturityReportData maturityData = new MaturityReportData(_context);
+
+            maturityData.MaturityModels = _report.GetMaturityModelData();
+            maturityData.information = _report.GetInformation();
+            maturityData.AnalyzeMaturityData();
+
+
+            // null out a few navigation properties to avoid circular references that blow up the JSON stringifier
+            maturityData.MaturityModels.ForEach(d =>
+            {
+                d.MaturityQuestions.ForEach(q =>
+                {
+                    q.Answer.Assessment = null;
+                });
+            });
+
+            CrrResultsModel retVal = new CrrResultsModel();
+            List<DomainStats> cmmcDataDomainLevelStats = maturityData.MaturityModels.FirstOrDefault(d => d.MaturityModelName == "CRR")?.StatsByDomainAndLevel;
+            retVal.EvaluateDataList(cmmcDataDomainLevelStats);
+            retVal.TrimToNElements(10);
+            retVal.GenerateWidthValues(); //If generating wrong values, check inner method values match the ones set in the css
+            return retVal;
+        }
+
+        private CrrResultsModel GenerateCrrResults(MaturityReportData data)
+        {
+            //For Testing
+
+            CrrResultsModel retVal = new CrrResultsModel();
+            List<DomainStats> cmmcDataDomainLevelStats = data.MaturityModels.Where(d => d.MaturityModelName == "CRR").First().StatsByDomainAndLevel;
+            retVal.EvaluateDataList(cmmcDataDomainLevelStats);
+            retVal.TrimToNElements(10);
+            retVal.GenerateWidthValues(); //If generating wrong values, check inner method values match the ones set in the css
+            return retVal;
+        }
+    }
+}
