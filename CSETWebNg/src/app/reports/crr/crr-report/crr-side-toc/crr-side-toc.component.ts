74e9daafd2981058f69d3a0b222d4ef789936109
Marcus Goeckner <mbgoec2000@gmail.com>
2022-08-16 14:21:04 -0700

    Add CRR side-toc

--- CSETWebNg/src/app/reports/crr/crr-report/crr-side-toc/crr-side-toc.component.ts
+++ CSETWebNg/src/app/reports/crr/crr-report/crr-side-toc/crr-side-toc.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-crr-side-toc',
   templateUrl: './crr-side-toc.component.html',
-  styleUrls: ['./crr-side-toc.component.scss']
+  styleUrls: ['./../crr-report.component.scss']
 })
 export class CrrSideTocComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  scrollToElement(element) {
+    (document.getElementById(element) as HTMLElement).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
 }
