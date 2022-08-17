e05020db4ec212b3cf8b1c10a34c2a9c868db51c
Marcus Goeckner <mbgoec2000@gmail.com>
2022-08-17 12:34:24 -0700

    Use new components on crr summary results page

--- CSETWebNg/src/app/assessment/results/crr/crr-summary-results/crr-summary-results.component.ts
+++ CSETWebNg/src/app/assessment/results/crr/crr-summary-results/crr-summary-results.component.ts
@@ -1,10 +1,6 @@
-import { ChartService } from './../../../../services/chart.service';
 import { Component, OnInit } from '@angular/core';
 import { CrrService } from '../../../../services/crr.service';
 import { CrrReportModel } from './../../../../models/report.model';
-import Chart from 'chart.js/auto';
-import { ConfigService } from '../../../../services/config.service';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-crr-summary-results',
@@ -12,32 +8,22 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 })
 export class CrrSummaryResultsComponent implements OnInit {
 
-  chart: Chart;
   summaryResult: any = '';
-  stylesheetUrl: SafeUrl;
+  crrModel: CrrReportModel;
 
   chartLoaded: boolean = false;
   summaryResultLoaded: boolean = false;
 
   constructor(
     private crrSvc: CrrService,
-    private chartSvc: ChartService,
-    private configSvc: ConfigService,
-    private domSanitizer: DomSanitizer
     ) {
   }
 
   ngOnInit(): void {
-    this.stylesheetUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.configSvc.reportsUrl + 'css/CRRResults.css');
-
     this.crrSvc.getCrrModel().subscribe((data: CrrReportModel) => {
-      this.chart = this.chartSvc.buildCrrPercentagesOfPracticesBarChart('percentagePractices', data.reportChart)
+      this.crrModel = data;
       this.chartLoaded = true;
-    });
-
-    this.crrSvc.getCrrHtml("_CrrResultsSummary").subscribe((data: any) => {
-      this.summaryResult = data.html;
       this.summaryResultLoaded = true;
-    })
+    });
   }
 }
