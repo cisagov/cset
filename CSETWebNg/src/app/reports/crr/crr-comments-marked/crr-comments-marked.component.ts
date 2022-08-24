86640e15a6919df81aa069388fdd4b78b526c6ad
Marcus Goeckner <mbgoec2000@gmail.com>
2022-08-24 14:27:49 -0700

    Finish CRR comments / mfr report conversion to Angular

--- CSETWebNg/src/app/reports/crr/crr-comments-marked/crr-comments-marked.component.ts
+++ CSETWebNg/src/app/reports/crr/crr-comments-marked/crr-comments-marked.component.ts
@@ -59,21 +59,76 @@ export class CrrCommentsMarkedComponent implements OnInit {
         this.crrModel = r;
         console.log(this.crrModel);
 
+        // Build up comments list
         this.crrModel.reportData.comments.forEach(matAns => {
           const domain = matAns.mat.question_Title.split(':')[0];
-          const comment = this.commentsList.find(e => e.cat === this.keyToCategory[domain]);
+          const cElement = this.commentsList.find(e => e.cat === this.keyToCategory[domain]);
 
-          if (!comment) {
+          if (!cElement) {
             this.commentsList.push({ cat: this.keyToCategory[domain], matAnswers: [matAns] });
           } else {
-            comment.matAnswers.push(matAns);
+            cElement.matAnswers.push(matAns);
           }
-        })
+        });
 
-        console.log(this.commentsList);
+        // Sort the marked for review list
+        this.commentsList.forEach(e => {
+          e.matAnswers.sort((a, b) => {
+            return a.mat.question_Title.localeCompare(b.mat.question_Title);
+          })
+          e.matAnswers.sort((a, b) => {
+            return a.mat.question_Text.localeCompare(b.mat.question_Text);
+          })
+        });
+
+        // mark questions followed by a child for border display
+        this.commentsList.forEach(e => {
+          for (let i = 0; i < e.matAnswers.length; i++) {
+            if (e.matAnswers[i + 1]?.mat.parent_Question_Id != null) {
+              e.matAnswers[i].isFollowedByChild = true;
+            }
+          }
+        });
+
+        // Build up marked for review list
+        this.crrModel.reportData.markedForReviewList.forEach(matAns => {
+          const domain = matAns.mat.question_Title.split(':')[0];
+          const mfrElement = this.markedForReviewList.find(e => e.cat === this.keyToCategory[domain]);
+
+          if (!mfrElement) {
+            this.markedForReviewList.push({ cat: this.keyToCategory[domain], matAnswers: [matAns] });
+          } else {
+            mfrElement.matAnswers.push(matAns);
+          }
+        });
+
+        // Sort the marked for review list
+        this.markedForReviewList.forEach(e => {
+          e.matAnswers.sort((a, b) => {
+            return a.mat.question_Title.localeCompare(b.mat.question_Title);
+          })
+          e.matAnswers.sort((a, b) => {
+            return a.mat.question_Text.localeCompare(b.mat.question_Text);
+          })
+        });
+
+        // mark questions followed by a child for border display
+        this.markedForReviewList.forEach(e => {
+          for (let i = 0; i < e.matAnswers.length; i++) {
+            if (e.matAnswers[i + 1]?.mat.parent_Question_Id != null) {
+              e.matAnswers[i].isFollowedByChild = true;
+            }
+          }
+        });
+
+        console.log(this.markedForReviewList);
         this.loading = false;
       },
       error => console.log('Comments Marked Report Error: ' + (<Error>error).message)
     );
+  }
+
+  getFullAnswerText(abb: string) {
+    return this.configSvc.config['answerLabel' + abb];
   }
 }
