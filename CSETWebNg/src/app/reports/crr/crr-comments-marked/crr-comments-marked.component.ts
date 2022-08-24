447bab83bebe987848175c1b2e97f7c96635015d
Marcus Goeckner <mbgoec2000@gmail.com>
2022-08-24 11:49:17 -0700

    More progress on cmfr report

--- CSETWebNg/src/app/reports/crr/crr-comments-marked/crr-comments-marked.component.ts
+++ CSETWebNg/src/app/reports/crr/crr-comments-marked/crr-comments-marked.component.ts
@@ -15,23 +15,24 @@ import { ReportService } from '../../../services/report.service';
 export class CrrCommentsMarkedComponent implements OnInit {
 
   crrModel: CrrReportModel;
-
   loading: boolean = false;
+  logoPath: string = '';
 
   keyToCategory = {
-    'AM':  'Asset Management',
-    'CM':  'Controls Management',
-    'CCM': 'Configuration and Change Management',
-    'VM':  'Vulnerability Management',
-    'IM':  'Incident Management',
-    'SCM': 'Service Continuity Management',
-    'RM': 'Risk Management',
-    'EDM': 'External Dependencies Management',
-    'TA':  'Training and Awareness',
-    'SA':  'Situational Awareness'
+    AM: 'Asset Management',
+    CM: 'Controls Management',
+    CCM: 'Configuration and Change Management',
+    VM: 'Vulnerability Management',
+    IM: 'Incident Management',
+    SCM: 'Service Continuity Management',
+    RM: 'Risk Management',
+    EDM: 'External Dependencies Management',
+    TA: 'Training and Awareness',
+    SA: 'Situational Awareness'
   };
 
-
+  commentsList = [];
+  markedForReviewList = [];
 
   constructor(
   public analysisSvc: ReportAnalysisService,
@@ -45,12 +46,31 @@ export class CrrCommentsMarkedComponent implements OnInit {
     this.loading = true;
     this.titleService.setTitle("Comments Report - CISA CRR");
 
+    let appCode = this.configSvc.installationMode;
+    if (!appCode || appCode === 'CSET') {
+      this.logoPath = "assets/images/CISA_Logo_1831px.png";
+    }
+    else if (appCode === 'CSET-TSA') {
+      this.logoPath = "assets/images/TSA/tsa_insignia_rgbtransparent.png";
+    }
+
     this.crrSvc.getCrrModel().subscribe(
       (r: CrrReportModel) => {
         this.crrModel = r;
+        console.log(this.crrModel);
 
+        this.crrModel.reportData.comments.forEach(matAns => {
+          const domain = matAns.mat.question_Title.split(':')[0];
+          const comment = this.commentsList.find(e => e.cat === this.keyToCategory[domain]);
 
+          if (!comment) {
+            this.commentsList.push({ cat: this.keyToCategory[domain], matAnswers: [matAns] });
+          } else {
+            comment.matAnswers.push(matAns);
+          }
+        })
 
+        console.log(this.commentsList);
         this.loading = false;
       },
       error => console.log('Comments Marked Report Error: ' + (<Error>error).message)
