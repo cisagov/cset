b2a4fd9df2f96f19f739dc143dbabd47c624da3b
Marcus Goeckner <mbgoec2000@gmail.com>
2022-08-25 12:55:47 -0700

    Add upgrade scripts for 11.2.0.0 --> 11.2.1.0

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/ConvertDatabase11210.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.IO;
+namespace UpgradeLibrary.Upgrade
+{
+    internal class ConvertDatabase11210 : ConvertSqlDatabase
+    {
+        public ConvertDatabase11210(string path) : base(path)
+        {
+            myVersion = new Version("11.2.1.0");
+        }
+        /// <summary>
+        /// Runs the database update script
+        /// </summary>
+        /// <param name="conn"></param>
+        public override void Execute(SqlConnection conn)
+        {
+            try
+            {
+                RunFile(Path.Combine(this.applicationPath, @"VersionUpgrader\SQL\11200_to_11210.sql"), conn);
+                RunFile(Path.Combine(this.applicationPath, @"VersionUpgrader\SQL\11200_to_11210_data.sql"), conn);
+                this.UpgradeToVersionLocalDB(conn, myVersion);
+            }
+            catch (Exception e)
+            {
+                throw new DatabaseUpgradeException("Error in upgrading assessment version 11.2.0.0 to 11.2.1.0: " + e.Message);
+            }
+        }
+    }
+}
