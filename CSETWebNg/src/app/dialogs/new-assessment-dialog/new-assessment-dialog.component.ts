f1d7fabbbd2187cc7496c0c335321bf3eb9ffaa3
randywoods <randall.woods@inl.gov>
2022-09-08 11:46:58 -0600

    Added launch plumbing to gallery detail dialog

--- CSETWebNg/src/app/dialogs/new-assessment-dialog/new-assessment-dialog.component.ts
+++ CSETWebNg/src/app/dialogs/new-assessment-dialog/new-assessment-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AssessmentService } from '../../services/assessment.service';
 
 @Component({
   selector: 'app-new-assessment-dialog',
@@ -8,13 +9,39 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class NewAssessmentDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialog: MatDialogRef<NewAssessmentDialogComponent>) { 
-    console.log(data)
+  /**
+   * The gallery item detail for this card
+   */
+  galleryItem: any;
+
+  /**
+   * 
+   */
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any, 
+    private dialog: MatDialogRef<NewAssessmentDialogComponent>,
+    public assessSvc: AssessmentService
+    ) { 
+    this.galleryItem = data;
   }
 
+  /**
+   * 
+   */
   ngOnInit(): void {
   }
 
+  /**
+   * 
+   */
+  launchAssessment() {
+    this.dialog.close();
+    this.assessSvc.newAssessmentGallery(this.galleryItem.gallery_Item_Id);
+  }
+
+  /**
+   * 
+   */
   close() {
     return this.dialog.close();
   }
