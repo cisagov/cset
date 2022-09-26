9e2be8830aa8ef5ae7a52812672a4845d04ebcc3
Marcus Goeckner <mbgoec2000@gmail.com>
2022-09-26 15:39:50 -0700

    Update some more version numbers

--- CSETWebNg/src/environments/environment.ts
+++ CSETWebNg/src/environments/environment.ts
@@ -34,7 +34,7 @@ export const environment = {
   docUrl: 'https://localhost:5001/Documents/',
   analyticsUrl: "https://cset-analytics.dev.inltesting.xyz/",
   appCode: 'CSET',
-  version: '12.0.0.0',
+  version: '12.0.0.1',
   helpContactEmail: 'cset@cisa.dhs.gov',
   helpContactPhone: ''
 };
