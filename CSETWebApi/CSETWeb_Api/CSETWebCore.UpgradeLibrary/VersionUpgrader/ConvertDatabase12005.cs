074bc181f0258e04996f256fa3fe6c16eca5b4d2
Marcus Goeckner <mbgoec2000@gmail.com>
2022-10-18 13:30:54 -0700

    Upgrade scripts for 12004 --> 12005

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/ConvertDatabase12005.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.IO;
+namespace UpgradeLibrary.Upgrade
+{
+    internal class ConvertDatabase12005 : ConvertSqlDatabase
+    {
+        public ConvertDatabase12005(string path) : base(path)
+        {
+            myVersion = new Version("12.0.0.5");
+        }
+
+        /// <summary>
+        /// Runs the database update script
+        /// </summary>
+        /// <param name="conn"></param>
+        public override void Execute(SqlConnection conn)
+        {
+            try
+            {
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12004_to_12005.sql"), conn);
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12004_to_12005_data.sql"), conn);
+
+                this.UpgradeToVersionLocalDB(conn, myVersion);
+            }
+            catch (Exception e)
+            {
+                throw new DatabaseUpgradeException("Error in upgrading database version 12.0.0.4 to 12.0.0.5: " + e.Message);
+            }
+        }
+    }
+}
