f75c8c8663ee6920436ad9263d4ac6bd69915472
Brett <43300357+MathesonBrett@users.noreply.github.com>
2022-11-01 12:55:16 -0700

    Fixing issue riskarea

--- CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Findings/Finding.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Findings/Finding.cs
@@ -19,8 +19,8 @@ namespace CSETWebCore.Model.Findings
         public Importance Importance { get; set; }
         public string Title { get; set; }
         public string Type { get; set; }
-        public string RiskArea { get; set; }
-        public string SubRisk { get; set; }
+        public string Risk_Area { get; set; }
+        public string Sub_Risk { get; set; }
         public string Description { get; set; }
         public string Citations { get; set; }
         public int Auto_Generated { get; set; }
