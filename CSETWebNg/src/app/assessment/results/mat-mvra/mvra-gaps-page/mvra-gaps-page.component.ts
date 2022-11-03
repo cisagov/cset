2e520b99c427ddbffd949d37e1db7bec3ee075a8
Randy <randall.woods@inl.gov>
2022-11-03 14:46:01 -0600

    Built the MVRA report from the results depictions

---
+++ CSETWebNg/src/app/assessment/results/mat-mvra/mvra-gaps-page/mvra-gaps-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mvra-gaps-page',
+  templateUrl: './mvra-gaps-page.component.html',
+  styleUrls: ['./mvra-gaps-page.component.scss']
+})
+export class MvraGapsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
