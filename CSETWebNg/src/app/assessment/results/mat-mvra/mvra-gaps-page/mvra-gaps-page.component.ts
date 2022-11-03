c6c9c312e7c4b2b70612a19e086563235d3ea500
Randy <randall.woods@inl.gov>
2022-11-03 15:00:16 -0600

    Fixed reference to removed scss

--- CSETWebNg/src/app/assessment/results/mat-mvra/mvra-gaps-page/mvra-gaps-page.component.ts
+++ CSETWebNg/src/app/assessment/results/mat-mvra/mvra-gaps-page/mvra-gaps-page.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mvra-gaps-page',
-  templateUrl: './mvra-gaps-page.component.html',
-  styleUrls: ['./mvra-gaps-page.component.scss']
+  templateUrl: './mvra-gaps-page.component.html'
 })
 export class MvraGapsPageComponent implements OnInit {
 
