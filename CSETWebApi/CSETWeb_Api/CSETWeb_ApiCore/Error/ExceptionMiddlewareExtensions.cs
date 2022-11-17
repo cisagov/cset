151d48538e73610443fb811ac3876bc8b28b296e
inlguy <5023364+inlguy@users.noreply.github.com>
2022-11-16 17:26:57 -0700

    Global Exception Handler is built(but doesn't do anything)

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Error/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,34 @@
+﻿using log4net.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace CSETWebCore.Api.Error
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        log4net.LogManager.GetLogger(app.GetType()).Error($"... {contextFeature.Error}");
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
