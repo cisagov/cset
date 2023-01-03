65296a291063fa23d9e61e6436ae8b17cedf8cd3
Matt Winston <mrw0036@mix.wvu.edu>
2023-01-03 16:29:05 -0500

    switching to ANA, this has Barry's sql query

--- CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManagerTests1/ImportExportTests.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManagerTests1/ImportExportTests.cs
@@ -39,12 +39,44 @@ namespace CSETWebCore.DatabaseManager.Tests
             string appCode = "CSET";
             DbManager manager = new DbManager(new Version("12.0.0.16"), clientCode, appCode);
             //run the same test twice and make sure that the number increment works
-            string mdf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWebTest.mdf";
-            string ldf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWebTest_log.ldf";
-            manager.CopyDBFromInstallationSource(mdf,ldf);
-            manager.CopyDBFromInstallationSource(mdf, ldf);
-            manager.CopyDBFromInstallationSource(mdf, ldf);
-            manager.CopyDBFromInstallationSource(mdf, ldf);
+            string mdf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWeb.mdf";
+            string ldf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWeb_log.ldf";
+            string conString = "Server=(localdb)\\mssqllocaldb;Integrated Security=true;AttachDbFileName=" + mdf;
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "INSERT INTO [dbo].[ASSESSMENT_CONTACTS]\r\n           " +
+                    "([Assessment_Id]\r\n\t\t    " +
+                    ",[PrimaryEmail]\r\n           " +
+                    ",[FirstName]\r\n           " +
+                    ",[LastName]\r\n           " +
+                    ",[Invited]\r\n           " +
+                    ",[AssessmentRoleId]\r\n           " +
+                    ",[UserId])\r\n" +
+                    "select distinct e.* from assessment_contacts d right join (\r\n" +
+                        "select a.assessment_id,c.* from assessments a, (\r\n" +
+                        "SELECT \r\n       " +
+                        "[PrimaryEmail]\r\n      " +
+                        ",[FirstName]\r\n      " +
+                        ",[LastName]\r\n      " +
+                        ",[Invited]\r\n      " +
+                        ",[AssessmentRoleId]\r\n      " +
+                        ",[UserId]\t  \r\n  " +
+                        "FROM [dbo].[ASSESSMENT_CONTACTS]\r\n  " +
+                        "where assessment_contact_id = 31331\r\n" +
+                    ") c ) e on d.assessment_id=e.assessment_id and d.userid = e.userid\r\n" +
+                    "where d.assessment_id is null";
+
+                cmd.CommandText = "select Assessment_Id from ASSESSMENTS where AssessmentCreatorId ;";
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.NextResult())
+                {
+                    Assert.IsNotNull(reader.GetString(2));
+                }
+                manager.CopyDBFromInstallationSource(mdf, ldf);
+            }
         }
 
     }
