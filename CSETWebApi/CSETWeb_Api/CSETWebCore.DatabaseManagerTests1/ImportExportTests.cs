3050c28af228cc33da036a04b914601dfb209348
Matt Winston <mrw0036@mix.wvu.edu>
2023-01-05 15:08:09 -0500

    went to an 'initialize' approach for getting auth

--- CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManagerTests1/ImportExportTests.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManagerTests1/ImportExportTests.cs
@@ -14,12 +14,51 @@ using System.Data;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Diagnostics;
 using UpgradeLibrary.Upgrade;
+using CSETWebCore.Api.Controllers;
+using CSETWebCore.Business.AssessmentIO.Export;
+using Microsoft.AspNetCore.Http;
+using CSETWebCore.Interfaces.Helpers;
+using CSETWebCore.Model.Authentication;
+using CSETWebCore.Helpers;
+using CSETWebCore.Business.Maturity;
+using DocumentFormat.OpenXml.InkML;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CSETWebCore.DatabaseManager.Tests
 {
     [TestClass()]
     public class ImportExportTests
     {
+        private CSETContext context;
+        private TokenManager tokenManager;
+        private LocalInstallationHelper localHelper;
+        private PasswordHash passwordHash;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<CSETContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("CSET_DB"));
+            this.context = new CSETContext(configuration);
+
+            var host = CreateDefaultBuilder().Build();
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            tokenManager = provider.GetRequiredService<TokenManager>();
+            localHelper = provider.GetRequiredService<LocalInstallationHelper>();
+            passwordHash = provider.GetRequiredService<PasswordHash>();
+           
+        }
+
         [TestMethod()]
         public void ImportAllAssessmentsTest()
         {   
@@ -37,46 +76,92 @@ namespace CSETWebCore.DatabaseManager.Tests
             //setup a destination file
             string clientCode = "DHS";
             string appCode = "CSET";
-            DbManager manager = new DbManager(new Version("12.0.0.16"), clientCode, appCode);
-            //run the same test twice and make sure that the number increment works
-            string mdf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWeb.mdf";
-            string ldf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWeb_log.ldf";
-            string conString = "Server=(localdb)\\mssqllocaldb;Integrated Security=true;AttachDbFileName=" + mdf;
-            using (SqlConnection conn = new SqlConnection(conString))
+
+            UserAuthentication userAuth = new UserAuthentication(passwordHash, null, localHelper, tokenManager, context);
+            Login login = new Login { Email = null, Password = null, TzOffset = "300", Scope = "CSET" };
+
+            string loginToken = userAuth.AuthenticateStandalone(login, tokenManager).Token;
+
+            var assessments = context.ASSESSMENTS.Select(x => x.Assessment_Id).AsEnumerable();
+
+            for( int i=0; i<assessments.Count(); i++ )
             {
-                conn.Open();
-                SqlCommand cmd = conn.CreateCommand();
+                int assess_Id = assessments.ElementAt(i);
 
-                cmd.CommandText = "INSERT INTO [dbo].[ASSESSMENT_CONTACTS]\r\n           " +
-                    "([Assessment_Id]\r\n\t\t    " +
-                    ",[PrimaryEmail]\r\n           " +
-                    ",[FirstName]\r\n           " +
-                    ",[LastName]\r\n           " +
-                    ",[Invited]\r\n           " +
-                    ",[AssessmentRoleId]\r\n           " +
-                    ",[UserId])\r\n" +
-                    "select distinct e.* from assessment_contacts d right join (\r\n" +
-                        "select a.assessment_id,c.* from assessments a, (\r\n" +
-                        "SELECT \r\n       " +
-                        "[PrimaryEmail]\r\n      " +
-                        ",[FirstName]\r\n      " +
-                        ",[LastName]\r\n      " +
-                        ",[Invited]\r\n      " +
-                        ",[AssessmentRoleId]\r\n      " +
-                        ",[UserId]\t  \r\n  " +
-                        "FROM [dbo].[ASSESSMENT_CONTACTS]\r\n  " +
-                        "where assessment_contact_id = 31331\r\n" +
-                    ") c ) e on d.assessment_id=e.assessment_id and d.userid = e.userid\r\n" +
-                    "where d.assessment_id is null";
-
-                cmd.CommandText = "select Assessment_Id from ASSESSMENTS where AssessmentCreatorId ;";
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.NextResult())
-                {
-                    Assert.IsNotNull(reader.GetString(2));
-                }
-                manager.CopyDBFromInstallationSource(mdf, ldf);
             }
+
+            //run the same test twice and make sure that the number increment works
+            //string mdf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWeb.mdf";
+            //string ldf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWeb_log.ldf";
+            //string conString = "Server=(localdb)\\mssqllocaldb;Integrated Security=true;AttachDbFileName=" + mdf;
+            //using (SqlConnection conn = new SqlConnection(conString))
+            //{
+            //    conn.Open();
+            //    SqlCommand cmd = conn.CreateCommand();
+
+                //cmd.CommandText = "INSERT INTO [dbo].[ASSESSMENT_CONTACTS]\r\n           " +
+                //    "([Assessment_Id]\r\n\t\t    " +
+                //    ",[PrimaryEmail]\r\n           " +
+                //    ",[FirstName]\r\n           " +
+                //    ",[LastName]\r\n           " +
+                //    ",[Invited]\r\n           " +
+                //    ",[AssessmentRoleId]\r\n           " +
+                //    ",[UserId])\r\n" +
+                //    "select distinct e.* from assessment_contacts d right join (\r\n" +
+                //        "select a.assessment_id,c.* from assessments a, (\r\n" +
+                //        "SELECT \r\n       " +
+                //        "[PrimaryEmail]\r\n      " +
+                //        ",[FirstName]\r\n      " +
+                //        ",[LastName]\r\n      " +
+                //        ",[Invited]\r\n      " +
+                //        ",[AssessmentRoleId]\r\n      " +
+                //        ",[UserId]\t  \r\n  " +
+                //        "FROM [dbo].[ASSESSMENT_CONTACTS]\r\n  " +
+                //        "where assessment_contact_id = 31331\r\n" +
+                //    ") c ) e on d.assessment_id=e.assessment_id and d.userid = e.userid\r\n" +
+                //    "where d.assessment_id is null";
+
+                //cmd.CommandText = "select * from ASSESSMENTS;";
+                //SqlDataReader reader = cmd.ExecuteReader();
+                //while (reader.Read())
+                //{
+                //    Assert.IsNotNull(reader.GetInt32(0)); //checks assess_id
+                //    Assert.IsNotNull(reader.GetInt32(2)); //checks creator_id
+                //    var context = new CSETContext();
+
+                //    var tokenManager = new TokenManager(null,null,null, context);
+                //    string token = tokenManager.GenerateToken(reader.GetInt32(2), null, "360", -1, null, null, "CSET");
+
+                //    //var tokenControl = new AuthController(null, null, null);
+
+                //    var exportControl = new AssessmentExportController(null, context, null);
+                //    //var loginObject = new Login("email", "pass", "offset", "scope");
+
+                //    //string tokenString = tokenControl.IssueToken(reader.GetInt32(0), -1, "*default*", 5000).ToString();
+                //    exportControl.ExportAssessment(token);
+                    
+                //}
+
+                //manager.
+
+            
+        }
+
+        private IHostBuilder CreateDefaultBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(app =>
+                {
+                    app.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddDbContext<CSETContext>(
+                        options => options.UseSqlServer("name=ConnectionStrings:CSETWeb"));
+                    services.AddTransient<IResourceHelper, ResourceHelper>();
+                    services.AddTransient<ILocalInstallationHelper, LocalInstallationHelper>();
+                    services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+                });
         }
 
     }
