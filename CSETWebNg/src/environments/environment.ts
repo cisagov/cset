451ac1271de4d6fdc82e4a03b1180f25cd817007
Marcus Goeckner <mbgoec2000@gmail.com>
2023-02-14 13:59:02 -0800

    Adding upgrade script to handle guids

--- CSETWebNg/src/environments/environment.ts
+++ CSETWebNg/src/environments/environment.ts
@@ -34,7 +34,7 @@ export const environment = {
   docUrl: 'https://localhost:5001/Documents/',
   appCode: 'CSET',
   visibleVersion: "11.5",
-  version: '12.0.1.3',
+  version: '12.0.1.4',
   helpContactEmail: 'cset@cisa.dhs.gov',
   helpContactPhone: ''
 };
