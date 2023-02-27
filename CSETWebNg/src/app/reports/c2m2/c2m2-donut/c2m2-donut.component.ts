b0338f40101c573fd95acc7980ea45e8532f9434
Marcus Goeckner <mbgoec2000@gmail.com>
2023-02-27 09:58:43 -0800

    Dummy donut

--- CSETWebNg/src/app/reports/c2m2/c2m2-donut/c2m2-donut.component.ts
+++ CSETWebNg/src/app/reports/c2m2/c2m2-donut/c2m2-donut.component.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+////////////////////////////////
+//
+//   Copyright 2023 Battelle Energy Alliance, LLC
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+////////////////////////////////
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-c2m2-donut',
@@ -7,9 +30,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class C2m2DonutComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  @Input() questionDistribution: any;
+  data: any[];
+  view: any[] = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+
+  colorScheme = {
+    domain: ['#265B94', '#90A5C7', '#F5DA8C', '#DCA237']
+  };
+
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.data = [
+      {
+        "name": "Fully Implemented",
+        "value": this.questionDistribution.fi
+      },
+      {
+        "name": "Largely Implemented",
+        "value": this.questionDistribution.li
+      },
+      {
+        "name": "Partially Implemented",
+        "value": this.questionDistribution.pi
+      },
+      {
+        "name": "Not Implemented",
+        "value": this.questionDistribution.ni
+      },
+      {
+        "name": "Unanswered",
+        "value": this.questionDistribution.u
+      },
+    ]
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
--- CSETWebNg/src/app/reports/c2m2/c2m2-report/c2m2-detailed-results/c2m2-detailed-results.component.ts
+++ CSETWebNg/src/app/reports/c2m2/c2m2-report/c2m2-detailed-results/c2m2-detailed-results.component.ts
@@ -35,4 +35,6 @@ export class C2m2DetailedResultsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  questionDistributionTest = {"title":"Manage IT and OT Asset Configurations","fi":2,"li":3,"pi":4,"ni":1,"u":5}
+
 }
