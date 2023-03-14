99640d9f41f00d0011f7e6f19d1e3bb49d57747e
Randy <randall.woods@inl.gov>
2023-03-14 11:16:27 -0600

    Add logic to api/users endpoint to populate a client

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Models/UserAdmin.cs
@@ -0,0 +1,13 @@
+﻿namespace CSETWebCore.Api.Models
+{
+    public class UserAdmin
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string PrimaryEmail { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
