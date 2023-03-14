53ddf9858200b6dcbe11c87649653e8f27726861
Marcus Goeckner <mbgoec2000@gmail.com>
2023-03-14 12:57:03 -0700

    Upgrade to v12.0.1.6 for TSA build

--- CSETWebNg/src/environments/environment.prod.ts
+++ CSETWebNg/src/environments/environment.prod.ts
@@ -28,7 +28,7 @@ export const environment = {
   docUrl: '',
   appCode: 'CSET',
   visibleVersion: '11.5',
-  version: '12.0.1.5',
+  version: '12.0.1.6',
   helpContactEmail: '',
   helpContactPhone: ''
 };
