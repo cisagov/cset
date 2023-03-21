2ec656063eb5cbaa85ebb27026de36742c71864d
Marcus Goeckner <mbgoec2000@gmail.com>
2023-03-21 11:54:53 -0700

    Tree shaking some monaco features we don't use

--- CSETWebNg/custom-webpack.config.ts
+++ CSETWebNg/custom-webpack.config.ts
@@ -2,6 +2,62 @@ import MonacoEditorWebpackPlugin from 'monaco-editor-webpack-plugin';
 import * as webpack from 'webpack';
 
 export default (config: webpack.Configuration) => {
-  config?.plugins?.push(new MonacoEditorWebpackPlugin());
+  config?.plugins?.push(new MonacoEditorWebpackPlugin({
+  // we can disable features that we end up not needing/using
+  features: [
+    'accessibilityHelp',
+    'format',
+    '!anchorSelect',
+    '!bracketMatching',
+    '!browser',
+    '!caretOperations',
+    '!clipboard',
+    '!codeAction',
+    '!codelens',
+    '!colorPicker',
+    '!comment',
+    '!contextmenu',
+    '!copyPaste',
+    '!cursorUndo',
+    '!dnd',
+    '!documentSymbols',
+    '!dropIntoEditor',
+    '!find',
+    '!folding',
+    '!fontZoom',
+    '!gotoError',
+    '!gotoLine',
+    '!gotoSymbol',
+    '!hover',
+    '!iPadShowKeyboard',
+    '!inPlaceReplace',
+    '!indentation',
+    '!inlayHints',
+    '!inlineCompletions',
+    '!inspectTokens',
+    '!lineSelection',
+    '!linesOperations',
+    '!linkedEditing',
+    '!links',
+    '!multicursor',
+    '!parameterHints',
+    '!quickCommand',
+    '!quickHelp',
+    '!quickOutline',
+    '!readOnlyMessage',
+    '!referenceSearch',
+    '!rename',
+    '!smartSelect',
+    '!snippet',
+    '!stickyScroll',
+    '!suggest',
+    '!toggleHighContrast',
+    '!toggleTabFocusMode',
+    '!tokenization',
+    '!unicodeHighlighter',
+    '!unusualLineTerminators'
+  ],
+  globalAPI: true
+}));
   return config;
 };
