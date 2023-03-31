c50ecaf0b76288f461a72c798fa01edc6cde67cf
Brett <43300357+MathesonBrett@users.noreply.github.com>
2023-03-31 11:12:37 -0600

    More efficient credit union looping

--- CSETWebNg/src/app/assessment/prepare/assessment-info/assessment-detail-ncua/assessment-detail-ncua.component.ts
+++ CSETWebNg/src/app/assessment/prepare/assessment-info/assessment-detail-ncua/assessment-detail-ncua.component.ts
@@ -187,11 +187,16 @@ export class AssessmentDetailNcuaComponent implements OnInit {
 
   }
 
-
+  /**
+   * 
+   */
   displayOptions (creditUnion: CreditUnionDetails): string {
     return creditUnion.name && creditUnion.name ? creditUnion.name : '';
   }
 
+  /**
+   * 
+   */
   filter (name: string): CreditUnionDetails[] {
     const filterValue = name.toLowerCase();
     return this.creditUnionOptions.filter(option => option.name.toLowerCase().includes(filterValue));
@@ -208,48 +213,68 @@ export class AssessmentDetailNcuaComponent implements OnInit {
       }
     }
 
-    for (let i = 0; i < this.creditUnionOptions.length; i++) {
-      if (e.target != null && e.target.value != null) {
-        if (e.target.value === this.creditUnionOptions[i].name) {
-          this.assessment.creditUnion = this.creditUnionOptions[i].name;
-          this.assessment.cityOrSiteName = this.creditUnionOptions[i].cityOrSite;
-          this.assessment.stateProvRegion = this.creditUnionOptions[i].state;
-          this.assessment.charter = this.creditUnionOptions[i].charter;
-          this.assessment.charterType = this.creditUnionOptions[i].charterType;
-          this.assessment.regionCode = this.creditUnionOptions[i].regionCode;
-
-          this.acetDashboard.creditUnionName = this.creditUnionOptions[i].name;
-          this.acetDashboard.charter = this.creditUnionOptions[i].charter;
-        } 
-        
-        if ((e.target.value.padStart(5, '0')) === (this.creditUnionOptions[i].charter.toString())) {
-          this.assessment.creditUnion = this.creditUnionOptions[i].name;
-          this.assessment.cityOrSiteName = this.creditUnionOptions[i].cityOrSite;
-          this.assessment.stateProvRegion = this.creditUnionOptions[i].state;
-          this.assessment.charter = this.creditUnionOptions[i].charter;
-          this.assessment.charterType = this.creditUnionOptions[i].charterType;
-          this.assessment.regionCode = this.creditUnionOptions[i].regionCode;
-
-          if (this.creditUnionOptions[i].charterType != 1) {
-            this.assessment.isE_StateLed = false;
-          }
-
-          this.acetDashboard.creditUnionName = this.creditUnionOptions[i].name;
-          this.acetDashboard.charter = this.creditUnionOptions[i].charter;
-        }
+    let i = 0;
+    while (i < this.creditUnionOptions.length) {
+      if (e.target.value == this.creditUnionOptions[i].name) {
+        console.log("Credit Union Name match found at: " + i);
+        this.populateAssessmentFields(i);
+        break;
       }
+
+      if ((e.target.value.padStart(5, '0')) === (this.creditUnionOptions[i].charter.toString())) {
+        console.log("Charter Number match found at: " + i);
+        this.populateAssessmentFields(i);
+        break;
+      }
+
+      i++;
     }
 
-    // if (e.target.value == true || e.target.value == false) {
-    //   this.toggleJoint();
-    // }
-
+    if (e.target.value === "") {
+      this.clearAssessmentFields();
+    }
+    
     this.createAssessmentName();
     this.setCharterPad();
 
     this.assessSvc.updateAssessmentDetails(this.assessment);
   }
 
+  /**
+  * 
+  */
+  populateAssessmentFields(i: number) {
+    this.assessment.creditUnion = this.creditUnionOptions[i].name;
+    this.assessment.cityOrSiteName = this.creditUnionOptions[i].cityOrSite;
+    this.assessment.stateProvRegion = this.creditUnionOptions[i].state;
+    this.assessment.charter = this.creditUnionOptions[i].charter;
+    this.assessment.charterType = this.creditUnionOptions[i].charterType;
+    this.assessment.regionCode = this.creditUnionOptions[i].regionCode;
+
+    if (this.creditUnionOptions[i].charterType != 1) {
+      this.assessment.isE_StateLed = false;
+    }
+
+    this.acetDashboard.creditUnionName = this.creditUnionOptions[i].name;
+    this.acetDashboard.charter = this.creditUnionOptions[i].charter;
+  }
+
+  /**
+  * 
+  */
+  clearAssessmentFields() {
+    this.assessment.creditUnion = "";
+    this.assessment.cityOrSiteName = "";
+    this.assessment.stateProvRegion = "";
+    this.assessment.charter = '00000';
+    this.assessment.charterType = 0;
+    this.assessment.regionCode = 0;
+    this.assessment.assets = '0';
+  }
+
+  /**
+  * 
+  */
   updateAssets(e: any) {
     // default Assessment Name if it is left empty
     if (!!this.assessment) {
@@ -270,6 +295,9 @@ export class AssessmentDetailNcuaComponent implements OnInit {
     this.assessSvc.updateAssessmentDetails(this.assessment);
   }
 
+  /**
+  * 
+  */
   updateOverride(e: any) {
     this.examOverride = e;
     if (e === "No Override") {
@@ -288,7 +316,7 @@ export class AssessmentDetailNcuaComponent implements OnInit {
 
   /**
    * 
-   */
+  */
   setCharterPad() {
     if (!!this.assessment) {
       this.assessment.charter = this.padLeft(this.assessment.charter, '0', 5);
@@ -305,6 +333,9 @@ export class AssessmentDetailNcuaComponent implements OnInit {
     return (String(padChar).repeat(size) + text).substr((size * -1), size);
   }
 
+  /**
+  * 
+  */
   isAnExamination() {
     if (this.assessment.maturityModel?.modelName === 'ISE') {
       return true;
@@ -347,7 +378,9 @@ export class AssessmentDetailNcuaComponent implements OnInit {
     }
   }
 
-
+  /**
+  * 
+  */
   toggleJoint() {
     this.ncuaSvc.ISE_StateLed = !this.ncuaSvc.ISE_StateLed;
 
@@ -356,7 +389,9 @@ export class AssessmentDetailNcuaComponent implements OnInit {
     this.assessSvc.updateAssessmentDetails(this.assessment);
   }
 
-
+  /**
+  * 
+  */
   regionTranslator(regionCode: number) {
     switch(regionCode) {
       case 1:
