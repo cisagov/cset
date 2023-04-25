0c7716c493b75ae8c1464d9cedd656cc5e37f815
Marcus Goeckner <marcus.goeckner@inl.gov>
2023-04-25 08:26:45 -0700

    Add 24 hour expiration date for temp passwords

--- CSETWebApi/CSETWeb_Api/CSETWebCore.Helpers/PasswordExpiration.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Helpers/PasswordExpiration.cs
@@ -16,7 +16,7 @@ namespace CSETWebCore.Helpers
     public class PasswordExpiration
     {
         private int _expiryTermDays = 180;
-
+        private int _tempPassExpiryTermDays = 1;
 
         /// <summary>
         /// Checks the password's age.
@@ -33,6 +33,13 @@ namespace CSETWebCore.Helpers
             if (hist.Created.AddDays(_expiryTermDays) < DateTime.UtcNow)
             {
                 // the password is old
+                return true;
+            }
+
+            var temp = context.TEMP_PASSWORDS.Where(x => x.UserId == userId && x.Password == passwordHash).FirstOrDefault();
+            if (temp.GeneratedDate.AddDays(_tempPassExpiryTermDays) < DateTime.UtcNow) 
+            {
+                // temp pass expired
                 return true;
             }
 
