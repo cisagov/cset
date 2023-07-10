9c93dbc1ecc1cf105ef041a8ba1c5ff6ba4fd3a1
Marcus Goeckner <marcus.goeckner@inl.gov>
2023-07-10 15:32:58 -0700

    Update to version 12.0.2.4 for new CSET release

--- CSETWebNg/src/environments/environment.ts
+++ CSETWebNg/src/environments/environment.ts
@@ -33,8 +33,8 @@ export const environment = {
   apiUrl: 'https://localhost:5001/api/',
   docUrl: 'https://localhost:5001/Documents/',
   appCode: 'CSET',
-  visibleVersion: "12.0.2.3",
-  version: '12.0.2.3',
+  visibleVersion: "12.0.2.4",
+  version: '12.0.2.4',
   helpContactEmail: 'cset@cisa.dhs.gov',
   helpContactPhone: ''
 };
