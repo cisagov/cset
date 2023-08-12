19d699e636be36f433e7545b8b17e6cabb8dbe06
Jason Kuipers <jason.kuipers@inl.gov>
2023-08-12 06:49:53 -0700

    sd scoring matrix

---
+++ CSETWebNg/src/app/reports/tsa-sd/tsa-sd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tsa-sd',
+  templateUrl: './tsa-sd.component.html',
+  styleUrls: ['./tsa-sd.component.scss']
+})
+export class TsaSdComponent {
+
+}
