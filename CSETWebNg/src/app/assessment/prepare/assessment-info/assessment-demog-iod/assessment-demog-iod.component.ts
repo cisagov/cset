d58b532ab079ac3a58660cc5e9ed6e65c5ff2fe1
LaddieZeigler <85979055+LaddieZeigler@users.noreply.github.com>
2023-10-17 10:56:25 -0600

    First pass import/export buttons

--- CSETWebNg/src/app/assessment/prepare/assessment-info/assessment-demog-iod/assessment-demog-iod.component.ts
+++ CSETWebNg/src/app/assessment/prepare/assessment-info/assessment-demog-iod/assessment-demog-iod.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-assessment-demog-iod',
@@ -7,15 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AssessmentDemogIodComponent {
 
-importClick(){
-console.log("import test")
-}
+  eventImportExport: Subject<string> = new Subject<string>();
+
+  importClick(evt){
+    const fileReader = new FileReader();
+    fileReader.readAsText(evt.target.files[0], "UTF-8");
+    fileReader.onload = () => {
+    this.eventImportExport.next(JSON.parse(String(fileReader.result)));
+    }
+    fileReader.onerror = (error) => {
+      console.log(error);
+    }
+  }
 
 
 
 
-exportClick(){
-console.log("export test")
+  exportClick(){
+    this.eventImportExport.next("export");
 
 }
 
--- CSETWebNg/src/app/assessment/prepare/assessment-info/demographics-iod/demographics-iod.component.ts
+++ CSETWebNg/src/app/assessment/prepare/assessment-info/demographics-iod/demographics-iod.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DemographicIodService } from '../../../../services/demographic-iod.service';
 import { DemographicsIod } from '../../../../models/demographics-iod.model';
+import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+import { saveAs } from 'file-saver';
+
 
 @Component({
   selector: 'app-demographics-iod',
@@ -8,12 +12,19 @@ import { DemographicsIod } from '../../../../models/demographics-iod.model';
   styleUrls: ['./demographics-iod.component.scss']
 })
 export class DemographicsIodComponent implements OnInit {
+
+  @Input() events: Observable<void>;
+
+  private eventsSubscription: any;
+  
   /**
    * The principal model for this page
    */
   demographicData: DemographicsIod = {};
 
-  constructor(public demoSvc: DemographicIodService) {}
+  constructor(public demoSvc: DemographicIodService, 
+    private sanitizer: DomSanitizer
+    ) {}
 
   /**
    *
@@ -22,6 +33,45 @@ export class DemographicsIodComponent implements OnInit {
     this.demoSvc.getDemographics().subscribe((data: any) => {
       this.demographicData = data;
     });
+
+    this.eventsSubscription = this.events.subscribe((importExportFlag) => this.importExport(importExportFlag));
+
+  }
+
+
+  ngOnDestroy() {
+    this.eventsSubscription.unsubscribe();
+  }
+
+  importProfile(file){
+    console.log(file)
+  }
+
+  importExport(importExportFlag){
+    if (importExportFlag == "import"){
+      
+    } else {
+        this.demographicData.version = 1;
+        
+        if (this.demographicData.organizationName){
+          var FileSaver = require('file-saver');
+          var demoString = JSON.stringify(this.demographicData);
+          const blob = new Blob([demoString], { type: 'application/json' });
+          
+          try {
+            FileSaver.saveAs(blob, this.demographicData.organizationName + ".json");
+          } catch (error) {
+            console.error("Error during file download:", error);
+          }
+
+        } else {
+          // Find better way to send notification to user
+          // Verify organization name can be used as file name
+          console.log("Organization name required")
+        }
+        
+      }
+    
   }
 
   /**
