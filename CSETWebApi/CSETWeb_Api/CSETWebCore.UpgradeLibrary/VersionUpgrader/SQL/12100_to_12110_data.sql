47f123b37b1f319e33ac02d7b3e0a47edac32a43
Marcus Goectau <marcus.goectau@inl.gov>
2023-11-30 13:24:53 -0800

    Update data upgrade script

--- CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/SQL/12100_to_12110_data.sql
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/SQL/12100_to_12110_data.sql
@@ -9,7 +9,7 @@ to synchronize it with:
 
 You are recommended to back up your database before running this script
 
-Script created by SQL Data Compare version 14.10.9.22680 from Red Gate Software Ltd at 11/29/2023 9:18:05 AM
+Script created by SQL Data Compare version 14.10.9.22680 from Red Gate Software Ltd at 11/30/2023 1:24:08 PM
 
 */
 		
@@ -32,6 +32,13 @@ ALTER TABLE [dbo].[REQUIREMENT_SOURCE_FILES] NOCHECK CONSTRAINT [FK_REQUIREMENT_
 PRINT(N'Drop constraints from [dbo].[REQUIREMENT_REFERENCES]')
 ALTER TABLE [dbo].[REQUIREMENT_REFERENCES] NOCHECK CONSTRAINT [FK_REQUIREMENT_REFERENCES_GEN_FILE]
 ALTER TABLE [dbo].[REQUIREMENT_REFERENCES] NOCHECK CONSTRAINT [FK_REQUIREMENT_REFERENCES_NEW_REQUIREMENT]
+
+PRINT(N'Drop constraints from [dbo].[GEN_FILE_LIB_PATH_CORL]')
+ALTER TABLE [dbo].[GEN_FILE_LIB_PATH_CORL] NOCHECK CONSTRAINT [FK_GEN_FILE_LIB_PATH_CORL_GEN_FILE]
+ALTER TABLE [dbo].[GEN_FILE_LIB_PATH_CORL] NOCHECK CONSTRAINT [FK_GEN_FILE_LIB_PATH_CORL_REF_LIBRARY_PATH]
+
+PRINT(N'Drop constraints from [dbo].[FILE_KEYWORDS]')
+ALTER TABLE [dbo].[FILE_KEYWORDS] NOCHECK CONSTRAINT [FILE_KEYWORDS_GEN_FILE_FK]
 
 PRINT(N'Drop constraints from [dbo].[MATURITY_QUESTIONS]')
 ALTER TABLE [dbo].[MATURITY_QUESTIONS] NOCHECK CONSTRAINT [FK__MATURITY___Matur__5B638405]
@@ -69,8 +76,89 @@ ALTER TABLE [dbo].[MATURITY_SUB_MODEL_QUESTIONS] NOCHECK CONSTRAINT [FK_MATURITY
 PRINT(N'Drop constraint FK_TTP_MAT_QUESTION_MATURITY_QUESTIONS from [dbo].[TTP_MAT_QUESTION]')
 ALTER TABLE [dbo].[TTP_MAT_QUESTION] NOCHECK CONSTRAINT [FK_TTP_MAT_QUESTION_MATURITY_QUESTIONS]
 
-PRINT(N'Update row in [dbo].[MATURITY_QUESTIONS]')
+PRINT(N'Drop constraints from [dbo].[GEN_FILE]')
+ALTER TABLE [dbo].[GEN_FILE] NOCHECK CONSTRAINT [FK_GEN_FILE_FILE_REF_KEYS]
+ALTER TABLE [dbo].[GEN_FILE] NOCHECK CONSTRAINT [FK_GEN_FILE_FILE_TYPE]
+
+PRINT(N'Drop constraint FK_MATURITY_REFERENCES_GEN_FILE from [dbo].[MATURITY_REFERENCES]')
+ALTER TABLE [dbo].[MATURITY_REFERENCES] NOCHECK CONSTRAINT [FK_MATURITY_REFERENCES_GEN_FILE]
+
+PRINT(N'Drop constraint FK_MATURITY_SOURCE_FILES_GEN_FILE from [dbo].[MATURITY_SOURCE_FILES]')
+ALTER TABLE [dbo].[MATURITY_SOURCE_FILES] NOCHECK CONSTRAINT [FK_MATURITY_SOURCE_FILES_GEN_FILE]
+
+PRINT(N'Drop constraint FK_SET_FILES_GEN_FILE from [dbo].[SET_FILES]')
+ALTER TABLE [dbo].[SET_FILES] NOCHECK CONSTRAINT [FK_SET_FILES_GEN_FILE]
+
+PRINT(N'Update rows in [dbo].[MATURITY_QUESTIONS]')
+UPDATE [dbo].[MATURITY_QUESTIONS] SET [Supplemental_Info]=N'Operationally sensitive - HIPAA, PII, network diagrams, Critical Services inventories, etc.' WHERE [Mat_Question_Id] = 6004
 UPDATE [dbo].[MATURITY_QUESTIONS] SET [Question_Text]=N'<p>Are there mitigation measures in place for accounts that do not comply with the above requirement (AC-3C.Q3)? Mitigation measures may include:</p> <ul><li>Physical security measures</li><li>Increased monitoring of accounts</li></ul> <p>The above measures should be clearly defined in the CIP.</p>' WHERE [Mat_Question_Id] = 6836
+PRINT(N'Operation applied to 2 rows out of 2')
+
+PRINT(N'Update rows in [dbo].[GEN_FILE]')
+UPDATE [dbo].[GEN_FILE] SET [Summary]=N'The NCUA Board (Board) is amending its regulations regarding loans to members and lines of credit to members to reduce regulatory burden, improve clarity, and make compliance easier. The amendments make the NCUA’s regulations more user friendly by: (1) identifying in one section all of the various maturity limits applicable to federal credit union (FCU) loans; (2) stating that the maturity date for a new loan under generally accepted accounting principles (GAAP) is calculated from the origination date of the new loan; and (3) more clearly expressing the limits for loans to a single borrower or group of associated borrowers.' WHERE [Gen_File_Id] = 6091
+UPDATE [dbo].[GEN_FILE] SET [Summary]=N'Services, facilities, personnel, or equipment shared with any party shall be supported by a written contract, with the duties and responsibilities of each party specified and the allocation of service fee/expenses fully supported and documented.' WHERE [Gen_File_Id] = 6109
+UPDATE [dbo].[GEN_FILE] SET [Summary]=N'NCUA is issuing a final rule governing corporate credit unions. The rule strengthens capital requirements, establishes parameters to ensure that the risk on corporate credit union balance sheets is adequately managed, provides for corporate credit unions with more developed systems and infrastructures to take more planned and controlled risk, and sets forth special rules for wholesale corporate credit unions.' WHERE [Gen_File_Id] = 6110
+UPDATE [dbo].[GEN_FILE] SET [Summary]=N'The provisions of this part apply to federal credit unions, federally insured state-chartered credit unions, and credit unions making application for insurance of accounts pursuant to title II of the Act, unless the context of a provision indicates its application is otherwise limited. This part prescribes various requirements for obtaining and maintaining federal insurance and the payment of insurance premiums and capitalization deposit. Subpart A of this part contains substantive requirements that are not codified elsewhere in this chapter. Subpart B of this part lists additional regulations, set forth elsewhere in this chapter as applying to federal credit unions, that also apply to federally insured state-chartered credit unions. As used in this part, “insured credit union” means a credit union whose accounts are insured by the National Credit Union Share Insurance Fund (NCUSIF).' WHERE [Gen_File_Id] = 6111
+UPDATE [dbo].[GEN_FILE] SET [Summary]=N'12 CFR 748.0 is a section of the National Credit Union Administration (NCUA) regulations. It outlines the requirements for a federally insured credit union to develop a written security program within 90 days of the effective date of insurance. The security program should be designed to protect each credit union office from robberies, burglaries, larcenies, and embezzlement.' WHERE [Gen_File_Id] = 6112
+UPDATE [dbo].[GEN_FILE] SET [Summary]=N'This part describes the obligations of all federally insured credit unions to maintain a records preservation program to identify, store and reconstruct vital records in the event that the credit union''s records are destroyed and provides recommendations for restoring vital member services. All credit unions must have a written program that includes plans for safeguarding records and reconstructing vital records. To complement these plans, it is recommended a credit union develop a method for restoring vital member services in the event of a catastrophic act as defined in § 748.1(b) of this chapter. Additionally, the regulation establishes flexibility in the format credit unions may use for maintaining writings, records or information required by other NCUA regulations.' WHERE [Gen_File_Id] = 6113
+PRINT(N'Operation applied to 6 rows out of 6')
+
+PRINT(N'Add rows to [dbo].[FILE_KEYWORDS]')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6091, N'12')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6091, N'12 CFR 701')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6091, N'701')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6091, N'cfr')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6109, N'12')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6109, N'12 CFR 704.16')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6109, N'704.16')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6109, N'cfr')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6110, N'12')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6110, N'12 CFR 704')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6110, N'704')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6110, N'cfr')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6111, N'12')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6111, N'12 CFR 741')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6111, N'741')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6111, N'cfr')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6112, N'12')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6112, N'12 CFR 748')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6112, N'748')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6112, N'cfr')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6113, N'12')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6113, N'12 CFR 749')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6113, N'749')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6113, N'cfr')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'Diligence')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'Due')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'due diligence')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'Due Diligence over Third Party Service Providers')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'party')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'providers')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'service')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'third')
+INSERT INTO [dbo].[FILE_KEYWORDS] ([Gen_File_Id], [Keyword]) VALUES (6118, N'third-party')
+PRINT(N'Operation applied to 33 rows out of 33')
+
+PRINT(N'Add rows to [dbo].[GEN_FILE_LIB_PATH_CORL]')
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6088, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6089, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6091, 496)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6092, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6094, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6095, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6097, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6098, 500)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6099, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6106, 500)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6109, 496)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6110, 496)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6111, 496)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6112, 496)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (6113, 496)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (7069, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (7070, 516)
+INSERT INTO [dbo].[GEN_FILE_LIB_PATH_CORL] ([Gen_File_Id], [Lib_Path_Id]) VALUES (7071, 516)
+PRINT(N'Operation applied to 18 rows out of 18')
 
 PRINT(N'Add rows to [dbo].[REQUIREMENT_REFERENCES]')
 INSERT INTO [dbo].[REQUIREMENT_REFERENCES] ([Requirement_Id], [Gen_File_Id], [Section_Ref], [Page_Number], [Destination_String], [Sequence]) VALUES (36403, 691, N'ID.AM-01', NULL, NULL, NULL)
@@ -298,6 +386,13 @@ PRINT(N'Add constraints to [dbo].[REQUIREMENT_REFERENCES]')
 ALTER TABLE [dbo].[REQUIREMENT_REFERENCES] CHECK CONSTRAINT [FK_REQUIREMENT_REFERENCES_GEN_FILE]
 ALTER TABLE [dbo].[REQUIREMENT_REFERENCES] CHECK CONSTRAINT [FK_REQUIREMENT_REFERENCES_NEW_REQUIREMENT]
 
+PRINT(N'Add constraints to [dbo].[GEN_FILE_LIB_PATH_CORL]')
+ALTER TABLE [dbo].[GEN_FILE_LIB_PATH_CORL] CHECK CONSTRAINT [FK_GEN_FILE_LIB_PATH_CORL_GEN_FILE]
+ALTER TABLE [dbo].[GEN_FILE_LIB_PATH_CORL] CHECK CONSTRAINT [FK_GEN_FILE_LIB_PATH_CORL_REF_LIBRARY_PATH]
+
+PRINT(N'Add constraints to [dbo].[FILE_KEYWORDS]')
+ALTER TABLE [dbo].[FILE_KEYWORDS] CHECK CONSTRAINT [FILE_KEYWORDS_GEN_FILE_FK]
+
 PRINT(N'Add constraints to [dbo].[MATURITY_QUESTIONS]')
 ALTER TABLE [dbo].[MATURITY_QUESTIONS] CHECK CONSTRAINT [FK__MATURITY___Matur__5B638405]
 ALTER TABLE [dbo].[MATURITY_QUESTIONS] WITH CHECK CHECK CONSTRAINT [FK_MATURITY_QUESTIONS_MATURITY_GROUPINGS]
@@ -314,5 +409,12 @@ ALTER TABLE [dbo].[MATURITY_REFERENCES] CHECK CONSTRAINT [FK_MATURITY_REFERENCES
 ALTER TABLE [dbo].[MATURITY_SOURCE_FILES] CHECK CONSTRAINT [FK_MATURITY_SOURCE_FILES_MATURITY_QUESTIONS]
 ALTER TABLE [dbo].[MATURITY_SUB_MODEL_QUESTIONS] WITH CHECK CHECK CONSTRAINT [FK_MATURITY_SUB_MODEL_QUESTIONS_MATURITY_QUESTIONS]
 ALTER TABLE [dbo].[TTP_MAT_QUESTION] WITH CHECK CHECK CONSTRAINT [FK_TTP_MAT_QUESTION_MATURITY_QUESTIONS]
+
+PRINT(N'Add constraints to [dbo].[GEN_FILE]')
+ALTER TABLE [dbo].[GEN_FILE] WITH CHECK CHECK CONSTRAINT [FK_GEN_FILE_FILE_REF_KEYS]
+ALTER TABLE [dbo].[GEN_FILE] WITH CHECK CHECK CONSTRAINT [FK_GEN_FILE_FILE_TYPE]
+ALTER TABLE [dbo].[MATURITY_REFERENCES] CHECK CONSTRAINT [FK_MATURITY_REFERENCES_GEN_FILE]
+ALTER TABLE [dbo].[MATURITY_SOURCE_FILES] CHECK CONSTRAINT [FK_MATURITY_SOURCE_FILES_GEN_FILE]
+ALTER TABLE [dbo].[SET_FILES] WITH CHECK CHECK CONSTRAINT [FK_SET_FILES_GEN_FILE]
 COMMIT TRANSACTION
 GO
