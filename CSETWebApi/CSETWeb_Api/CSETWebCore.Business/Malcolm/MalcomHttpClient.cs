bcb57bdc2911275ad57ec812762e842c587e5801
Barry Hansen <5023364+inlguy@users.noreply.github.com>
2024-02-22 10:41:37 -0700

    malcom test

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Business/Malcolm/MalcomHttpClient.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Business.Malcolm
+{
+    public class MalcomHttpClient
+    {
+        private readonly HttpClient _client;
+
+        public MalcomHttpClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<string> PostAndSaveResponseAsJsonAsync(string url, string postValue)
+        {
+            var content = new StringContent(postValue, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseJson = await response.Content.ReadAsStringAsync();
+                return responseJson;
+            }
+
+            return null;
+        }
+
+        public async Task<string> getMalcomData(string ipaddress)
+        {
+            var data = "{\"from\":\"5 years ago\",\"to\": \"now\"}";
+            var responseJson = await PostAndSaveResponseAsJsonAsync("https://"+ipaddress+"/mapi/agg/source.ip,source.device.role,destination.ip,destination.device.role", data);
+            return responseJson;
+        } 
+    }
+}
---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.BusinessTests/Malcolm/MalcomHttpClientTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSETWebCore.Business.Malcolm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Business.Malcolm.Tests
+{
+    [TestClass()]
+    public class MalcomHttpClientTests
+    {
+        [TestMethod()]
+        public async void getMalcomDataTest()
+        {
+            MalcomHttpClient malcomHttp = new MalcomHttpClient();
+            string test = await malcomHttp.getMalcomData("127.0.0.1");
+            Assert.IsNotNull(test);
+            Console.WriteLine(test);
+        }
+    }
+}
