0f2457a8486a1404a7af6714867d3d399728ffde
Matthew Winston <Matthew.Winston@inl.gov>
2024-02-26 11:31:33 -0500

    Taking out firewall stuff from Malcolm rules

--- CSETWebApi/CSETWeb_Api/CSETWebCore.Business/Diagram/analysis/rules/MalcolmRules/Rule9.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Business/Diagram/analysis/rules/MalcolmRules/Rule9.cs
@@ -34,6 +34,16 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers.Diagram.analysis.rules.Malc
 
         public List<IDiagramAnalysisNodeMessage> Evaluate()
         {
+            CheckRule9();
+            return this.Messages;
+        }
+
+        /// <summary>
+        /// Check for IDS (Malcolm can't passively detect a firewall,
+        /// so we don't worry about how the IDS is connected)
+        /// </summary>
+        private void CheckRule9()
+        {
             bool idsDetected = false;
             var allNodes = network.Nodes.Values.ToList();
             foreach (var node in allNodes)
@@ -44,96 +54,12 @@ namespace CSETWeb_Api.BusinessLogic.BusinessManagers.Diagram.analysis.rules.Malc
                 }
             }
 
-            if (!idsDetected) 
-            {
-                return this.Messages;
-            }
-
-            var firewalls = network.Nodes.Values.Where(x => x.IsFirewall).ToList();
-
-            if (firewalls.Count == 0)
+            if (!idsDetected)
             {
                 String text = String.Format(rule9, allNodes[0].ComponentName).Replace("\n", " ");
                 SetNodeMessage(allNodes[0], text, 9); // 9 because rule9 was violated
             }
-
-            else
-            {
-                foreach (var firewall in firewalls)
-                {
-                    Visited.Clear();
-                    CheckRule9(firewall);
-                }
-            }
-            
-            return this.Messages;
         }
 
-        private HashSet<String> Visited = new HashSet<string>();
-
-        /// <summary>
-        /// Check Firewall for IPS and IDS past the firewall
-        /// </summary>
-        /// <param name="multiServiceComponent"></param>
-        /// <param name="visitedNodes"></param>
-        private void CheckRule9(NetworkComponent firewall)
-        {
-            // This code is here because component can be a multiple service component that is IDS
-            if (firewall.IsIDS)
-            {
-                return;
-            }
-
-            //recurse through all the edges and see if you can find an ids or ips
-            //if it is in the same zone             
-            foreach (NetworkComponent child in firewall.Connections)
-            {
-                if (child.IsInSameZone(firewall))
-                {
-                    if (child.IsIDS)
-                    {
-                        return;
-                    }
-                    else if (RecurseDownConnections(child, firewall))
-                    {
-                        return;
-                    }
-                }
-            }
-
-            String componentName = "unnamed";
-            if (!String.IsNullOrWhiteSpace(firewall.ComponentName))
-            {
-                componentName = firewall.ComponentName;
-            }
-
-            String text = String.Format(rule9, componentName).Replace("\n", " ");
-            SetNodeMessage(firewall, text, 9); // 9 because rule9 was violated
-        }
-
-        private bool RecurseDownConnections(NetworkComponent itemToCheck, NetworkComponent firewall)
-        {
-            foreach (NetworkComponent child in itemToCheck.Connections)
-            {
-                if (Visited.Add(child.ID))
-                {
-                    //Trace.WriteLine("->" + child.ComponentName + ":" + firewall.ComponentName);
-                    if (child.IsInSameZone(firewall))
-                    {
-                        if (child.IsIDS)
-                        {
-                            //Trace.WriteLine("Found it");
-                            return true;
-                        }
-                        else if (RecurseDownConnections(child, firewall))
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
     }
 }
