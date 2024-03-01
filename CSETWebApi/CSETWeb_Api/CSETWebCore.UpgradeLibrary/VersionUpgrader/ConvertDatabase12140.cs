16c0cc34643f823de44282d396a37d84ed764ce9
Marcus Goectau <marcus.goectau@inl.gov>
2024-03-01 14:40:32 -0800

    Update version

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/ConvertDatabase12140.cs
@@ -0,0 +1,37 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2024 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using System;
+using Microsoft.Data.SqlClient;
+using System.IO;
+namespace UpgradeLibrary.Upgrade
+{
+    internal class ConvertDatabase12140 : ConvertSqlDatabase
+    {
+        public ConvertDatabase12140(string path) : base(path)
+        {
+            myVersion = new Version("12.1.4.0");
+        }
+
+        /// <summary>
+        /// Runs the database update script
+        /// </summary>
+        /// <param name="conn"></param>
+        public override void Execute(SqlConnection conn)
+        {
+            try
+            {
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12130_to_12140.sql"), conn);
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12130_to_12140_data.sql"), conn);
+                this.UpgradeToVersionLocalDB(conn, myVersion);
+            }
+            catch (Exception e)
+            {
+                throw new DatabaseUpgradeException("Error in upgrading database version 12.1.3.0 to 12.1.4.0: " + e.Message);
+            }
+        }
+    }
+}
--- CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/VersionUpgrader.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/VersionUpgrader.cs
@@ -106,6 +106,7 @@ namespace UpgradeLibrary.Upgrade
             converters.Add("12.1.1.0", new ConvertDatabase12120(path));
             converters.Add("12.1.2.0", new ConvertDatabase12121(path));
             converters.Add("12.1.2.1", new ConvertDatabase12130(path));
+            converters.Add("12.1.3.0", new ConvertDatabase12140(path));
         }
 
         public void UpgradeOnly(Version currentVersion, string tempConnect)
