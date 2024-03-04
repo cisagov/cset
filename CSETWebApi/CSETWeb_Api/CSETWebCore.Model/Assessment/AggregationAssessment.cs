795f8b16e0fd37482999d70bd176937653887509
Jason E. Kuipers <89001003597157@fedidcard.gov>
2024-03-04 10:11:37 -0700

    query for pull aggregation data

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Assessment/AggregationAssessment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSETWebCore.DataLayer.Model;
+
+namespace CSETWebCore.Model.Assessment
+{
+    public class AggregationAssessment
+    {
+        public ASSESSMENTS Assessment { get; set; }
+        public List<ANSWER> Answers { get; set; }
+        public DEMOGRAPHICS Demographics { get; set; }
+        public List<DOCUMENT_FILE> Documents { get; set; }
+        public List<FINDING> Findings { get; set; }
+    }
+}
