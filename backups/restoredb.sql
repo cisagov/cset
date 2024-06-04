49459f4cb5412b703491135cd6f23c2d841132bc
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2024-06-04 14:22:51 -0700

    feature: restoredb from backup script

--- backups/restoredb.sql
+++ backups/restoredb.sql
@@ -1,5 +1,11 @@
-RESTORE DATABASE CSETWeb.bak
+ALTER DATABASE CSETWeb
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE
+GO
+
+RESTORE DATABASE CSETWeb
 FROM DISK = '/var/opt/mssql/backup/CSETWeb.bak'
-WITH MOVE 'CSETWeb' TO '/var/opt/mssql/data/CSETWeb.mdf'
+WITH REPLACE,
+MOVE 'CSETWeb' TO '/var/opt/mssql/data/CSETWeb.mdf',
 MOVE 'CSETWeb_Log' TO '/var/opt/mssql/data/CSETWeb_Log.ldf'
 GO
