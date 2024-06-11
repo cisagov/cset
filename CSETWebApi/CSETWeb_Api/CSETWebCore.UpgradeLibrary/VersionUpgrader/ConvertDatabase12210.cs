0801c2e4d9d7dd38f53ad9d781d16ae8096b2949
Marcus Goectau <marcus.goectau@inl.gov>
2024-06-11 11:18:50 -0700

    Final build updates

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/ConvertDatabase12210.cs
@@ -0,0 +1,37 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2024 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using System;
+using Microsoft.Data.SqlClient;
+using System.IO;
+namespace UpgradeLibrary.Upgrade
+{
+    internal class ConvertDatabase12210 : ConvertSqlDatabase
+    {
+        public ConvertDatabase12210(string path) : base(path)
+        {
+            myVersion = new Version("12.2.1.0");
+        }
+
+        /// <summary>
+        /// Runs the database update script
+        /// </summary>
+        /// <param name="conn"></param>F
+        public override void Execute(SqlConnection conn)
+        {
+            try
+            {
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12200_to_12210.sql"), conn);
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12200_to_12210_data.sql"), conn);
+                this.UpgradeToVersionLocalDB(conn, myVersion);
+            }
+            catch (Exception e)
+            {
+                throw new DatabaseUpgradeException("Error in upgrading database version 12.2.0.0 to 12.2.1.0: " + e.Message);
+            }
+        }
+    }
+}
