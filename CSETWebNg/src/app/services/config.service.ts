031cdf6539fa96ce7863f2336472e51c2a2186c2
Marcus Goectau <marcus.goectau@inl.gov>
2024-06-17 16:04:13 -0700

    fixing up logic for libraryUrl

--- CSETWebNg/src/app/services/config.service.ts
+++ CSETWebNg/src/app/services/config.service.ts
@@ -172,7 +172,7 @@ export class ConfigService {
 
 
     const localStorageApiUrl = localStorage.getItem('apiUrl');
-    const localStoragelibraryUrl = localStorage.getItem('libraryUrl');
+    const localStorageLibraryUrl = localStorage.getItem('libraryUrl');
     if (!!localStorageApiUrl) {
       this.apiUrl = localStorageApiUrl + '/' + this.config.api.apiIdentifier + '/';
       this.docUrl = localStorageApiUrl + '/' + this.config.api.documentsIdentifier + '/';
@@ -181,29 +181,27 @@ export class ConfigService {
       this.docUrl = apiProtocol + this.config.api.host + apiPort + '/' + this.config.api.documentsIdentifier + '/';
     }
 
-    if (!!localStoragelibraryUrl) {
-      this.libraryUrl = localStorageApiUrl + '/library/';
-    } else {
-      this.libraryUrl = apiProtocol + this.config.api.host + apiPort + '/' + this.config.api.apiIdentifier + '/library/';
-    }
-
-    this.refDocUrl = this.libraryUrl + 'doc/';
-
     this.appUrl = appProtocol + this.config.app.host + appPort;
     this.analyticsUrl = "http://localhost:5278/";
     this.helpContactEmail = this.config.helpContactEmail;
     this.helpContactPhone = this.config.helpContactPhone;
     this.csetGithubApiUrl = this.config.csetGithubApiUrl;
 
-     // configure the reference document URL if the "library" property is defined
-     const rl = this.config.library;
-     if (!!rl) {
-       const rlProtocol = rl.protocol + '://';
-       const rlPort = !!rl.port ? ':' + rl.port : '';
-       this.libraryUrl = rlProtocol + rl.host + rlPort + '/' + (rl.apiIdentifier ?? 'api') + '/library/'
-       this.refDocUrl = this.libraryUrl + 'doc/';
-     }
+    // configure the reference document URL if the "library" property is defined
+    // or if passed in as query param and stored as local storage variable. Local storage should
+    // take precedence over the config file, since Electron uses it to dynamically set the port.
+    const rl = this.config.library;
+    if (!!localStorageLibraryUrl) {
+      this.libraryUrl = localStorageLibraryUrl + '/library/';
+    } else if (!!rl) {
+      const rlProtocol = rl.protocol + '://';
+      const rlPort = !!rl.port ? ':' + rl.port : '';
+      this.libraryUrl = rlProtocol + rl.host + rlPort + '/' + (rl.apiIdentifier ?? 'api') + '/library/'
+    } else {
+      this.libraryUrl = apiProtocol + this.config.api.host + apiPort + '/' + this.config.api.apiIdentifier + '/library/';
+    }
 
+    this.refDocUrl = this.libraryUrl + 'doc/';
 
     this.galleryLayout = this.config.galleryLayout?.toString() || 'CSET';
     this.mobileEnvironment = this.config.mobileEnvironment;
