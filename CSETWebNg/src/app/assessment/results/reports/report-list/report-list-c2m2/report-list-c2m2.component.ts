3ea0c0f1c9ff511fff624d7f6afe34e84aef146c
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2024-06-19 08:29:13 -0600

    Updated c2m2 report layout

---
+++ CSETWebNg/src/app/assessment/results/reports/report-list/report-list-c2m2/report-list-c2m2.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-list-c2m2',
+  templateUrl: './report-list-c2m2.component.html'
+})
+export class ReportListC2M2Component {
+
+  reportList = [
+    {
+      linkUrl: "c2m2Report"
+    }
+  ]
+
+}
