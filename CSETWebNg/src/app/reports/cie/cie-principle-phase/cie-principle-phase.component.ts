e22d9f755870fb4ec9778edad175c3c710f62318
Marcus Goectau <marcus.goectau@inl.gov>
2024-06-26 16:48:05 -0700

    More electron download fixes for CIE

--- CSETWebNg/src/app/reports/cie/cie-principle-phase/cie-principle-phase.component.ts
+++ CSETWebNg/src/app/reports/cie/cie-principle-phase/cie-principle-phase.component.ts
@@ -49,7 +49,7 @@ export class CiePrinciplePhaseComponent {
   ) { }
 
   ngOnInit(): void {
-    this.titleService.setTitle("Export Principle-Phase CIE-CSET - Report");
+    this.titleService.setTitle("Export Principle-Phase CIE - Report");
 
     this.cieSvc.getCieAllQuestions().subscribe(
       (r: any) => {
@@ -128,7 +128,7 @@ export class CiePrinciplePhaseComponent {
     }
     return combinedClass;
   }
-  
+
   /**
    *
    */
@@ -136,7 +136,7 @@ export class CiePrinciplePhaseComponent {
     // get short-term JWT from API
     this.authSvc.getShortLivedToken().subscribe((response: any) => {
       const url = this.fileSvc.downloadUrl + doc.document_Id + "?token=" + response.token;
-      window.open(url, "_blank");
+      window.location.href = url;
     });
   }
 
@@ -160,7 +160,7 @@ export class CiePrinciplePhaseComponent {
       this.expandedOptions.set(this.phaseTitleList[i], mode);
     }
   }
-  
+
   /**
    * Re-evaluates the visibility of all questions/subcategories/categories
    * based on the current filter settings.
@@ -170,7 +170,7 @@ export class CiePrinciplePhaseComponent {
   refreshQuestionVisibility(matLevel: number) {
     this.filterSvc.evaluateFiltersForReportCategories(this.response?.matAnsweredQuestions[0], matLevel);
   }
-  
+
   /**
    *
    */
