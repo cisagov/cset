4c64f480dbf06c80839e442e87610fe4e9d3d54e
Marcus Goectau <marcus.goectau@inl.gov>
2024-08-02 12:50:25 -0700

    Update upgrade script

--- CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/SQL/12240_to_12250.sql
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/SQL/12240_to_12250.sql
@@ -9,7 +9,7 @@ to synchronize it with:
 
 You are recommended to back up your database before running this script
 
-Script created by SQL Compare version 14.10.9.22680 from Red Gate Software Ltd at 7/25/2024 11:25:12 AM
+Script created by SQL Compare version 14.10.9.22680 from Red Gate Software Ltd at 8/2/2024 12:41:49 PM
 
 */
 SET NUMERIC_ROUNDABORT OFF
@@ -120,6 +120,147 @@ BEGIN
 	exec FillEmptyMaturityQuestionsForAnalysis @assessment_id
 	exec FillEmptyQuestionsForAnalysis @assessment_id
 END
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Creating [dbo].[GetMaturityComparisonBestToWorst]'
+GO
+-- =============================================
+-- Author:		hansbk
+-- Create date: 7/9/2018
+-- Description:	NOTE that this needs to be changed
+-- to allow for mulitple asssessments just by 
+-- passing mulitple id's 
+-- =============================================
+CREATE PROCEDURE [dbo].[GetMaturityComparisonBestToWorst]	
+@assessment_id int,
+@applicationMode nvarchar(100) = null
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	if((@applicationMode is null) or (@applicationMode = ''))
+		exec dbo.GetApplicationModeDefault @assessment_id, @ApplicationMode output
+
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	if(@ApplicationMode = 'Questions Based')
+		SELECT assessment_id,
+		AssessmentName = Alias,                
+		Name = Question_Group_Heading,
+		AlternateCount = [A],
+		AlternateValue = Round(((cast(([A]) as float)/isnull(nullif(Total,0),1)))*100,2),
+		NaCount = [NA],
+		NaValue = Round(((cast(([NA]) as float)/isnull(nullif(Total,0),1))*100),2),
+		NoCount = [N],
+		NoValue = Round(((cast(([N]) as float)/isnull(nullif(Total,0),1)))*100,2),
+		TotalCount = Total,
+		TotalValue = Total,
+		UnansweredCount = [U],
+		UnansweredValue = Round(cast([U] as float)/Total*100,2),
+		YesCount = [Y],
+		YesValue =  Round((cast(([Y]) as float)/isnull(nullif(Total,0),1))*100,2),
+		Value = Round(((cast(([Y]+ isnull([A],0)) as float)/isnull(nullif((Total-[NA]),0),1)))*100,2)
+		FROM 
+		(
+			select b.assessment_id, f.Alias, b.Question_Group_Heading, b.Answer_Text, isnull(c.Value,0) as Value, Total = sum(c.Value) over(partition by b.assessment_id,b.question_group_heading)
+			from 
+			 (select distinct Assessment_Id, Question_Group_Heading, l.answer_Text
+			from answer_lookup l, (select * from Answer_Maturity where assessment_id = @assessment_id) a 
+			join NEW_QUESTION q on a.Question_Or_Requirement_Id = q.Question_Id
+			join vQuestion_Headings h on q.Heading_Pair_Id = h.heading_pair_id
+			) b left join 
+			(select Assessment_Id, Question_Group_Heading, a.Answer_Text, count(a.answer_text) as Value
+				from (select * from Answer_Maturity where assessment_id = @assessment_id) a 
+				join NEW_QUESTION q on a.Question_Or_Requirement_Id = q.Question_Id
+				join vQuestion_Headings h on q.Heading_Pair_Id = h.heading_pair_id
+			 group by Assessment_Id, Question_Group_Heading, a.Answer_Text) c
+			 on b.Assessment_Id = c.Assessment_Id and b.Question_Group_Heading = c.Question_Group_Heading and b.Answer_Text = c.Answer_Text
+			 join ASSESSMENTS f on b.Assessment_Id = f.Assessment_Id
+		) p
+		PIVOT
+		(
+			sum(value)
+		FOR answer_text IN
+		( [Y],[N],[NA],[A],[U] )
+		) AS pvt
+	else-----------------------------------------requirements and framework side
+		SELECT Assessment_Id,
+		AssessmentName = Alias,                
+		Name = Question_Group_Heading,
+		AlternateCount = [A],
+		AlternateValue = Round(((cast(([A]) as float)/isnull(nullif(Total,0),1)))*100,2),
+		NaCount = [NA],
+		NaValue = Round(((cast(([NA]) as float)/isnull(nullif(Total,0),1)))*100,2),
+		NoCount = [N],
+		NoValue = Round(((cast(([N]) as float)/isnull(nullif(Total,0),1)))*100,2),
+		TotalCount = Total,
+		TotalValue = Total,
+		UnansweredCount = [U],
+		UnansweredValue = Round(cast([U] as float)/Total*100,2),
+		YesCount = [Y],
+		YesValue = Round((cast(([Y]) as float)/isnull(nullif(Total,0),1))*100,2),
+		Value = Round(((cast(([Y]+ isnull([A],0)) as float)/isnull(nullif((Total-[NA]),0),1)))*100,2)
+		FROM 
+		(
+			select b.Assessment_Id, f.Alias, b.Question_Group_Heading, b.Answer_Text, isnull(c.Value,0) as Value, Total = sum(c.Value) over(partition by b.Assessment_Id, b.question_group_heading)			
+			from 
+			 (select distinct a.[Assessment_Id], h.Question_Group_Heading, l.answer_Text
+			from answer_lookup l, (select * from Answer_Maturity where assessment_id = @assessment_id) a 
+			join NEW_REQUIREMENT q on a.Question_Or_Requirement_Id = q.Requirement_Id
+			join QUESTION_GROUP_HEADING h on q.Question_Group_Heading_Id = h.Question_Group_Heading_Id
+			) b left join 
+			(select a.Assessment_Id, Question_Group_Heading, a.Answer_Text, count(a.answer_text) as Value
+				from (select * from Answer_Maturity where assessment_id = @assessment_id) a 
+				join NEW_REQUIREMENT q on a.Question_Or_Requirement_Id = q.Requirement_Id
+				join QUESTION_GROUP_HEADING h on q.Question_Group_Heading_Id = h.Question_Group_Heading_Id
+			 group by Assessment_Id, Question_Group_Heading, a.Answer_Text) c
+			 on b.Assessment_Id = c.Assessment_Id and b.Question_Group_Heading = c.Question_Group_Heading and b.Answer_Text = c.Answer_Text
+			 join ASSESSMENTS f on b.Assessment_Id = f.Assessment_Id
+		) p
+		PIVOT
+		(
+			sum(value)
+		FOR answer_text IN
+		( [Y],[N],[NA],[A],[U] )
+		) AS pvt
+END
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Creating [dbo].[usp_getMaturitySummaryOverall]'
+GO
+-- =============================================
+-- Author:		hansbk
+-- Create date: 8/30/2018
+-- Description:	Stub needs completed
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_getMaturitySummaryOverall]
+	@assessment_id int
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+select *
+into #answers
+from answer
+where question_type = 'maturity' and Question_Or_Requirement_Id in (select mat_question_id from maturity_questions where maturity_model_id = 5)
+	select a.Answer_Full_Name,a.Answer_Text, isnull(m.qc,0) qc,isnull(m.Total,0) Total, isnull(cast(IsNull(Round((cast((qc) as float)/(isnull(nullif(Total,0),1)))*100,0),0) as int),0)  as [Percent]
+	from ANSWER_LOOKUP a left join (
+	SELECT a.Answer_Text, isnull(count(a.question_or_requirement_id),0) qc, SUM(count(a.question_or_requirement_id)) OVER() AS Total
+			FROM #answers a 				
+			where a.Assessment_Id = @assessment_id
+			group by a.Answer_Text
+	) m on a.Answer_Text = m.Answer_Text
+
+END
+
 GO
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
