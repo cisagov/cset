5074bc680cb95f349a31570cee5aaa48f2117ed1
Marcus Goectau <marcus.goectau@inl.gov>
2024-08-26 11:52:42 -0700

    Change for ACET release 12.2.6.0

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/ConvertDatabase12260.cs
@@ -0,0 +1,37 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2024 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using System;
+using Microsoft.Data.SqlClient;
+using System.IO;
+namespace UpgradeLibrary.Upgrade
+{
+    internal class ConvertDatabase12260 : ConvertSqlDatabase
+    {
+        public ConvertDatabase12260(string path) : base(path)
+        {
+            myVersion = new Version("12.2.6.0");
+        }
+
+        /// <summary>
+        /// Runs the database update script
+        /// </summary>
+        /// <param name="conn"></param>F
+        public override void Execute(SqlConnection conn)
+        {
+            try
+            {
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12250_to_12260.sql"), conn);
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12250_to_12260_data.sql"), conn);
+                this.UpgradeToVersionLocalDB(conn, myVersion);
+            }
+            catch (Exception e)
+            {
+                throw new DatabaseUpgradeException("Error in upgrading database version 12.2.5.0 to 12.2.6.0: " + e.Message);
+            }
+        }
+    }
+}
