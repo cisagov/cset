ce570a854bfd8e836160475fa22b6f83768e01f8
Randy Woods <randy1@radiorandy.com>
2024-10-14 15:20:27 -0600

    Replaced regenerated User Guide HTML files

---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Documents/htmlhelp/de_search/0xu0037.js
@@ -0,0 +1,9 @@
+DR_EXPLAIN = DR_EXPLAIN || {};
+DR_EXPLAIN.searchIndex = DR_EXPLAIN.searchIndex || {};
+DR_EXPLAIN.searchIndex["\u0037"] = [
+	{ "s": "7", "p": [65, 101, 81] },
+	{ "s": "7.0", "p": [101] },
+	{ "s": "7.1", "p": [101] },
+	{ "s": "701", "p": [6] },
+	{ "s": "7628", "p": [101] }
+];
---
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Documents/htmlhelp/de_search/0xu004b.js
@@ -0,0 +1,17 @@
+DR_EXPLAIN = DR_EXPLAIN || {};
+DR_EXPLAIN.searchIndex = DR_EXPLAIN.searchIndex || {};
+DR_EXPLAIN.searchIndex["\u004b"] = [
+	{ "s": "KEEP", "p": [90, 88, 68, 60, 58, 50, 33, 29, 23, 19, 17] },
+	{ "s": "KEPT", "p": [99, 65] },
+	{ "s": "KEY", "p": [100, 99, 77, 75, 61, 54, 42] },
+	{ "s": "KEYBOARD", "p": [42, 75, 100, 40] },
+	{ "s": "KEYS", "p": [77] },
+	{ "s": "KEYWORD", "p": [25, 84, 54, 75, 38, 24, 22] },
+	{ "s": "KEYWORD(S", "p": [38, 24, 22] },
+	{ "s": "KEYWORDS", "p": [38, 39] },
+	{ "s": "KIND", "p": [6] },
+	{ "s": "KIOSK", "p": [80] },
+	{ "s": "KNOW", "p": [99, 76, 63, 5] },
+	{ "s": "KNOWLEDGE", "p": [4, 3, 16, 6] },
+	{ "s": "KNOWN", "p": [5, 91, 80] }
+];
