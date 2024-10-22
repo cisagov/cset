45626e26fbe33a7b9c300d936a93a77b1bca28f0
Randy Woods <randy1@radiorandy.com>
2024-10-22 13:14:45 -0600

    Set version number in User Guide files

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Interfaces/Logging/Class1.cs
@@ -0,0 +1,18 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2024 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSETWebCore.Interfaces.Logging
+{
+    public interface Class1
+    {
+    }
+}
