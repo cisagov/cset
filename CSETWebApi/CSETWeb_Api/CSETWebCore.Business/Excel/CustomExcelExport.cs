0414bf9b9c46b3c9d85bb12eeeab10252b987f22
Barry Hansen <5023364+inlguy@users.noreply.github.com>
2024-11-11 08:59:51 -0700

    WIP Trying get Excel Working

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Business/Excel/CustomExcelExport.cs
@@ -0,0 +1,58 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2024 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using CSETWebCore.DataLayer.Model;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Data;
+using System.IO;
+
+namespace CSETWebCore.Api.Controllers
+{
+    public class CustomExcelExport
+    {
+        private CSETContext context;
+        private int assessmentId;
+
+        public CustomExcelExport(CSETContext context, int assessmentId)
+        {
+            this.context = context;
+            this.assessmentId = assessmentId;
+        }
+
+        public MemoryStream ExportExcel(int assessmentId)
+        {
+
+            
+        }
+
+        private XSSFWorkbook RenderDataTableToExcel(DataTable SourceTable)
+        {
+            XSSFWorkbook workbook = null;            
+            ISheet sheet = null;
+            XSSFRow headerRow = null;
+            
+            workbook = new XSSFWorkbook();                
+            sheet = workbook.CreateSheet();
+            headerRow = (XSSFRow)sheet.CreateRow(0);
+            foreach (DataColumn column in SourceTable.Columns)
+                headerRow.CreateCell(column.Ordinal).SetCellValue(column.ColumnName);
+            int rowIndex = 1;
+            foreach (DataRow row in SourceTable.Rows)
+            {
+                XSSFRow dataRow = (XSSFRow)sheet.CreateRow(rowIndex);
+                foreach (DataColumn column in SourceTable.Columns)
+                    dataRow.CreateCell(column.Ordinal).SetCellValue(row[column].ToString());
+                ++rowIndex;
+            }
+            for (int i = 0; i <= SourceTable.Columns.Count; ++i)
+                sheet.AutoSizeColumn(i);
+            return workbook;
+            
+        }
+    }
+}
