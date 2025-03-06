57c1bc8070d617bfbbd5d3b78fcd07c252973605
Marcus Goectau <marcus.goectau@inl.gov>
2025-03-06 15:51:35 -0800

    Updates for version 12.4.0.1

--- CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManagerTests1/DbManagerTests.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.DatabaseManagerTests1/DbManagerTests.cs
@@ -19,7 +19,7 @@ namespace CSETWebCore.DatabaseManager.Tests
         {
             string clientCode = "DHS";
             string appCode = "CSET";
-            DbManager manager = new DbManager(new Version("12.4.0.0"), clientCode, appCode);
+            DbManager manager = new DbManager(new Version("12.4.0.1"), clientCode, appCode);
             //TODO finish this.
             //manager.CopyDBAcrossServers();
         }
@@ -31,7 +31,7 @@ namespace CSETWebCore.DatabaseManager.Tests
             //setup a destination file
             string clientCode = "DHS";
             string appCode = "CSET";
-            DbManager manager = new DbManager(new Version("12.4.0.0"), clientCode, appCode);
+            DbManager manager = new DbManager(new Version("12.4.0.1"), clientCode, appCode);
             //run the same test twice and make sure that the number increment works
             string mdf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWebTest.mdf";
             string ldf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWebTest_log.ldf";
@@ -57,7 +57,7 @@ namespace CSETWebCore.DatabaseManager.Tests
             //setup a destination file
             string clientCode = "Test";
             string appCode = "Test";
-            DbManager manager = new DbManager(new Version("12.4.0.0"), clientCode, appCode);
+            DbManager manager = new DbManager(new Version("12.4.0.1"), clientCode, appCode);
 
             //manager.ForceCloseAndDetach(DbManager.localdb2019_ConnectionString, "TestWeb");
             //manager.AttachTest("TestWeb", testdb, testlog);
@@ -70,7 +70,7 @@ namespace CSETWebCore.DatabaseManager.Tests
 
             manager.SetupDb();
 
-            upgrader.UpgradeOnly(new Version("12.4.0.0"), "data source=(localdb)\\inllocaldb2022;initial catalog=TestWeb;persist security info=True;Integrated Security=SSPI;MultipleActiveResultSets=True");
+            upgrader.UpgradeOnly(new Version("12.4.0.1"), "data source=(localdb)\\inllocaldb2022;initial catalog=TestWeb;persist security info=True;Integrated Security=SSPI;MultipleActiveResultSets=True");
 
 
         }
@@ -83,7 +83,7 @@ namespace CSETWebCore.DatabaseManager.Tests
             //setup a destination file
             string clientCode = "DHS";
             string appCode = "CSET";
-            DbManager manager = new DbManager(new Version("12.4.0.0"), clientCode, appCode);
+            DbManager manager = new DbManager(new Version("12.4.0.1"), clientCode, appCode);
             manager.SetupDb();
         }
 
@@ -95,7 +95,7 @@ namespace CSETWebCore.DatabaseManager.Tests
             string mdf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWebTest.mdf";
             string ldf = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\CSETWebTest_log.ldf";
 
-            DbManager manager = new DbManager(new Version("12.4.0.0"), clientCode, appCode);
+            DbManager manager = new DbManager(new Version("12.4.0.1"), clientCode, appCode);
             //run the same test twice and make sure that the number increment works
             string conString = "Server=(localdb)\\inllocaldb2022;Integrated Security=true;AttachDbFileName=" + mdf;
             using (SqlConnection conn = new SqlConnection(conString))
---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/ConvertDatabase12401.cs
@@ -0,0 +1,37 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2024 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using System;
+using Microsoft.Data.SqlClient;
+using System.IO;
+namespace UpgradeLibrary.Upgrade
+{
+    internal class ConvertDatabase12401 : ConvertSqlDatabase
+    {
+        public ConvertDatabase12401(string path) : base(path)
+        {
+            myVersion = new Version("12.4.0.1");
+        }
+
+        /// <summary>
+        /// Runs the database update script
+        /// </summary>
+        /// <param name="conn"></param>F
+        public override void Execute(SqlConnection conn)
+        {
+            try
+            {
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12400_to_12401.sql"), conn);
+                RunFile(Path.Combine(this.applicationPath, "VersionUpgrader", "SQL", "12400_to_12401_data.sql"), conn);
+                this.UpgradeToVersionLocalDB(conn, myVersion);
+            }
+            catch (Exception e)
+            {
+                throw new DatabaseUpgradeException("Error in upgrading database version 12.4.0.0 to 12.4.0.1: " + e.Message);
+            }
+        }
+    }
+}
