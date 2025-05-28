8e61e6e51b7eaf4bb508357454f8c62b683a0261
Jason Kuipers <jason.kuipers@inl.gov>
2025-05-28 11:27:31 -0600

    add role on user creation

--- CSETWebApi/CSETWeb_Api/CSETWebCore.Business/User/UserBusiness.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Business/User/UserBusiness.cs
@@ -60,7 +60,7 @@ namespace CSETWebCore.Business.User
                 PasswordResetRequired = true,
                 IsActive = true
             };
-
+            
             // default the new user to NOT active if CSET Online is running in beta mode
             if (new CSETGlobalProperties(_context).GetBoolProperty("IsCsetOnlineBeta") ?? false)
             {
@@ -68,8 +68,7 @@ namespace CSETWebCore.Business.User
             }
 
             tmpContext.USERS.Add(u);
-
-
+            
             try
             {
                 tmpContext.SaveChanges();
@@ -100,8 +99,17 @@ namespace CSETWebCore.Business.User
                 Password = hash,
                 Salt = salt
             };
+            
+            var roles = _context.ROLES.FirstOrDefault(x=>x.RoleName == "USER");
+            var userRole = new USER_ROLES()
+            {
+                UserId = u.UserId,
+                RoleId = roles.RoleId
+            };
+            
             _context.PASSWORD_HISTORY.Add(history);
-
+            _context.USER_ROLES.Add(userRole);
+            
             UserCreateResponse resp = new UserCreateResponse
             {
                 UserId = u.UserId == 0 ? 1 : u.UserId,
