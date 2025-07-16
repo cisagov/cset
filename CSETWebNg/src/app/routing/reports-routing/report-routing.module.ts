0a564d7bb45459a25d14c7be3d7e368afdf04524
Randy Woods <randy1@radiorandy.com>
2025-07-16 10:06:10 -0600

    Skeleton for CRE MIL Charts 2 report

--- CSETWebNg/src/app/routing/reports-routing/report-routing.module.ts
+++ CSETWebNg/src/app/routing/reports-routing/report-routing.module.ts
@@ -45,6 +45,7 @@ import { VadrDeficiencyComponent } from '../../reports/vadr/vadr-deficiency/vadr
 import { VadrReportComponent } from '../../reports/vadr/vadr-report/vadr-report.component';
 import { CisaVadrReportComponent } from '../../reports/cisa-vadr/cisa-vadr-report/cisa-vadr-report.component';
 import { CreHeatmapsComponent } from '../../reports/crePlus/cre-heatmaps/cre-heatmaps.component';
+import { CreMilCharts2Component } from '../../reports/crePlus/cre-mil-charts-2/cre-mil-charts-2.component';
 
 
 const routes: Routes = [
@@ -82,6 +83,7 @@ const routes: Routes = [
     { path: 'cre-final-report/:m', component: CreFinalReportComponent },
     { path: 'cre-assessment-overview', component: CreAssessmentOverview },
     { path: 'cre-model-chart-report/:m', component: CreModelChartsComponent },
+    { path: 'cre-mil-charts-2', component: CreMilCharts2Component },
     { path: 'cre-heatmaps', component: CreHeatmapsComponent },
     { path: 'commentsmfr', component: CommentsMfrComponent },
     { path: 'module-content', component: ModuleContentComponent },
