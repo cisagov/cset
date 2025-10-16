632925c16fcebb1355a00a60c277b0212fcba2a9
Jason Kuipers <jason.kuipers@inl.gov>
2025-10-16 08:18:17 -0600

    add button for changing mode

--- CSETWebNg/src/app/components/theme-toggle/theme-toggle.component.ts
+++ CSETWebNg/src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,29 +1,7 @@
-////////////////////////////////
-//
-//   Copyright 2025 Battelle Energy Alliance, LLC
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
-//
-////////////////////////////////
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ThemeService, Theme } from '../../services/theme.service';
 import { Subscription } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-theme-toggle',
@@ -38,9 +16,13 @@ export class ThemeToggleComponent implements OnInit, OnDestroy {
   constructor(private themeService: ThemeService) {}
 
   ngOnInit(): void {
-    this.themeSubscription = this.themeService.theme$.subscribe((theme: Theme) => {
-      this.isDarkMode = theme === 'dark';
-    });
+    this.isDarkMode = this.themeService.getTheme() === 'dark';
+
+    this.themeSubscription = this.themeService.theme$
+      .pipe(distinctUntilChanged())
+      .subscribe((theme: Theme) => {
+        this.isDarkMode = theme === 'dark';
+      });
   }
 
   ngOnDestroy(): void {
