f0cb5d58a396c44000c5899015fc3272d9384c11
Lilianne Cantillo <lcantillodev@gmail.com>
2025-10-24 10:45:33 -0400

    Standardize star colors using the CSET blue

--- CSETWebNg/src/app/initial/my-assessments/my-assessments.component.ts
+++ CSETWebNg/src/app/initial/my-assessments/my-assessments.component.ts
@@ -248,7 +248,7 @@ export class MyAssessmentsComponent implements OnInit {
           const assessment = params.data;
           const percentage = this.getCompletionPercentage(assessment);
           const favoriteIcon = assessment.favorite ? 'favorite' : 'favorite_border';
-          const favoriteClass = assessment.favorite ? 'tw:text-amber-500' : 'tw:text-gray-400';
+          const favoriteClass = assessment.favorite ? 'star-favorite' : 'star-inactive';
           const reviewFlag = (assessment.markedForReview || assessment.altTextMissing);
           const flagClass = reviewFlag ? 'tw:text-orange-500' : 'tw:text-gray-400';
           const tooltipText = this.getProgressTooltip(assessment);
