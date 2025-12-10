b0445177e66e4ad1a0cf20c73acd630b8a16f810
Mostafa Abdo <mostafaxcodes@gmail.com>
2025-12-10 08:36:07 -0800

    fix restoredb script

--- backup/restoredb.sql
+++ backup/restoredb.sql
@@ -1,5 +1,5 @@
 RESTORE DATABASE CSET
-FROM DISK = '/var/opt/mssql/backup/CSETWeb.bak'
+FROM DISK = '/var/opt/mssql/backup/CSET.bak'
 WITH REPLACE,
 MOVE 'CSETWeb' TO '/var/opt/mssql/data/CSET.mdf',
 MOVE 'CSETWeb_Log' TO '/var/opt/mssql/data/CSET_Log.ldf'
