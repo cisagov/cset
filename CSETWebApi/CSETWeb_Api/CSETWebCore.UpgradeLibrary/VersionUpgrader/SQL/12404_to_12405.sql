05fdb97a6528d2a866b6d62e9817fdc6d321e163
Mostafa Abdo <mostafaxcodes@gmail.com>
2026-01-07 20:04:02 -0800

    chore(db): add DROP PROCEDURE for usp_getStandardsResultsByCategory
    Add database migration script to drop the stored procedure that has
    been replaced with LINQ equivalent in StandardsResultsByCategoryBusiness.

--- CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/SQL/12404_to_12405.sql
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.UpgradeLibrary/VersionUpgrader/SQL/12404_to_12405.sql
@@ -347,3 +347,8 @@ GO
 IF OBJECT_ID('dbo.usp_getComponentsSummary', 'P') IS NOT NULL
     DROP PROCEDURE dbo.usp_getComponentsSummary;
 GO
+
+-- usp_getStandardsResultsByCategory (replaced with LINQ equivalent in StandardsResultsByCategoryBusiness.cs)
+IF OBJECT_ID('dbo.usp_getStandardsResultsByCategory', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.usp_getStandardsResultsByCategory;
+GO
