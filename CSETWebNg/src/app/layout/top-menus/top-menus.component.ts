8f1c40eef9e37a5da4aad0b9bfca78fb039e3ff1
Randy Woods <randy1@radiorandy.com>
2026-01-19 16:03:59 -0500

    Hiding Global/Shared document feature for now

--- CSETWebNg/src/app/layout/top-menus/top-menus.component.ts
+++ CSETWebNg/src/app/layout/top-menus/top-menus.component.ts
@@ -559,6 +559,7 @@ export class TopMenusComponent implements OnInit {
 
   isNullOrEmptyAssessment() {
     let str = localStorage.getItem('assessmentId');
+    console.log(str);
     let hasNoVal = str === null || str === undefined || str === '';
     return hasNoVal;
   }
