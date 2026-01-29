b700d3ddc97beda70a45f56fbf1b9c7ba4857efa
Randy Woods <randy1@radiorandy.com>
2026-01-29 12:24:10 -0500

    More multi-sector refinements

--- CSETWebNg/src/app/assessment/prepare/assessment-info/sector-subsector/sector-subsector.component.ts
+++ CSETWebNg/src/app/assessment/prepare/assessment-info/sector-subsector/sector-subsector.component.ts
@@ -1,3 +1,26 @@
+////////////////////////////////
+//
+//   Copyright 2025 Battelle Energy Alliance, LLC
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+////////////////////////////////
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { DemographicIodService } from '../../../../services/demographic-iod.service';
 import { AssessmentService } from '../../../../services/assessment.service';
@@ -5,11 +28,12 @@ import { DemographicsIod } from '../../../../models/demographics-iod.model';
 import { SectorSub } from '../../../../models/demographics-extended.model';
 import { ConstantsService } from '../../../../services/constants.service';
 
+
 @Component({
   selector: 'app-sector-subsector',
   standalone: false,
   templateUrl: './sector-subsector.component.html',
-  styleUrl: './sector-subsector.component.scss',
+  styleUrl: './sector-subsector.component.scss'
 })
 export class SectorSubsectorComponent implements OnInit, OnChanges {
 
@@ -76,6 +100,8 @@ export class SectorSubsectorComponent implements OnInit, OnChanges {
     const maxSeq = Math.max(...seqs);
 
     const newSectorSubsector: SectorSub = {
+      sectorId: null,
+      subsectorId: null,
       sequence: maxSeq + 1
     };
     this.demographicData.sectorSubsectors.push(newSectorSubsector);
