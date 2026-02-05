80d5294d68f6807d427b479d63e7232883bb67cc
Mostafa Abdo <mostafaxcodes@gmail.com>
2026-02-05 14:59:22 -0800

    chore: remove unused using directives
    Remove unused imports from SectorUpgrade.cs and AnalyticsController.cs
    to align with the IDE0005 warning configuration.

--- CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/AnalyticsController.cs
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Controllers/AnalyticsController.cs
@@ -17,7 +17,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
