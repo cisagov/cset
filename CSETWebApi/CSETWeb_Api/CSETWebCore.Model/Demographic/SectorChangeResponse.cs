93f094e98cb3ac228cd203b48641564f55b35c05
Randy Woods <randy1@radiorandy.com>
2026-02-06 10:38:21 -0500

    Add question count responses to sector changes
    Sector changes may affect the number of questions in play (SSG inclusion) so now those events can refresh the completion bar as sectors are changed.

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Model/Demographic/SectorChangeResponse.cs
@@ -0,0 +1,20 @@
+﻿//////////////////////////////// 
+// 
+//   Copyright 2025 Battelle Energy Alliance, LLC  
+// 
+// 
+//////////////////////////////// 
+using System.Collections.Generic;
+
+namespace CSETWebCore.Model.Demographic
+{
+    public class SectorChangeResponse
+    {
+        public List<ListItem2> Subsectors { get; set; } = [];
+
+        public int? CompletedCount { get; set; }
+        public int? TotalMaturityQuestionsCount { get; set; }
+        public int? TotalDiagramQuestionsCount { get; set; }
+        public int? TotalStandardQuestionsCount { get; set; }
+    }
+}
