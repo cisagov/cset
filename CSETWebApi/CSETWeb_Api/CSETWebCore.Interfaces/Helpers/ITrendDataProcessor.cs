eedf647e00d54e29bd1563d56e8961c6be88aca9
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2026-02-23 09:55:23 -0700

    async data processing

--- CSETWebApi/CSETWeb_Api/CSETWebCore.Interfaces/Helpers/ITrendDataProcessor.cs
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Interfaces/Helpers/ITrendDataProcessor.cs
@@ -4,6 +4,8 @@
 // 
 // 
 //////////////////////////////// 
+
+using System.Threading.Tasks;
 using CSETWebCore.DataLayer.Model;
 using CSETWebCore.Model.Aggregation;
 
@@ -11,6 +13,7 @@ namespace CSETWebCore.Interfaces.Helpers
 {
     public interface ITrendDataProcessor
     {
-        void Process(CSETContext db, int aggregationID, LineChart response, string Type);
+        Task ProcessAsync(CSETContext db, int aggregationID, LineChart response, string type);
     }
+    
 }
