6ec2da9972f4fa43821a12d16600d0d2c008e5a6
Randy Woods <randy1@radiorandy.com>
2026-03-16 16:58:28 -0400

    Removed Feedback references from User Guide

--- CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Documents/htmlhelp/de_search/0xu0033.js
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Documents/htmlhelp/de_search/0xu0033.js
@@ -1,7 +1,7 @@
 DR_EXPLAIN = DR_EXPLAIN || {};
 DR_EXPLAIN.searchIndex = DR_EXPLAIN.searchIndex || {};
 DR_EXPLAIN.searchIndex["\u0033"] = [
-	{ "s": "3", "p": [98, 96, 78, 5] },
-	{ "s": "3.0", "p": [98] },
+	{ "s": "3", "p": [96, 94, 78, 5] },
+	{ "s": "3.0", "p": [96] },
 	{ "s": "365", "p": [62, 61] }
 ];
--- CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Documents/htmlhelp/de_search/0xu005a.js
+++ CSETWebApi/CSETWeb_Api/CSETWeb_ApiCore/Documents/htmlhelp/de_search/0xu005a.js
@@ -1,8 +1,8 @@
 DR_EXPLAIN = DR_EXPLAIN || {};
 DR_EXPLAIN.searchIndex = DR_EXPLAIN.searchIndex || {};
 DR_EXPLAIN.searchIndex["\u005a"] = [
-	{ "s": "ZONE", "p": [73, 70, 65, 97, 78, 77] },
+	{ "s": "ZONE", "p": [73, 70, 65, 95, 78, 77] },
 	{ "s": "ZONE'S", "p": [70] },
-	{ "s": "ZONES", "p": [65, 78, 73, 98, 76, 97, 79, 70] },
+	{ "s": "ZONES", "p": [65, 78, 73, 96, 76, 95, 79, 70] },
 	{ "s": "ZOOM", "p": [69, 65] }
 ];
